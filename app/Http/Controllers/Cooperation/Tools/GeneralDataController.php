63315825e300d76b5a0e4031867976d87fac530a
Bodhi <bodhilooij1998@gmail.com>
2018-03-16 13:55:32 +0100

    Created file's for the general data tab

---
+++ app/Http/Controllers/Cooperation/Tools/GeneralDataController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Cooperation\Tools;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GeneralDataController extends Controller
+{
+    //
+}

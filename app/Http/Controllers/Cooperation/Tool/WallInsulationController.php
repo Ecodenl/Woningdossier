2ea5fb9cbd42cac4cfc79ab8cb6bceb52d93b270
pvankouteren <pvankouteren@gmail.com>
2018-04-20 13:13:14 +0200

    Accept comma's, update temperature

--- app/Http/Controllers/Cooperation/Tool/WallInsulationController.php
+++ app/Http/Controllers/Cooperation/Tool/WallInsulationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Cooperation\Tool;
 
 use App\Helpers\Calculation\BankInterestCalculator;
 use App\Helpers\Calculator;
+use App\Helpers\Hoomdossier;
 use App\Helpers\KeyFigures\WallInsulation\Temperature;
 use App\Helpers\NumberFormatter;
 use App\Http\Requests\WallInsulationRequest;
@@ -75,7 +76,7 @@ class WallInsulationController extends Controller
         $damagedPaintwork = $request->get('facade_damaged_paintwork_id', 0);
         $wallJoints = $request->get('wall_joints', '');
         $wallJointsContaminated = $request->get('contaminated_wall_joints', '');
-        $wallSurface = $request->get('facade_surface', '');
+        $wallSurface = Hoomdossier::convertDecimal($request->get('facade_surface', ''));
         $additionalInfo = $request->get('additional_info', '');
         $cavityWall = $request->get('cavity_wall', '');
         $facadePlasteredOrPainted = $request->get('facade_plastered_painted', '');
@@ -175,7 +176,7 @@ class WallInsulationController extends Controller
 
     	$cavityWall = $request->get('cavity_wall', -1);
 		$elements = $request->get('element', []);
-		$facadeSurface = $request->get('facade_surface', 0);
+		$facadeSurface = Hoomdossier::convertDecimal($request->get('facade_surface', 0));
 
     	$result = [
     		'savings_gas' => 0,

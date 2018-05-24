a3d088a5c842dce31e1dc01b65f09bafdd2cb0ac
Bodhi <bodhilooij1998@gmail.com>
2018-05-24 12:48:33 +0200

    Added target blank to registration confirmation

--- resources/views/cooperation/mail/user/confirm_account.blade.php
+++ resources/views/cooperation/mail/user/confirm_account.blade.php
@@ -4,6 +4,6 @@
     $url = route('cooperation.confirm', ['cooperation' => $cooperation, 'u' => $user->email, 't' => $user->confirm_token]);
 ?>
 
-@lang('mail.confirm_account.text', ['home_url' => config('app.url'), 'confirm_url' => '<a href="'.$url.'">'.$url .'</a>'])
+@lang('mail.confirm_account.text', ['home_url' => config('app.url'), 'confirm_url' => '<a target="_blank" href="'.$url.'">'.$url .'</a>'])
 
 @lang('mail.confirm_account.signature', ['app_name' => config('app.name')])

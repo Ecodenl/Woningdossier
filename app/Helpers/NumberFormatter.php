e7f2bdb969c1cf2eb0a04efdd2b58e14d04e5058
pvankouteren <pvankouteren@gmail.com>
2018-05-24 09:13:41 +0200

    Watch it

--- app/Helpers/NumberFormatter.php
+++ app/Helpers/NumberFormatter.php
@@ -20,21 +20,12 @@ class NumberFormatter {
 		if (is_null($number)){
 			$number = 0;
 		}
-
-		\Log::debug("anders kanker je ff op. " . $locale . " | " . $number);
-
-		try {
-			$f = number_format(
+		return number_format(
 				$number,
 				$decimals,
 				self::$localeSeparators[ $locale ]['decimal'],
 				self::$localeSeparators[ $locale ]['thousands']
-			);
-			return $f;
-		}
-		catch(\Exception $e){
-			\Log::debug("Ik ben een achterlijke grafkankertyfusmongool: " . $number . ", " . $decimals . ", " . self::$localeSeparators[ $locale ]['decimal'] . ", " . self::$localeSeparators[ $locale ]['thousands']);
-		}
+		);
 	}
 
 	public static function reverseFormat($number){

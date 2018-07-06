17d0af6ea3abdf311354bc2d19368bdd7801cbaa
Bodhi <bodhilooij1998@gmail.com>
2018-07-06 11:46:08 +0200

    Cleaner code

--- app/Http/Controllers/Admin/CsvExportController.php
+++ app/Http/Controllers/Admin/CsvExportController.php
@@ -17,8 +17,7 @@ class CsvExportController extends Controller
 
     public function downloadByYear()
     {
-
-        // set the headers and stuff
+        // set the headers for the browser
         $headers = [
             "Content-type" => "text/csv",
             "Content-Disposition" => "attachment; filename=by-year.csv",
@@ -27,15 +26,37 @@ class CsvExportController extends Controller
             "Expires" => "0"
         ];
 
-        // get the data
+        // get user data
         $user = \Auth::user();
         $cooperation = $user->cooperations()->first();
 
+        // get the users from the cooperations
         $users = $cooperation->users;
 
-        // todo: find a cleaner way if possible
+        // set the csv headers
+        $csvHeaders =[
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.first-name'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.last-name'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.email'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.phonenumber'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.mobilenumber'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.street'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.house-number'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.city'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.zip-code'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.country-code'),
+        ];
 
-        foreach ($users as $user) {
+        // put the measures inside the header array
+        for ($startYear = 2018; $startYear <= 2118; $startYear++) {
+            $csvHeaders[] = $startYear;
+        }
+
+        // new array for the userdata
+        $rows = [];
+
+
+        foreach ($users as $key => $user) {
 
             $building = $user->buildings()->first();
             $street = $building->street;
@@ -50,91 +71,50 @@ class CsvExportController extends Controller
             $phoneNumber = $user->phone_number;
             $mobileNumber = $user->mobile;
 
+            // set the personal userinfo
+            $row[$key] = [$firstName, $lastName, $email, $phoneNumber, $mobileNumber, $street, $number, $city, $postalCode, $countryCode];
 
-            // create the user info array
-            $allUserInfo[$user->id]["user"] = [$firstName, $lastName, $email, $phoneNumber, $mobileNumber, $street, $number, $city, $postalCode, $countryCode];
-
-            // get the advices for a specific user
-            $advices = UserActionPlanAdvice::getCategorizedActionPlan($user);
-
-            // add the year titles
-            $yearTitles = [];
+            // set all the years in a range from 2018 to 2118
             for ($startYear = 2018; $startYear <= 2118; $startYear++) {
-                $yearTitles[] = $startYear;
+                $row[$key][$startYear] = "";
             }
 
-            foreach($advices as $measureType => $stepAdvices) {
-                foreach ($stepAdvices as $step => $advicesForStep) {
-                    foreach ($advicesForStep as $advice) {
-                        $measure = $advice->measureApplication->measure_name;
-                        $plannedYear = $advice->planned_year == null ? $advice->year : $advice->planned_year;
-                        $allUserInfo[$user->id][$plannedYear][] = $measure;
-                    }
+            // get the user measures / advices
+            foreach ($user->actionPlanAdvices as $actionPlanAdvice) {
+
+                $plannedYear = $actionPlanAdvice->planned_year == null ? $actionPlanAdvice->year : $actionPlanAdvice->planned_year;
+                $measureName = $actionPlanAdvice->measureApplication->measure_name;
+
+                if (is_null($plannedYear)){
+                    $plannedYear = $actionPlanAdvice->getAdviceYear();
                 }
+
+                // create a new array with the measures for the user connected to the planned year
+                $allUserMeasures[$plannedYear][] = $measureName;
+
             }
+
+            // loop through the user measures and add them to the row
+            foreach ($allUserMeasures as $year => $userMeasures) {
+                $row[$key][$year] = implode(", ", $userMeasures);
+            }
+
+            $rows = $row;
 
         }
 
-        $nameTitles =[
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.first-name'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.last-name'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.email'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.phonenumber'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.mobilenumber'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.street'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.house-number'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.city'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.zip-code'),
-            __('woningdossier.cooperation.admin.csv-export.csv-columns.country-code'),
-        ];
-
-
-        // merge the titles toghetter to 1 array
-        $titles = array_merge($nameTitles, $yearTitles);
-
-        // loop through all the user info per user
-        foreach ($allUserInfo as $userInfo) {
-
-            // get the information off one specific user
-            foreach ($userInfo as $plannedYear => $user) {
-                // if the array key is a user extract the user information
-                // and put it inside the array with the email as key
-                if ($plannedYear == "user") {
-                    $userEmail = $user[2];
-                    foreach ($user as $userPersonalInfo) {
-                        $result[$userEmail][] = $userPersonalInfo;
-                    }
-                }
-                // if its not a user then its a year that holds the measures
-                // put them inside the array
-                if ($plannedYear != "user") {
-                    for ($startYear = 2018; $startYear <= 2118; $startYear++) {
-                        // if the startyear is = to the planned year
-                        // put the measures inside the year
-                        // else add the year with an empty string
-                        if ($startYear == $plannedYear) {
-                            $result[$userEmail][$plannedYear] = implode(", ", $user);
-                        } else {
-                            $result[$userEmail][] = "";
-                        }
-
-                    }
-
-                }
-            }
-        }
 
 
         // write the CSV file
-        $callback = function () use ($result, $titles) {
+        $callback = function () use ($csvHeaders, $rows) {
 
             $file = fopen('php://output', 'w');
 
-            fputcsv($file, $titles, ';');
+            fputcsv($file, $csvHeaders, ';');
 
-            foreach ($result as $userMeasures) {
+            foreach ($rows as $userMeasures) {
 
-                    fputcsv($file, $userMeasures, ';');
+                fputcsv($file, $userMeasures, ';');
             }
 
             fclose($file);
@@ -144,13 +124,142 @@ class CsvExportController extends Controller
         return \Response::stream($callback, 200, $headers);
     }
 
+//    public function downloadByYear()
+//    {
+//
+//        // set the headers and stuff
+//        $headers = [
+//            "Content-type" => "text/csv",
+//            "Content-Disposition" => "attachment; filename=by-year.csv",
+//            "Pragma" => "no-cache",
+//            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+//            "Expires" => "0"
+//        ];
+//
+//        // get the data
+//        $user = \Auth::user();
+//        $cooperation = $user->cooperations()->first();
+//
+//        $users = $cooperation->users;
+//
+//        // todo: find a cleaner way if possible
+//
+//        foreach ($users as $user) {
+//
+//            $building = $user->buildings()->first();
+//            $street = $building->street;
+//            $number = $building->number;
+//            $city = $building->city;
+//            $postalCode = $building->postal_code;
+//            $countryCode = $building->country_code;
+//
+//            $firstName = $user->first_name;
+//            $lastName = $user->last_name;
+//            $email = $user->email;
+//            $phoneNumber = $user->phone_number;
+//            $mobileNumber = $user->mobile;
+//
+//
+//            // create the user info array
+//            $allUserInfo[$user->id]["user"] = [$firstName, $lastName, $email, $phoneNumber, $mobileNumber, $street, $number, $city, $postalCode, $countryCode];
+//
+//            // get the advices for a specific user
+//            $advices = UserActionPlanAdvice::getCategorizedActionPlan($user);
+//
+//            // add the year titles
+//            $yearTitles = [];
+//            for ($startYear = 2018; $startYear <= 2118; $startYear++) {
+//                $yearTitles[] = $startYear;
+//            }
+//
+//            foreach($advices as $measureType => $stepAdvices) {
+//                foreach ($stepAdvices as $step => $advicesForStep) {
+//                    foreach ($advicesForStep as $advice) {
+//                        $measure = $advice->measureApplication->measure_name;
+//                        $plannedYear = $advice->planned_year == null ? $advice->year : $advice->planned_year;
+//                        $allUserInfo[$user->id][$plannedYear][] = $measure;
+//                    }
+//                }
+//            }
+//
+//        }
+//
+//        $nameTitles =[
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.first-name'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.last-name'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.email'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.phonenumber'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.mobilenumber'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.street'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.house-number'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.city'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.zip-code'),
+//            __('woningdossier.cooperation.admin.csv-export.csv-columns.country-code'),
+//        ];
+//
+//
+//        // merge the titles toghetter to 1 array
+//        $titles = array_merge($nameTitles, $yearTitles);
+//
+//        // loop through all the user info per user
+//        foreach ($allUserInfo as $userInfo) {
+//
+//            // get the information off one specific user
+//            foreach ($userInfo as $plannedYear => $user) {
+//                // if the array key is a user extract the user information
+//                // and put it inside the array with the email as key
+//                if ($plannedYear == "user") {
+//                    $userEmail = $user[2];
+//                    foreach ($user as $userPersonalInfo) {
+//                        $result[$userEmail][] = $userPersonalInfo;
+//                    }
+//                }
+//                // if its not a user then its a year that holds the measures
+//                // put them inside the array
+//                if ($plannedYear != "user") {
+//                    for ($startYear = 2018; $startYear <= 2118; $startYear++) {
+//                        // if the startyear is = to the planned year
+//                        // put the measures inside the year
+//                        // else add the year with an empty string
+//                        if ($startYear == $plannedYear) {
+//                            $result[$userEmail][$plannedYear] = implode(", ", $user);
+//                        } else {
+//                            $result[$userEmail][] = "";
+//                        }
+//
+//                    }
+//
+//                }
+//            }
+//        }
+//
+//
+//        // write the CSV file
+//        $callback = function () use ($result, $titles) {
+//
+//            $file = fopen('php://output', 'w');
+//
+//            fputcsv($file, $titles, ';');
+//
+//            foreach ($result as $userMeasures) {
+//
+//                    fputcsv($file, $userMeasures, ';');
+//            }
+//
+//            fclose($file);
+//        };
+//
+//
+//        return \Response::stream($callback, 200, $headers);
+//    }
+
     public function downloadByMeasure()
     {
 
         // set the headers for the browser
         $headers = [
             "Content-type" => "text/csv",
-            "Content-Disposition" => "attachment; filename=by-year.csv",
+            "Content-Disposition" => "attachment; filename=by-measure.csv",
             "Pragma" => "no-cache",
             "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
             "Expires" => "0"

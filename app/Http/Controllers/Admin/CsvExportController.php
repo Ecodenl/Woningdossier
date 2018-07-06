3057443339f781570ef25a6c4933bde7364ef9c5
Bodhi <bodhilooij1998@gmail.com>
2018-07-06 11:08:07 +0200

    Added download plan by measure

--- app/Http/Controllers/Admin/CsvExportController.php
+++ app/Http/Controllers/Admin/CsvExportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Measure;
+use App\Models\MeasureApplication;
 use App\Models\UserActionPlanAdvice;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -127,7 +129,7 @@ class CsvExportController extends Controller
         $callback = function () use ($result, $titles) {
 
             $file = fopen('php://output', 'w');
-            
+
             fputcsv($file, $titles, ';');
 
             foreach ($result as $userMeasures) {
@@ -145,5 +147,106 @@ class CsvExportController extends Controller
     public function downloadByMeasure()
     {
 
+        // set the headers for the browser
+        $headers = [
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=by-year.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0"
+        ];
+
+        // get user data
+        $user = \Auth::user();
+        $cooperation = $user->cooperations()->first();
+
+        // get the users from the cooperations
+        $users = $cooperation->users;
+
+        // set the csv headers
+        $csvHeaders =[
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.first-name'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.last-name'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.email'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.phonenumber'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.mobilenumber'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.street'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.house-number'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.city'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.zip-code'),
+            __('woningdossier.cooperation.admin.csv-export.csv-columns.country-code'),
+        ];
+
+        // get all the measures
+        $measures = MeasureApplication::all();
+
+        // put the measures inside the header array
+        foreach ($measures as $measure) {
+            $csvHeaders[] = $measure->measure_name;
+        }
+
+        // new array for the userdata
+        $rows = [];
+
+
+        foreach ($users as $key => $user) {
+
+            $building = $user->buildings()->first();
+            $street = $building->street;
+            $number = $building->number;
+            $city = $building->city;
+            $postalCode = $building->postal_code;
+            $countryCode = $building->country_code;
+
+            $firstName = $user->first_name;
+            $lastName = $user->last_name;
+            $email = $user->email;
+            $phoneNumber = $user->phone_number;
+            $mobileNumber = $user->mobile;
+
+            // set the personal userinfo
+            $row[$key] = [$firstName, $lastName, $email, $phoneNumber, $mobileNumber, $street, $number, $city, $postalCode, $countryCode];
+
+            // set alle the measures to the user
+            foreach ($measures as $measure) {
+                $row[$key][$measure->measure_name] = "";
+            }
+
+            // get the user measures / advices
+            foreach ($user->actionPlanAdvices as $actionPlanAdvice) {
+
+                $plannedYear = $actionPlanAdvice->planned_year == null ? $actionPlanAdvice->year : $actionPlanAdvice->planned_year;
+                $measureName = $actionPlanAdvice->measureApplication->measure_name;
+
+                if (is_null($plannedYear)){
+                    $plannedYear = $actionPlanAdvice->getAdviceYear();
+                }
+
+                // fill the measure with the planned year
+                $row[$key][$measureName] = $plannedYear;
+            }
+
+            $rows = $row;
+
+        }
+
+
+        // write the CSV file
+        $callback = function () use ($csvHeaders, $rows) {
+
+            $file = fopen('php://output', 'w');
+
+            fputcsv($file, $csvHeaders, ';');
+
+            foreach ($rows as $userMeasures) {
+
+                fputcsv($file, $userMeasures, ';');
+            }
+
+            fclose($file);
+        };
+
+
+        return \Response::stream($callback, 200, $headers);
     }
 }

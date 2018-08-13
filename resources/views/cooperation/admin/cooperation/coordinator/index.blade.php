7ee8f5a5df394912e5dd1ad5e027cad9e6955490
Bodhi <bodhilooij1998@gmail.com>
2018-08-13 16:30:09 +0200

    moved some blade files

--- resources/views/cooperation/admin/cooperation/coordinator/index.blade.php
+++ resources/views/cooperation/admin/cooperation/coordinator/index.blade.php
@@ -1,23 +1,32 @@
-@extends('cooperation.admin.layouts.app')
+@extends('cooperation.admin.coach.layouts.app')
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">@lang('woningdossier.cooperation.admin.cooperation.coordination.header')</div>
+@section('coach_content')
+    <div class="panel panel-default">
+        <div class="panel-heading">@lang('woningdossier.cooperation.admin.coach.index.header')</div>
 
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <h4>@lang('woningdossier.cooperation.admin.cooperation.coordination.text')</h4>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="row">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4>@lang('woningdossier.cooperation.admin.coach.index.text')</h4>
+                    <table class="table table-responsive table-condensed">
+                        <thead>
+                        <tr>
+                            <th>@lang('woningdossier.cooperation.admin.coach.index.table.columns.city')</th>
+                            <th>@lang('woningdossier.cooperation.admin.coach.index.table.columns.street')</th>
+                            <th>@lang('woningdossier.cooperation.admin.coach.index.table.columns.owner')</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($buildingPermissions as $buildingPermission)
+                            <tr>
+                                <td>{{ $buildingPermission->building->city }}</td>
+                                <td>{{ $buildingPermission->building->city }}</td>
+                                <td>{{ $buildingPermission->building->user->first_name .' '. $buildingPermission->building->user->last_name}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
 
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>

b2ababd98ee1d8b31fe77d0c1adb447e6c8b4248
pvankouteren <pvankouteren@gmail.com>
2018-09-04 16:49:03 +0200

    Take submit off the button in case JS does not work / is disabled

--- resources/views/cooperation/my-account/settings/index.blade.php
+++ resources/views/cooperation/my-account/settings/index.blade.php
@@ -131,7 +131,7 @@
                             <div class="form-group">
                                 <label for="reset-file" class="col-md-4 control-label">@lang('woningdossier.cooperation.my-account.settings.form.reset-file.label')</label>
                                 <div class="col-md-8">
-                                    <button type="submit" id="reset-account" class="btn btn-danger">
+                                    <button id="reset-account" class="btn btn-danger">
                                         @lang('woningdossier.cooperation.my-account.settings.form.reset-file.submit')
                                     </button>
                                 </div>
@@ -176,8 +176,9 @@
         var areYouSure = '@lang('woningdossier.cooperation.my-account.settings.form.reset-file.are-you-sure')';
         $('#reset-account').click(function () {
             if (confirm(areYouSure)) {
-
+                $(this).submit();
             } else {
+                return false;
                 event.preventDefault();
             }
         })

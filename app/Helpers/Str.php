c002eeb578eefe4c9756cd1a846e61d2df7c5b63
pvankouteren <pvankouteren@gmail.com>
2018-09-11 08:16:08 +0200

    Updated UUID generation

--- app/Helpers/Str.php
+++ app/Helpers/Str.php
@@ -15,12 +15,30 @@ class Str
     public static function uuid()
     {
         $laravel = app();
-        $laravel::VERSION;
         if (version_compare($laravel::VERSION, '5.6.0') < 0) {
-            // use
-            return (string) Uuid::uuid4();
+        	try {
+		        return (string) Uuid::uuid4();
+	        }
+	        catch(\Exception $e){
+        		return (string) self::randomUuid();
+	        }
         } else {
             return (string) \Illuminate\Support\Str::uuid();
         }
     }
+
+	/**
+	 * Returns a random UUID. Only used as a fallback in case all other methods
+	 * don't work.
+	 * @return string
+	 */
+	protected static function randomUuid(){
+		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0x0fff ) | 0x4000,
+			mt_rand( 0, 0x3fff ) | 0x8000,
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+		);
+	}
 }

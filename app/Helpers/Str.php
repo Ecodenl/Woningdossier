e89cb8fe7cb5a253094ee3019e1e1d621638312b
pvankouteren <pvankouteren@gmail.com>
2018-09-11 08:42:42 +0200

    Progressing

--- app/Helpers/Str.php
+++ app/Helpers/Str.php
@@ -16,29 +16,30 @@ class Str
     {
         $laravel = app();
         if (version_compare($laravel::VERSION, '5.6.0') < 0) {
-        	try {
-		        return (string) Uuid::uuid4();
-	        }
-	        catch(\Exception $e){
-        		return (string) self::randomUuid();
-	        }
+            try {
+                return (string) Uuid::uuid4();
+            } catch (\Exception $e) {
+                return (string) self::randomUuid();
+            }
         } else {
             return (string) \Illuminate\Support\Str::uuid();
         }
     }
 
-	/**
-	 * Returns a random UUID. Only used as a fallback in case all other methods
-	 * don't work.
-	 * @return string
-	 */
-	protected static function randomUuid(){
-		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-			mt_rand( 0, 0xffff ),
-			mt_rand( 0, 0x0fff ) | 0x4000,
-			mt_rand( 0, 0x3fff ) | 0x8000,
-			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-		);
-	}
+    /**
+     * Returns a random UUID. Only used as a fallback in case all other methods
+     * don't work.
+     *
+     * @return string
+     */
+    protected static function randomUuid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
 }

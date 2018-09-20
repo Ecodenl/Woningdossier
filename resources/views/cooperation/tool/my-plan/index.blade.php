2ddeb905c1ce0ee794fb2bbf9c9536d8eca83325
Bodhi <bodhilooij1998@gmail.com>
2018-09-20 11:20:50 +0200

    energy_saving checkboxes are checked if the planned column = 1 maintenance checkboxes are checked if the user has interest in the step and the planned column = 1

--- resources/views/cooperation/tool/my-plan/index.blade.php
+++ resources/views/cooperation/tool/my-plan/index.blade.php
@@ -47,9 +47,9 @@
 
                         <td>
                             @if($measureType == "energy_saving")
-                                <input class="interested-checker" name="advice[{{ $advice->id }}][{{$stepSlug}}][interested]" value="1" type="checkbox" id="advice-{{$advice->id}}-planned" @if(\App\Helpers\StepHelper::hasInterestInStep($step) && $advice->planned) checked @endif />
+                                <input class="interested-checker" name="advice[{{ $advice->id }}][{{$stepSlug}}][interested]" value="1" type="checkbox" id="advice-{{$advice->id}}-planned" @if($advice->planned == 1) checked @endif />
                             @else
-                                <input class="interested-checker" name="advice[{{ $advice->id }}][{{$stepSlug}}][interested]" value="1" type="checkbox" id="advice-{{$advice->id}}-planned" @if($advice->planned == 1) checked @elseif(\App\Helpers\StepHelper::hasInterestInStep($step) || $advice->planned) checked @endif />
+                                <input class="interested-checker" name="advice[{{ $advice->id }}][{{$stepSlug}}][interested]" value="1" type="checkbox" id="advice-{{$advice->id}}-planned" @if(\App\Helpers\StepHelper::hasInterestInStep($step) && $advice->planned) checked @endif />
                             @endif
                         </td>
                         <td>

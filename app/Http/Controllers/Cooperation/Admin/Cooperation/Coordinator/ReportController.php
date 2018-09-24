0be64d8bde5256ad44a0e84f1a3913ddc721b990
Bodhi <bodhilooij1998@gmail.com>
2018-09-24 16:50:30 +0200

    Moved the report controller and routes Renamed some translations Only add it to the report if the user had a building

--- app/Http/Controllers/Cooperation/Admin/Cooperation/Coordinator/ReportController.php
+++ app/Http/Controllers/Cooperation/Admin/Cooperation/Coordinator/ReportController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Cooperation\Admin\Cooperation;
+namespace App\Http\Controllers\Cooperation\Admin\Cooperation\Coordinator;
 
 use App\Http\Controllers\Controller;
+use App\Models\Building;
 use App\Models\MeasureApplication;
 use App\Services\CsvExportService;
 use Carbon\Carbon;
@@ -25,16 +26,17 @@ class ReportController extends Controller
 
         // set the csv headers
         $csvHeaders = [
-            __('woningdossier.cooperation.admin.reports.csv-columns.first-name'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.last-name'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.email'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.phonenumber'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.mobilenumber'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.street'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.house-number'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.city'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.zip-code'),
-            __('woningdossier.cooperation.admin.reports.csv-columns.country-code'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.first-name'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.first-name'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.last-name'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.email'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.phonenumber'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.mobilenumber'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.street'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.house-number'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.city'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.zip-code'),
+            __('woningdossier.cooperation.admin.cooperation.coordinator.reports.csv-columns.country-code'),
         ];
 
         // put the measures inside the header array
@@ -49,42 +51,45 @@ class ReportController extends Controller
 
         foreach ($users as $key => $user) {
             $building = $user->buildings()->first();
-            $street = $building->street;
-            $number = $building->number;
-            $city = $building->city;
-            $postalCode = $building->postal_code;
-            $countryCode = $building->country_code;
+            if ($building instanceof Building) {
+                $street = $building->street;
+                $number = $building->number;
+                $city = $building->city;
+                $postalCode = $building->postal_code;
+                $countryCode = $building->country_code;
 
-            $firstName = $user->first_name;
-            $lastName = $user->last_name;
-            $email = $user->email;
-            $phoneNumber = $user->phone_number;
-            $mobileNumber = $user->mobile;
 
-            // set the personal userinfo
-            $row[$key] = [$firstName, $lastName, $email, $phoneNumber, $mobileNumber, $street, $number, $city, $postalCode, $countryCode];
+                $firstName = $user->first_name;
+                $lastName = $user->last_name;
+                $email = $user->email;
+                $phoneNumber = $user->phone_number;
+                $mobileNumber = $user->mobile;
 
-            // set all the years in range
-            for ($startYear = $thisYear; $startYear <= ($thisYear + 100); ++$startYear) {
-                $row[$key][$startYear] = '';
-            }
+                // set the personal userinfo
+                $row[$key] = [$firstName, $lastName, $email, $phoneNumber, $mobileNumber, $street, $number, $city, $postalCode, $countryCode];
 
-            // get the user measures / advices
-            foreach ($user->actionPlanAdvices as $actionPlanAdvice) {
-                $plannedYear = null == $actionPlanAdvice->planned_year ? $actionPlanAdvice->year : $actionPlanAdvice->planned_year;
-                $measureName = $actionPlanAdvice->measureApplication->measure_name;
-
-                if (is_null($plannedYear)) {
-                    $plannedYear = $actionPlanAdvice->getAdviceYear();
+                // set all the years in range
+                for ($startYear = $thisYear; $startYear <= ($thisYear + 100); ++$startYear) {
+                    $row[$key][$startYear] = '';
                 }
 
-                // create a new array with the measures for the user connected to the planned year
-                $allUserMeasures[$plannedYear][] = $measureName;
-            }
+                // get the user measures / advices
+                foreach ($user->actionPlanAdvices as $actionPlanAdvice) {
+                    $plannedYear = null == $actionPlanAdvice->planned_year ? $actionPlanAdvice->year : $actionPlanAdvice->planned_year;
+                    $measureName = $actionPlanAdvice->measureApplication->measure_name;
 
-            // loop through the user measures and add them to the row
-            foreach ($allUserMeasures as $year => $userMeasures) {
-                $row[$key][$year] = implode(', ', $userMeasures);
+                    if (is_null($plannedYear)) {
+                        $plannedYear = $actionPlanAdvice->getAdviceYear();
+                    }
+
+                    // create a new array with the measures for the user connected to the planned year
+                    $allUserMeasures[$plannedYear][] = $measureName;
+                }
+
+                // loop through the user measures and add them to the row
+                foreach ($allUserMeasures as $year => $userMeasures) {
+                    $row[$key][$year] = implode(', ', $userMeasures);
+                }
             }
 
             $rows = $row;
--- resources/views/cooperation/admin/cooperation/cooperation-admin/reports/index.blade.php
+++ resources/views/cooperation/admin/cooperation/cooperation-admin/reports/index.blade.php
@@ -5,23 +5,23 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">@lang('woningdossier.cooperation.admin.cooperation.cooperation-admin.reports.title')</div>
+                    <div class="panel-heading">@lang('woningdossier.cooperation.admin.cooperation.coordinator.reports.title')</div>
 
                     <div class="panel-body">
 
                         <div class="row">
                             <div class="col-sm-12">
-                                <h2>@lang('woningdossier.cooperation.admin.cooperation.cooperation-admin.reports.description')</h2>
+                                <h2>@lang('woningdossier.cooperation.admin.cooperation.coordinator.reports.description')</h2>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-sm-12">
                                 <ul>
                                     <li>
-                                        <a href="{{ route('cooperation.admin.cooperation.cooperation-admin.reports.download.by-year') }}">@lang('woningdossier.cooperation.admin.cooperation.cooperation-admin.reports.download.by-year') (CSV)</a>
+                                        <a href="{{ route('cooperation.admin.cooperation.coordinator.reports.download.by-year') }}">@lang('woningdossier.cooperation.admin.cooperation.coordinator.reports.download.by-year') (CSV)</a>
                                     </li>
                                     <li>
-                                        <a href="{{ route('cooperation.admin.cooperation.cooperation-admin.reports.download.by-measure') }}">@lang('woningdossier.cooperation.admin.cooperation.cooperation-admin.reports.download.by-measure') (CSV)</a>
+                                        <a href="{{ route('cooperation.admin.cooperation.coordinator.reports.download.by-measure') }}">@lang('woningdossier.cooperation.admin.cooperation.coordinator.reports.download.by-measure') (CSV)</a>
                                     </li>
                                 </ul>
                             </div>

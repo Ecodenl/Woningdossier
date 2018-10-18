215fe0c2995b3b5c163f62e466df190c15172661
Bodhi <bodhilooij1998@gmail.com>
2018-10-18 16:52:30 +0200

    auth check

--- resources/views/cooperation/layouts/navbar.blade.php
+++ resources/views/cooperation/layouts/navbar.blade.php
@@ -49,7 +49,9 @@
                     </ul>
                 </li>
             </ul>
-            @if (Auth::user()->buildings->first()->id != \App\Helpers\HoomdossierSession::getBuilding())
+
+
+            @if (Auth::check() && Auth::user()->buildings()->first()->id != \App\Helpers\HoomdossierSession::getBuilding())
                 <a href="{{route('cooperation.admin.index')}}" class="btn btn-warning navbar-btn">Stop sessie</a>
             @endif
         @endif

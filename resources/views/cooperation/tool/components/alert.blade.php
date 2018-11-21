06574770f6e203e6f5e950004164e935ffb2b5f4
pvankouteren <pvankouteren@gmail.com>
2018-11-21 15:39:52 +0100

    Removed class

--- resources/views/cooperation/tool/components/alert.blade.php
+++ resources/views/cooperation/tool/components/alert.blade.php
@@ -4,7 +4,7 @@
     $hide = (isset($hide) && $hide) ? 'hide' : '';
 ?>
 
-<div class="alert alert-{{ $alertType }} @if($dismissible)alert-dismissible @endif {{ $hide }} show" role="alert">
+<div class="alert alert-{{ $alertType }} @if($dismissible)alert-dismissible @endif {{ $hide }}" role="alert">
     @if($dismissible)
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>

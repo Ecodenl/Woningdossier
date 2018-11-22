971d520bb2310d917a38e14cc094375ec17c9270
Bodhi <bodhilooij1998@gmail.com>
2018-11-22 16:28:51 +0100

    More hiding and showing on different cases

--- resources/views/cooperation/tool/wall-insulation/index.blade.php
+++ resources/views/cooperation/tool/wall-insulation/index.blade.php
@@ -59,7 +59,7 @@
                 </div>
             </div>
         </div>
-        <div id="hideable">
+        <div class="hideable">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="form-group add-space{{ $errors->has('cavity_wall') ? ' has-error' : '' }}">
@@ -89,91 +89,90 @@
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="form-group add-space{{ $errors->has('facade_plastered_painted') ? ' has-error' : '' }}">
 
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="form-group add-space{{ $errors->has('facade_plastered_painted') ? ' has-error' : '' }}">
+                    <label for="facade_plastered_painted" class=" control-label"><i data-toggle="collapse" data-target="#wall-painted" class="glyphicon glyphicon-info-sign glyphicon-padding"></i>@lang('woningdossier.cooperation.tool.wall-insulation.intro.is-facade-plastered-painted')</label> <span> *</span>
 
-                        <label for="facade_plastered_painted" class=" control-label"><i data-toggle="collapse" data-target="#wall-painted" class="glyphicon glyphicon-info-sign glyphicon-padding"></i>@lang('woningdossier.cooperation.tool.wall-insulation.intro.is-facade-plastered-painted')</label> <span> *</span>
+                    <label class="radio-inline">
+                        <input class="is-painted" @if(old('facade_plastered_painted') == "1") checked @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_painted == "1") checked @endif type="radio" name="facade_plastered_painted" value="1">@lang('woningdossier.cooperation.radiobutton.yes')
+                    </label>
+                    <label class="radio-inline">
+                        <input @if(old('facade_plastered_painted') == "2") checked @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_painted == "2") checked @endif type="radio" name="facade_plastered_painted" value="2">@lang('woningdossier.cooperation.radiobutton.no')
+                    </label>
+                    <label class="radio-inline">
+                        <input class="is-painted" @if(old('facade_plastered_painted') == "3") checked @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_painted == "3") checked @endif type="radio" name="facade_plastered_painted" value="3">@lang('woningdossier.cooperation.radiobutton.mostly')
+                    </label>
+                    <br>
 
-                        <label class="radio-inline">
-                            <input class="is-painted" @if(old('facade_plastered_painted') == "1") checked @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_painted == "1") checked @endif type="radio" name="facade_plastered_painted" value="1">@lang('woningdossier.cooperation.radiobutton.yes')
-                        </label>
-                        <label class="radio-inline">
-                            <input @if(old('facade_plastered_painted') == "2") checked @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_painted == "2") checked @endif type="radio" name="facade_plastered_painted" value="2">@lang('woningdossier.cooperation.radiobutton.no')
-                        </label>
-                        <label class="radio-inline">
-                            <input class="is-painted" @if(old('facade_plastered_painted') == "3") checked @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_painted == "3") checked @endif type="radio" name="facade_plastered_painted" value="3">@lang('woningdossier.cooperation.radiobutton.mostly')
-                        </label>
-                        <br>
-
-                        <div id="wall-painted" class="collapse alert alert-info remove-collapse-space alert-top-space">
-                            I would like to have some helpful information right here!
-                        </div>
-
-                        @if ($errors->has('facade_plastered_painted'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('facade_plastered_painted') }}</strong>
-                            </span>
-                        @endif
-
-
+                    <div id="wall-painted" class="collapse alert alert-info remove-collapse-space alert-top-space">
+                        I would like to have some helpful information right here!
                     </div>
+
+                    @if ($errors->has('facade_plastered_painted'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('facade_plastered_painted') }}</strong>
+                        </span>
+                    @endif
+
 
                 </div>
-
-                <div id="painted-options" style="display: none;">
-                    <div class="col-sm-6">
-                        <div class="form-group add-space{{ $errors->has('facade_plastered_surface_id') ? ' has-error' : '' }}">
-                            <label for="facade_plastered_surface_id" class=" control-label"><i data-toggle="collapse" data-target="#facade-painted-surface" class="glyphicon glyphicon-info-sign glyphicon-padding"></i>@lang('woningdossier.cooperation.tool.wall-insulation.intro.surface-paintwork') </label>
-
-                            <select id="facade_plastered_surface_id" class="form-control" name="facade_plastered_surface_id">
-                                @foreach($facadePlasteredSurfaces as $facadePlasteredSurface)
-                                    <option @if(old('facade_plastered_surface_id') == $facadePlasteredSurface->id) selected @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_surface_id == $facadePlasteredSurface->id ) selected @endif value="{{ $facadePlasteredSurface->id }}">{{ $facadePlasteredSurface->name }}</option>
-                                @endforeach
-                            </select>
-
-                            <div id="facade-painted-surface" class="collapse alert alert-info remove-collapse-space alert-top-space">
-                                And i would like to have it to...
-                            </div>
-
-                        </div>
-
-                        @if ($errors->has('facade_plastered_surface_id'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('facade_plastered_surface_id') }}</strong>
-                            </span>
-                        @endif
-
-                    </div>
-
-                    <div class="col-sm-6">
-                        <div class="form-group add-space{{ $errors->has('facade_damaged_paintwork_id') ? ' has-error' : '' }}">
-                            <label for="facade_damaged_paintwork_id" class=" control-label"><i data-toggle="collapse" data-target="#damage-paintwork-surface" class="glyphicon glyphicon-info-sign glyphicon-padding"></i>@lang('woningdossier.cooperation.tool.wall-insulation.intro.damage-paintwork')</label>
-
-                            <select id="facade_damaged_paintwork_id" class="form-control" name="facade_damaged_paintwork_id">
-                                @foreach($facadeDamages as $facadeDamage)
-                                    <option @if(old('facade_damaged_paintwork_id') == $facadeDamage->id) selected @elseif(isset($buildingFeature) && $buildingFeature->facade_damaged_paintwork_id == $facadeDamage->id ) selected  @endif value="{{ $facadeDamage->id }}">{{ $facadeDamage->name }}</option>
-                                @endforeach
-                            </select>
-
-                            <div id="damage-paintwork-surface" class="collapse alert alert-info remove-collapse-space alert-top-space">
-                                And i would like to have it to...
-                            </div>
-
-                        </div>
-
-                        @if ($errors->has('facade_damaged_paintwork_id'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('facade_damaged_paintwork_id') }}</strong>
-                            </span>
-                        @endif
-
-                    </div>
-                </div>
-
             </div>
+        </div>
 
+        <div class="row">
+            <div id="painted-options" style="display: none;">
+                <div class="col-sm-6">
+                    <div class="form-group add-space{{ $errors->has('facade_plastered_surface_id') ? ' has-error' : '' }}">
+                        <label for="facade_plastered_surface_id" class=" control-label"><i data-toggle="collapse" data-target="#facade-painted-surface" class="glyphicon glyphicon-info-sign glyphicon-padding"></i>@lang('woningdossier.cooperation.tool.wall-insulation.intro.surface-paintwork') </label>
+
+                        <select id="facade_plastered_surface_id" class="form-control" name="facade_plastered_surface_id">
+                            @foreach($facadePlasteredSurfaces as $facadePlasteredSurface)
+                                <option @if(old('facade_plastered_surface_id') == $facadePlasteredSurface->id) selected @elseif(isset($buildingFeature) && $buildingFeature->facade_plastered_surface_id == $facadePlasteredSurface->id ) selected @endif value="{{ $facadePlasteredSurface->id }}">{{ $facadePlasteredSurface->name }}</option>
+                            @endforeach
+                        </select>
+
+                        <div id="facade-painted-surface" class="collapse alert alert-info remove-collapse-space alert-top-space">
+                            And i would like to have it to...
+                        </div>
+
+                    </div>
+
+                    @if ($errors->has('facade_plastered_surface_id'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('facade_plastered_surface_id') }}</strong>
+                        </span>
+                    @endif
+
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="form-group add-space{{ $errors->has('facade_damaged_paintwork_id') ? ' has-error' : '' }}">
+                        <label for="facade_damaged_paintwork_id" class=" control-label"><i data-toggle="collapse" data-target="#damage-paintwork-surface" class="glyphicon glyphicon-info-sign glyphicon-padding"></i>@lang('woningdossier.cooperation.tool.wall-insulation.intro.damage-paintwork')</label>
+
+                        <select id="facade_damaged_paintwork_id" class="form-control" name="facade_damaged_paintwork_id">
+                            @foreach($facadeDamages as $facadeDamage)
+                                <option @if(old('facade_damaged_paintwork_id') == $facadeDamage->id) selected @elseif(isset($buildingFeature) && $buildingFeature->facade_damaged_paintwork_id == $facadeDamage->id ) selected  @endif value="{{ $facadeDamage->id }}">{{ $facadeDamage->name }}</option>
+                            @endforeach
+                        </select>
+
+                        <div id="damage-paintwork-surface" class="collapse alert alert-info remove-collapse-space alert-top-space">
+                            And i would like to have it to...
+                        </div>
+
+                    </div>
+
+                    @if ($errors->has('facade_damaged_paintwork_id'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('facade_damaged_paintwork_id') }}</strong>
+                        </span>
+                    @endif
+
+                </div>
+            </div>
+        </div>
 
             <div id="options">
                 <hr>
@@ -228,6 +227,7 @@
 
                 </div>
 
+            <div class="hideable">
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="form-group add-space @if ($errors->has('wall_surface')) has-error @endif">
@@ -273,9 +273,9 @@
                             @endif
                         </div>
                     </div>
-
                 </div>
             </div>
+
             <div class="row" id="advice-help">
                 <div class="col-sm-12 col-md-8 col-md-offset-2">
                     <div class="alert alert-info" role="alert">
@@ -294,7 +294,7 @@
 
 
 
-        </div>
+
 
         <div id="indication-for-costs">
             <hr>
@@ -469,10 +469,10 @@
                var elementCalculateValue = $('#element_{{$buildingElements->id}} option:selected').data('calculate-value');
 
                if ((elementCalculateValue == 3 || elementCalculateValue == 4) && interestedCalculateValue <= 2) {
-                   $('#hideable').hide();
+                   $('.hideable').hide();
                    $('#wall-insulation-info-alert').find('.alert').removeClass('hide')
                } else {
-                   $('#hideable').show();
+                   $('.hideable').show();
                    $('#wall-insulation-info-alert').find('.alert').addClass('hide')
                }
 

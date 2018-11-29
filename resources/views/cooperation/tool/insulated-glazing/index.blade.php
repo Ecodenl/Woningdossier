214b1d1c6f9a2a31b8b9d7c7ec0bbfa0aad83d69
pvankouteren <pvankouteren@gmail.com>
2018-11-29 14:01:57 +0100

    Updated translation string

--- resources/views/cooperation/tool/insulated-glazing/index.blade.php
+++ resources/views/cooperation/tool/insulated-glazing/index.blade.php
@@ -537,7 +537,7 @@
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group add-space">
-                        <label class="control-label">{{\App\Helpers\Translation::translate('general.costs.comparable-rate.title')}}</label>
+                        <label class="control-label">{{\App\Helpers\Translation::translate('general.costs.comparable-rent.title')}}</label>
                         <div class="input-group">
                             <span class="input-group-addon">% / {{\App\Helpers\Translation::translate('general.unit.year.title')}}</span>
                             <input type="text" id="interest_comparable" class="form-control disabled" disabled="" value="0,0">

ac707ac95cf3ca34ed85141fcb6383b13d974d5c
Bodhi <bodhilooij1998@gmail.com>
2018-12-04 11:23:18 +0100

    Added migrations and models for the questionaires

---
+++ database/migrations/2018_12_04_110252_create_questions_answers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions_answers', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('question_id')->unsigned();
+            $table->foreign('question_id')->references('id')->on('questionnaires')->onDelete('cascade');
+
+            $table->integer('building_id')->unsigned()->nullable();
+            $table->foreign('building_id')->references('id')->on('buildings')->onDelete('set null');
+
+            // json
+            $table->longText('answers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions_answers');
+    }
+}
---
+++ database/migrations/2018_12_04_111727_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('type');
+
+            $table->integer('question_id')->unsigned();
+            $table->foreign('question_id')->references('id')->on('questionnaires')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}

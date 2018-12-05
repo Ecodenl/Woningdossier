7cff9a1c92e9cc05e4a87f25cea4cb24ca5bcecb
pvankouteren <pvankouteren@gmail.com>
2018-12-05 13:08:10 +0100

    Wall insulation hide alerts when interested, but insulation already present

--- resources/views/cooperation/tool/wall-insulation/index.blade.php
+++ resources/views/cooperation/tool/wall-insulation/index.blade.php
@@ -280,22 +280,23 @@
                 </div>
             </div>
 
-            <div class="row" id="advice-help">
-                <div class="col-sm-12 col-md-8 col-md-offset-2">
-                    <div class="alert alert-info" role="alert">
-                        <p>@lang('woningdossier.cooperation.tool.wall-insulation.insulation-advice.text')</p>
-                        <p id="insulation-advice"></p>
+            <div class="hideable">
+                <div class="row" id="advice-help">
+                    <div class="col-sm-12 col-md-8 col-md-offset-2">
+                        <div class="alert alert-info" role="alert">
+                            <p>@lang('woningdossier.cooperation.tool.wall-insulation.insulation-advice.text')</p>
+                            <p id="insulation-advice"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" id="cavity-wall-alert" style="display: none;">
+                    <div class="col-sm-12 col-md-8 col-md-offset-2">
+                        <div class="alert alert-warning" role="alert">
+                            <b><p>@lang('woningdossier.cooperation.tool.wall-insulation.alert.description')</p></b>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="row" id="cavity-wall-alert" style="display: none;">
-                <div class="col-sm-12 col-md-8 col-md-offset-2">
-                    <div class="alert alert-warning" role="alert">
-                        <b><p>@lang('woningdossier.cooperation.tool.wall-insulation.alert.description')</p></b>
-                    </div>
-                </div>
-            </div>
-
 
 
 
@@ -472,14 +473,15 @@
 
                if ((elementCalculateValue == 3 || elementCalculateValue == 4) && interestedCalculateValue <= 2) {
                    $('.hideable').hide();
-                   $('#wall-insulation-info-alert').find('.alert').removeClass('hide')
+                   $('#wall-insulation-info-alert').find('.alert').removeClass('hide');
                } else {
                    $('.hideable').show();
-                   $('#wall-insulation-info-alert').find('.alert').addClass('hide')
+                   $('#wall-insulation-info-alert').find('.alert').addClass('hide');
+
                }
 
                var form = $(this).closest("form").serialize();
-              $.ajax({
+               $.ajax({
                   type: "POST",
                   url: '{{ route('cooperation.tool.wall-insulation.calculate', [ 'cooperation' => $cooperation ]) }}',
                   data: form,
@@ -563,8 +565,9 @@
                         console.log(data);
                       @endif
                   }
-              })
+               });
             });
+
             // Trigger the change event so it will load the data
             $('form').find('*').filter(':input:visible:first').trigger('change');
 

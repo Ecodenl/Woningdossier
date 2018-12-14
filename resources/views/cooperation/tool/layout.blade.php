627f9d8ff710dd77ada9dd7de3132f5c28a0ee84
Bodhi <bodhilooij1998@gmail.com>
2018-12-14 13:05:36 +0100

    the top right submit button in custom questionnaire will now submit !

--- resources/views/cooperation/tool/layout.blade.php
+++ resources/views/cooperation/tool/layout.blade.php
@@ -20,72 +20,20 @@
         <div class="row">
             <div class="col-md-12">
                 @if(isset($currentStep) && $currentStep->hasQuestionnaires())
-                <ul class="nav nav-tabs">
-                    <li class="active">
-                        <a href="#main-tab" data-toggle="tab">{{$currentStep->name}}</a></li>
-                    @foreach($currentStep->questionnaires as $questionnaire)
-                        @if($questionnaire->isActive())
-                            <li><a href="#questionnaire-{{$questionnaire->id}}" data-toggle="tab">{{$questionnaire->name}}</a></li>
-                        @endif
-                    @endforeach
-                </ul>
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a href="#main-tab" data-toggle="tab">{{$currentStep->name}}</a></li>
+                        @foreach($currentStep->questionnaires as $questionnaire)
+                            @if($questionnaire->isActive())
+                                <li><a href="#questionnaire-{{$questionnaire->id}}" data-toggle="tab">{{$questionnaire->name}}</a></li>
+                            @endif
+                        @endforeach
+                    </ul>
                 @endif
 
                 <div class="tab-content">
-                    @if(isset($currentStep) && $currentStep->hasQuestionnaires())
-                        @foreach($currentStep->questionnaires as $questionnaire)
-                            @if($questionnaire->isActive())
-                            <div class="panel tab-pane panel-default" id="questionnaire-{{$questionnaire->id}}">
-                                <div class="panel-heading">
-                                    <h3>
-                                        {{$questionnaire->name}}
-                                    </h3>
 
-                                    <button id="submit-form-top-right" disabled class="pull-right btn btn-primary">
-                                        @lang('default.buttons.next')
-                                    </button>
-                                    <div class="clearfix"></div>
-                                </div>
-
-                                <div class="panel-body">
-                                    <form action="{{route('cooperation.tool.questionnaire.store')}}" method="post">
-                                        {{csrf_field()}}
-                                        @foreach($questionnaire->questions()->orderBy('order', 'ASC')->get() as $question)
-                                            @switch($question->type)
-                                                @case('text')
-                                                    @include('cooperation.tool.questionnaires.text', ['question' => $question])
-                                                    @break
-                                                @case('textarea')
-                                                    @include('cooperation.tool.questionnaires.textarea', ['question' => $question])
-                                                    @break
-                                                @case('select')
-                                                    @include('cooperation.tool.questionnaires.select', ['question' => $question])
-                                                    @break
-                                                @case('radio')
-                                                    @include('cooperation.tool.questionnaires.radio', ['question' => $question])
-                                                    @break
-                                                @case('checkbox')
-                                                    @include('cooperation.tool.questionnaires.checkbox', ['question' => $question])
-                                                    @break
-                                                @case('date')
-                                                    @include('cooperation.tool.questionnaires.date', ['question' => $question])
-                                                    @break
-                                            @endswitch
-                                        @endforeach
-                                        <div class="row">
-                                            <div class="col-sm-12">
-                                                <div class="form-group">
-                                                    <button type="submit" class="btn btn-primary">Opslaan</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            @endif
-                        @endforeach
-                    @endif
-
+                    @include('cooperation.layouts.custom-questionnaire')
                     <div class="panel tab-pane active tab-pane panel-default" id="main-tab">
                         <div class="panel-heading">
                             <h3>

48ecbb0448bc74661900bce8a94aaa7b879c82e8
pvankouteren <pvankouteren@gmail.com>
2018-12-18 16:02:48 +0100

    changed order and sorting

--- resources/views/cooperation/tool/components/radio.blade.php
+++ resources/views/cooperation/tool/components/radio.blade.php
@@ -1,6 +1,15 @@
+<?php
+// sort the incoming collection based on input source order
+if (is_array($userInputValues)){
+	$userInputValues = collect($userInputValues);
+}
+$userInputValues = $userInputValues->sortBy(function($a){
+	return $a->inputSource->order;
+});
+?>
 @if(is_array($inputValues))
-    @foreach($inputValues as $inputValue)
-        @foreach($userInputValues as $userInputValue)
+    @foreach($userInputValues as $userInputValue)
+        @foreach($inputValues as $inputValue)
             <?php
                 // simple check if the user input column has dots, if it does it means we have to get a array from the row so we use the array_get method
                 if (strpos($userInputColumn, ".") !== false) {

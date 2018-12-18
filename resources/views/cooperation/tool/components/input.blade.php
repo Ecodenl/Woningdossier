fb5091eac5d1c94b5cb0716b890f90eae54d1ee1
pvankouteren <pvankouteren@gmail.com>
2018-12-18 15:48:58 +0100

    HR boiler done

--- resources/views/cooperation/tool/components/input.blade.php
+++ resources/views/cooperation/tool/components/input.blade.php
@@ -1,3 +1,12 @@
+<?php
+// sort the incoming collection based on input source order
+if (is_array($userInputValues)){
+	$userInputValues = collect($userInputValues);
+}
+$userInputValues = $userInputValues->sortBy(function($a){
+	return $a->inputSource->order;
+});
+?>
 @foreach($userInputValues as $userInputValue)
     <?php
     // simple check if the user input column has dots, if it does it means we have to get a array from the row so we use the array_get method

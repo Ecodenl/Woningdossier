dcf09e9dcbbacebd929adbac6d7bf2d509ad2a21
Bodhi <bodhilooij1998@gmail.com>
2019-01-02 16:54:02 +0100

    added nav tabs, target blanks and links, multiple tabs.

--- public/js/app.js
+++ public/js/app.js
@@ -68,7 +68,10 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(1);
-module.exports = __webpack_require__(8);
+__webpack_require__(8);
+__webpack_require__(9);
+__webpack_require__(10);
+module.exports = __webpack_require__(11);
 
 
 /***/ }),
@@ -30111,6 +30114,24 @@ if (typeof jQuery === 'undefined') {
 
 /***/ }),
 /* 8 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 11 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
--- resources/views/cooperation/home.blade.php
+++ resources/views/cooperation/home.blade.php
@@ -4,33 +4,85 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">@lang('woningdossier.cooperation.home.disclaimer.panel-title')</div>
+            <ul class="nav nav-tabs">
+                <li class="active">
+                    <a href="#start" data-toggle="start">@lang('woningdossier.cooperation.home.disclaimer.tabs.start')</a>
+                </li>
+                <li>
+                    <a href="https://form.jotformeu.com/81345355694363" target="_blank" data-toggle="disclaimer">@lang('woningdossier.cooperation.home.disclaimer.tabs.bugreport')</a>
+                </li>
+                <li>
+                    <a href="{{route('cooperation.my-account.messages.index', ['cooperation' => $cooperation])}}" target="_blank">
+                        @lang('woningdossier.cooperation.home.disclaimer.tabs.messages')
+                        <span class="badge">{{$myUnreadMessages->count()}}</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#privacy" data-toggle="privacy">
+                        @lang('woningdossier.cooperation.home.disclaimer.tabs.privacy')
+                    </a>
+                </li>
+                <li>
+                    <a href="#disclaimer" data-toggle="disclaimer">
+                        @lang('woningdossier.cooperation.home.disclaimer.tabs.disclaimer')
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('cooperation.my-account.settings.index')}}" target="_blank">
+                        @lang('woningdossier.cooperation.home.disclaimer.tabs.settings')
+                    </a>
+                </li>
+            </ul>
+            <div class="tab-content">
 
-                <div class="panel-body">
-
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                <div class="panel tab-pane active tab-pane panel-default" id="start">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                @lang('woningdossier.cooperation.home.disclaimer.description')
+                            </div>
                         </div>
-                    @endif
-
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <a href="{{route('cooperation.conversation-requests.index', ['cooperation' => $cooperation, 'action' => \App\Models\PrivateMessage::REQUEST_TYPE_COACH_CONVERSATION])}}" class="btn btn-primary">@lang('woningdossier.cooperation.help.help.help-with-filling-tool')</a>
-                            <a href="{{route('cooperation.tool.general-data.index', ['cooperation' => $cooperation])}}" class="btn btn-primary">@lang('woningdossier.cooperation.help.help.no-help-with-filling-tool')</a>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a href="{{route('cooperation.conversation-requests.index', ['cooperation' => $cooperation, 'action' => \App\Models\PrivateMessage::REQUEST_TYPE_COACH_CONVERSATION])}}" class="btn btn-primary">@lang('woningdossier.cooperation.help.help.help-with-filling-tool')</a>
+                                <a href="{{route('cooperation.tool.general-data.index', ['cooperation' => $cooperation])}}" class="btn btn-primary">@lang('woningdossier.cooperation.help.help.no-help-with-filling-tool')</a>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a class="btn btn-primary" href="{{ route('cooperation.tool.index', ['cooperation' => $cooperation]) }}">@lang('woningdossier.cooperation.tool.title') <i class="glyphicon glyphicon-chevron-right"></i></a>
+                            </div>
                         </div>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            @lang('woningdossier.cooperation.home.disclaimer.description')
+                </div>
+                <div class="panel tab-pane active tab-pane panel-default" id="disclaimer">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                @lang('woningdossier.cooperation.home.disclaimer.description')
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a class="btn btn-primary" href="{{ route('cooperation.tool.index', ['cooperation' => $cooperation]) }}">@lang('woningdossier.cooperation.tool.title') <i class="glyphicon glyphicon-chevron-right"></i></a>
+                            </div>
                         </div>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <a class="btn btn-primary" href="{{ route('cooperation.tool.index', ['cooperation' => $cooperation]) }}">@lang('woningdossier.cooperation.tool.title') <i class="glyphicon glyphicon-chevron-right"></i></a>
+                </div>
+                <div class="panel tab-pane active tab-pane panel-default" id="privacy">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                @lang('woningdossier.cooperation.home.disclaimer.description')
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a class="btn btn-primary" href="{{ route('cooperation.tool.index', ['cooperation' => $cooperation]) }}">@lang('woningdossier.cooperation.tool.title') <i class="glyphicon glyphicon-chevron-right"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -39,3 +91,11 @@
     </div>
 </div>
 @endsection
+
+@push('js')
+    <script>
+        $('ul.nav.nav-pills li a').click(function() {
+            $(this).parent().addClass('active').siblings().removeClass('active');
+        });
+    </script>
+@endpush

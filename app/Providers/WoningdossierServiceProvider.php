3082c8ec9897b7699b75b9cd4d7696b1626bf033
Bodhi <bodhilooij1998@gmail.com>
2019-01-04 10:28:20 +0100

    cleaner the woningdossier sevice provider from all the view composer stuff and moved those to a separate composer

--- app/Providers/WoningdossierServiceProvider.php
+++ app/Providers/WoningdossierServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Helpers\HoomdossierSession;
 use App\Http\ViewComposers\CooperationComposer;
+use App\Http\ViewComposers\ToolComposer;
 use App\Models\Building;
 use App\Models\BuildingCoachStatus;
 use App\Models\Cooperation;
@@ -27,30 +28,11 @@ class WoningdossierServiceProvider extends ServiceProvider
     public function boot()
     {
 	    PrivateMessage::observe(PrivateMessageObserver::class);
+        UserActionPlanAdvice::observe(UserActionPlanAdviceObserver::class);
 
-        \View::composer('cooperation.tool.progress', function ($view) {
-            $cooperation = Cooperation::find(HoomdossierSession::getCooperation());
-
-            // get the steps from a cooperation that are active and ordered on the order column from the pivot table
-            $steps = $cooperation->getActiveOrderedSteps();
-
-            $view->with('steps', $steps);
-        });
-
-        \View::composer('cooperation.tool.includes.interested', function ($view) {
-            $view->with('interests', Interest::orderBy('order')->get());
-        });
-
-	    \View::composer('cooperation.tool.*', function ($view) {
-		    $slug = str_replace(['tool', '/'], '', request()->getRequestUri());
-		    $step = Step::where('slug', $slug)->first();
-
-		    $view->with('currentStep', $step);
-	    });
-
+	    \View::creator('cooperation.tool.*', ToolComposer::class);
         \View::creator('*', CooperationComposer::class);
 
-        UserActionPlanAdvice::observe(UserActionPlanAdviceObserver::class);
     }
 
     /**

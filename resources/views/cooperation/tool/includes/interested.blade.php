10d053e6c8bb595f080c6b5b8528cc1063704903
Bodhi <bodhilooij1998@gmail.com>
2019-01-08 14:12:09 +0100

    get the interests from the buildingOwner instead of the auth::user
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- resources/views/cooperation/tool/includes/interested.blade.php
+++ resources/views/cooperation/tool/includes/interested.blade.php
@@ -9,6 +9,7 @@
             }
 
             $userInterestsForMe = \App\Models\UserInterest::forMe()->get();
+            $buildingOwner = \App\Models\User::find(\App\Models\Building::find(\App\Helpers\HoomdossierSession::getBuilding())->user_id);
         ?>
         <div class="row">
             <div class="col-sm-12">
@@ -21,7 +22,7 @@
                     ['inputType' => 'select', 'inputValues' => $interests, 'userInputValues' => $userInterestsForMe->where('interested_in_type', $type)->where('interested_in_id', $typeId),  'userInputColumn' => 'interest_id'])
                         <select class="form-control" id="interest_{{ $type }}_{{ $typeId }}" name="interest[{{ $type }}][{{ $typeId }}]">
                             @foreach($interests as $interest)
-                                <option @if($interest->id == old('user_interest.' . $type . '.'. $typeId, \App\Helpers\Hoomdossier::getMostCredibleValue(Auth::user()->interests()->where('interested_in_type', $type)->where('interested_in_id', $typeId), 'interest_id'))) selected="selected" @endif value="{{ $interest->id }}">{{ $interest->name }}</option>
+                                <option @if($interest->id == old('user_interest.' . $type . '.'. $typeId, \App\Helpers\Hoomdossier::getMostCredibleValue($buildingOwner->interests()->where('interested_in_type', $type)->where('interested_in_id', $typeId), 'interest_id'))) selected="selected" @endif value="{{ $interest->id }}">{{ $interest->name }}</option>
                                 {{--<option data-calculate-value="{{$interest->calculate_value}}" @if($interest->id == old('user_interest.'.$type.'.'. $typeId . '')) selected @elseif(Auth::user()->getInterestedType($type, $typeId) != null && Auth::user()->getInterestedType($type, $typeId)->interest_id == $interest->id) selected  @endif value="{{ $interest->id }}">{{ $interest->name }}</option>--}}
                             @endforeach
                         </select>

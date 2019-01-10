157ada853ae29095d2256fef958ca1d7c9beda81
pvankouteren <pvankouteren@gmail.com>
2019-01-10 12:04:51 +0100

    Apart from translation key..

--- resources/views/cooperation/tool/building-detail/index.blade.php
+++ resources/views/cooperation/tool/building-detail/index.blade.php
@@ -8,7 +8,7 @@
         {{ csrf_field() }}
         <div class="row">
             <div class="col-md-12">
-                <p>{{ \App\Helpers\Translation::translate('building-detail.intro') }}</p>
+                <p>{{ \App\Helpers\Translation::translate('building-detail.intro.title') }}</p>
             </div>
             <div class="col-md-6">
                 <div class="form-group add-space{{ $errors->has('building_type_id') ? ' has-error' : '' }}">
@@ -110,7 +110,7 @@
                 var bt_now = $("select#building_type_id").val();
 
                 if (bt_now !== previous_bt){
-                    if (previous_bt === "" || confirm('{{ \App\Helpers\Translation::translate('building-detail.warning') }}')) {
+                    if (previous_bt === "" || confirm('{{ \App\Helpers\Translation::translate('building-detail.warning.title') }}')) {
                         @if(App::environment('local'))
                         console.log("Building Type was changed, but it was empty or a wanted change. Proceed.");
                         @endif

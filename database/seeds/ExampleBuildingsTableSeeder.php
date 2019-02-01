efa43822b5f65142f892bafc04949ed755177b72
pvankouteren <pvankouteren@gmail.com>
2019-02-01 11:04:51 +0100

    Updated seeder with building types

--- database/seeds/ExampleBuildingsTableSeeder.php
+++ database/seeds/ExampleBuildingsTableSeeder.php
@@ -17,60 +17,70 @@ class ExampleBuildingsTableSeeder extends Seeder
                     'nl' => 'Vrijstaande woning',
                 ],
                 'order' => 100,
+	            'building_type_id' => 1,
             ],
             [
                 'names' => [
                     'nl' => '2 onder 1 kap',
                 ],
                 'order' => 101,
+	            'building_type_id' => 2,
             ],
             [
                 'names' => [
                     'nl' => 'Hoekwoning',
                 ],
                 'order' => 102,
+                'building_type_id' => 3,
             ],
             [
                 'names' => [
                     'nl' => 'Tussenwoning',
                 ],
                 'order' => 103,
+                'building_type_id' => 4,
             ],
 	        [
 		        'names' => [
 			        'nl' => 'Bovenwoning hoek',
 		        ],
 		        'order' => 104,
+		        'building_type_id' => 5,
 	        ],
 	        [
 		        'names' => [
 			        'nl' => 'Bovenwoning tussen',
 		        ],
 		        'order' => 105,
+		        'building_type_id' => 6,
 	        ],
             [
                 'names' => [
                     'nl' => 'Bovenwoning hoek',
                 ],
                 'order' => 106,
+                'building_type_id' => 7,
             ],
             [
                 'names' => [
                     'nl' => 'Bovenwoning tussen',
                 ],
                 'order' => 107,
+                'building_type_id' => 8,
             ],
             [
                 'names' => [
                     'nl' => 'Appartement tussen op een tussenverdieping',
                 ],
                 'order' => 108,
+                'building_type_id' => 9,
             ],
             [
                 'names' => [
                     'nl' => 'Appartement hoek op een tussenverdieping',
                 ],
                 'order' => 109,
+                'building_type_id' => 10,
             ],
         ];
 
@@ -87,6 +97,7 @@ class ExampleBuildingsTableSeeder extends Seeder
             \DB::table('example_buildings')->insert([
                 'name' => $uuid,
                 'order' => $exampleBuilding['order'],
+	            'building_type_id' => $exampleBuilding['building_type_id'],
             ]);
         }
     }

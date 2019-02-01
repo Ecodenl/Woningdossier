9e6cc03a469158c97930f31b425ba8e79fea0d9e
pvankouteren <pvankouteren@gmail.com>
2019-02-01 12:07:47 +0100

    Use most credible value, also react on build year

--- resources/views/cooperation/tool/building-detail/index.blade.php
+++ resources/views/cooperation/tool/building-detail/index.blade.php
@@ -28,9 +28,7 @@
                     ])
                         <select id="building_type_id" class="form-control" name="building_type_id">
                             @foreach($buildingTypes as $buildingType)
-                                <option @if(old('building_type_id') && $buildingType->id == old('building_type_id'))
-                                        selected="selected"
-                                        @elseif(isset($building->buildingFeatures->buildingType) && $building->buildingFeatures->buildingType->id == $buildingType->id)
+                                <option @if($buildingType->id == old('building_type_id', \App\Helpers\Hoomdossier::getMostCredibleValue($building->buildingFeatures(), 'building_type_id')))
                                         selected="selected"
                                         @endif value="{{ $buildingType->id }}">{{ $buildingType->name }}
                                 </option>
@@ -105,17 +103,19 @@
 
             <?php
                 $myFeatures = $building->buildingFeatures()->forMe()->first();
-                $prevBt = ($myFeatures instanceof \App\Models\BuildingFeature) ? $myFeatures->building_type_id : "";
+                $prevBt = \App\Helpers\Hoomdossier::getMostCredibleValue($building->buildingFeatures(), 'building_type_id') ?? "";
+                $prevBy = \App\Helpers\Hoomdossier::getMostCredibleValue($building->buildingFeatures(), 'build_year') ?? "";
             ?>
             var previous_bt = "{{ $prevBt }}";
-
+            var previous_by = "{{ $prevBy }}";
 
             $("form.form-horizontal").on('submit', function () {
                 // Store the current value on focus and on change
                 var bt_now = $("select#building_type_id").val();
+                var by_now = $("input#build_year").val();
 
-                if (bt_now !== previous_bt){
-                    if (previous_bt === "" || confirm('{{ \App\Helpers\Translation::translate('building-detail.warning.title') }}')) {
+                if (bt_now !== previous_bt || by_now !== previous_by){
+                    if (previous_bt === "" && previous_by === "" || confirm('{{ \App\Helpers\Translation::translate('building-detail.warning.title') }}')) {
                         @if(App::environment('local'))
                         console.log("Building Type was changed, but it was empty or a wanted change. Proceed.");
                         @endif

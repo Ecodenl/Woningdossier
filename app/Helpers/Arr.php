157a6457b85e440c97aa4c0ec67fe27d4e9151bb
Bodhi <bodhilooij1998@gmail.com>
2019-02-05 16:35:17 +0100

    removed a fix, that didnt fix but break a method.
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- app/Helpers/Arr.php
+++ app/Helpers/Arr.php
@@ -11,23 +11,11 @@ class Arr
      *
      * @return array
      */
-//    public static function arrayUndot($content)
-//    {
-//        $array = [];
-//        foreach ($content as $key => $value) {
-//            array_set($array, $key, $value);
-//        }
-//
-//        return $array;
-//    }
     public static function arrayUndot($content)
     {
         $array = [];
-
-        foreach ($content as $key => $values) {
-            foreach ($values as $dottedKey => $value) {
-                array_set($array, $key.'.'.$dottedKey, $value);
-            }
+        foreach ($content as $key => $value) {
+            array_set($array, $key, $value);
         }
 
         return $array;

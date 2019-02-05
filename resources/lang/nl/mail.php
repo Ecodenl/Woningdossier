bcc8440f76654c31c42acdf230dfd19b9423941b
pvankouteren <pvankouteren@gmail.com>
2019-02-05 20:21:05 +0100

    Translation updates

--- resources/lang/nl/mail.php
+++ resources/lang/nl/mail.php
@@ -3,16 +3,16 @@
 return [
     'confirm_account' => [
         'salutation' => 'Beste :first_name :last_name,',
-        'text' => 'Je hebt een account aangevraagd op :home_url. Je kunt je account bevestigen via de volgende link:<br><br>:confirm_url',
+        'text' => 'U heeft een account aangevraagd op :home_url. U kunt uq account bevestigen via de volgende link:<br><br>:confirm_url',
         'signature' => '<br>Met vriendelijke groet,<br>:app_name',
     ],
     'reset_password' => [
-        'why' => 'You are receiving this email because we received a password reset request for your account.',
-        'action' => 'Reset Password',
-        'not_requested' => 'If you did not request a password reset, no further action is required.',
+        'why' => 'U ontvangt deze mail omdat iemand een wachtwoord reset heeft aangevraagd voor uw account.',
+        'action' => 'Wachtwoord resetten',
+        'not_requested' => 'Als u geen wachtwoord reset heeft aangevraagd hoeft u geen actie te ondernemen.',
     ],
     'user-created' => [
-        'why' => 'U heeft deze e-mail ontvangen omdat er account voor uw is aangemaakt',
+        'why' => 'U heeft deze e-mail ontvangen omdat er account voor u is aangemaakt',
         'action' => 'Aan de slag',
         'signature' => '<br>Met vriendelijke groet,<br>:app_name',
     ],

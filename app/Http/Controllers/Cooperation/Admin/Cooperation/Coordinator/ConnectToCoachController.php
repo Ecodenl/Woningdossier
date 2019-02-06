6d5d577afaa6f2f50320d1492c21dbcf101873b4
Bodhi <bodhilooij1998@gmail.com>
2019-02-06 15:48:42 +0100

    cleaned up the request folders
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- app/Http/Controllers/Cooperation/Admin/Cooperation/Coordinator/ConnectToCoachController.php
+++ app/Http/Controllers/Cooperation/Admin/Cooperation/Coordinator/ConnectToCoachController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Cooperation\Admin\Cooperation\Coordinator;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Cooperation\Coordinator\ConnectToCoachRequest;
 use App\Models\Building;
 use App\Models\BuildingCoachStatus;
 use App\Models\BuildingPermission;
--- app/Http/Requests/Cooperation/Admin/Cooperation/Coordinator/CoachRequest.php
+++ app/Http/Requests/Cooperation/Admin/Cooperation/Coordinator/CoachRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Admin\Cooperation\Coordinator;
+namespace App\Http\Requests\Cooperation\Admin\Cooperation\Coordinator;
 
 use App\Rules\AlphaSpace;
 use App\Rules\HouseNumber;

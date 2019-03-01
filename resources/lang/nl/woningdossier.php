7dcd60c84ea485b0a2fcb7be40f125e2e32d1031
pvankouteren <pvankouteren@gmail.com>
2019-03-01 12:28:33 +0100

    Home translatable too

--- resources/lang/nl/woningdossier.php
+++ resources/lang/nl/woningdossier.php
@@ -732,61 +732,6 @@ return [
                 'settings' => 'Instellingen',
                 'privacy' => 'Privacy',
             ],
-            'start' => [
-                'best-user' => '<b>Beste gebruiker</b>, <br><br> Hartelijk welkom in uw Hoomdossier! Hier kunt u de huidige situatie van uw woning in kaart brengen, kijken welke energiebesparende maatregelen interessant voor u kunnen zijn en het onderhoud van uw woning plannen:',
-                'get-started' => 'Meteen aan de slag!',
-                'by' => 'Het Hoomdossier wordt u aangeboden door :cooperation om u bij de verduurzaming van uw woning te ondersteunen. Mocht u vragen hebben over energiebesparende maatregelen, lopende wijkacties of hulp nodig hebben bij het invullen van het Hoomdossier, dan kunt u hier contact opnemen met uw coöperatie:',
-                'contact' => 'Contact opnemen met :cooperation',
-                'feedback' => 'Het Hoomdossier wordt continu doorontwikkeld en verbeterd. Praktijkervaringen van gebruikers zijn hierbij een belangrijke informatiebron om het pakket steeds verder te verbeteren. Als u opmerkingen en of vragen over het Hoomdossier heeft of iets niet naar verwachting werkt kunt u uw feedback hier geven:',
-                'give-feedback' => 'Feedback geven op het Hoomdossier',
-            ],
-            'privacy' => [
-                'description' => '<b>Privacy statement</b>
-                                <br>
-                                <br>
-                                De gegevens in het Hoomdossier worden uitsluitend gedeeld met de coöperatie :cooperation.
-
-                                We gaan zorgvuldig met uw gegevens om en beloven u dat:
-                                <ul>
-                                    <li>we uw gegevens uitsluitend gebruiken om u te kunnen adviseren en ondersteunen bij  het opstellen en uitvoeren van projecten voor het verduurzamen van uw woning;</li>
-                                    <li>we uw persoonsgegevens nooit zullen delen met derden (bijvoorbeeld bedrijven) zonder dat u uw aanvullende toestemming hebt gegeven;</li>
-                                    <li>wanneer we gegevens  gebruiken t.b.v. analyses of rapportages, deze nooit tot uw individuele gegevens terug te herleiden zijn.</li>
-                                </ul>
-                                <br>
-                                <br>
-                                U kunt toestemming geven aan de coöperatie :cooperation om uw dossier op afstand in te zien en u bij het invullen te helpen. Een dergelijke toestemming kan op elk moment ingetrokken worden en geldt uitsluitend voor de gegevens van het Basisadvies, uw accountgegevens en het berichtenverkeer met de coöperatie.
-                                <br>
-                                <br>
-                                U hebt de mogelijkheid om uw account te verwijderen of om alle toegevoegde informatie te wissen onder behoud van het account. Een dergelijke actie betekent dat al uw gegevens uit het Hoomdossier worden verwijderd.
-                                <br>
-                                <br>
-                                Gegevens die in het Hoomdossier opgeslagen worden:
-                                <br>
-                                <br>
-                                In het Hoomdossier worden gegevens over uw woning, uw energieverbruik en uw gebruiksgedrag opgeslagen. De gegevens worden gebruikt om u te adviseren welke energiebesparende maatregelen u kunt nemen in uw woning en wat de indicatieve kosten en baten van deze maatregelen zijn.
-                                <br>
-                                <br>
-                                De volgende gegevens worden in het Hoomdossier opgeslagen:
-                                <br>
-                                <br>
-                                <ul>
-                                    <li>NAW gegevens</li>
-                                    <li>Algemene gegevens van de woning</li>
-                                    <li>Welke energiebesparende maatregelen al zijn genomen</li>
-                                    <li>Interesse voor een maatregel (Ja/nee)</li>
-                                    <li>Gegevens over het gebruik van de woning</li>
-                                    <li>Indicatieve kosten van te nemen maatregelen</li>
-                                    <li>Indicatie van besparing gas per maatregel</li>
-                                    <li>Indicatie van besparing elektra per maatregel</li>
-                                    <li>Indicatieve financiële besparing per maatregel</li>
-                                    <li>Indicatieve CO2 besparing per maatregel</li>
-                                    <li>Geadviseerd uitvoeringsjaar</li>
-                                    <li>Zelf ingevuld uitvoeringsjaar</li>
-                                </ul>',
-            ],
-            'disclaimer' => [
-                'description' => '<b>Disclaimer</b><br><br>Het woondossier maakt gebruik van formules en vergelijkingen die een benadering zijn van de werkelijkheid. Hoewel het woondossier dus wel inzicht geeft in de potentiele impact van energiebesparende maatregelen, kan het een persoonlijk advies op maat niet vervangen. In overleg met uw coöperatie kunt u het woondossier gebruiken als basis voor een keukentafelgesprek of een professioneel advies.<br><br>Er kan geen garantie aan de resultaten van het woondossier ontleend worden ten aanzien van de daadwerkelijke energieprestaties, berekende energiegebruik of besparingen. De essentie van het rekenen met het woondossier is het krijgen van inzicht in consequenties van het nemen van maatregelen.',
-            ],
         ],
         'help' => [
             'title' => 'Help',

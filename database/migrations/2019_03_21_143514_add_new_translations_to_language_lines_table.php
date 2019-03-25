187a22d4d78b920f86421366376a6325f104b5cf
Bodhi <bodhilooij1998@gmail.com>
2019-03-25 13:38:12 +0100

    added translations to the migration
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- database/migrations/2019_03_21_143514_add_new_translations_to_language_lines_table.php
+++ database/migrations/2019_03_21_143514_add_new_translations_to_language_lines_table.php
@@ -9,6 +9,8 @@ class AddNewTranslationsToLanguageLinesTable extends Migration
     /**
      * Run the migrations.
      *
+     * @note we use the LanguageLine model so the cache get flushed.
+     *
      * @return void
      */
     public function up()
@@ -16,38 +18,56 @@ class AddNewTranslationsToLanguageLinesTable extends Migration
         $languageLinesData = [
             'solar-panels' => [
                 'indication-for-costs.performance.ideal' => ['nl' => __('woningdossier.cooperation.tool.solar-panels.indication-for-costs.performance.ideal')],
-                'indication-for-costs.performance.no-go' => ['nl' =>__('woningdossier.cooperation.tool.solar-panels.indication-for-costs.performance.no-go')],
-                'indication-for-costs.performance.possible' => ['nl' =>__('woningdossier.cooperation.tool.solar-panels.indication-for-costs.performance.possible')],
+                'indication-for-costs.performance.no-go' => ['nl' => __('woningdossier.cooperation.tool.solar-panels.indication-for-costs.performance.no-go')],
+                'indication-for-costs.performance.possible' => ['nl' => __('woningdossier.cooperation.tool.solar-panels.indication-for-costs.performance.possible')],
                 'advice-text' => ['nl' => __('woningdossier.cooperation.tool.solar-panels.advice-text')],
                 'total-power' => ['nl' => __('woningdossier.cooperation.tool.solar-panels.total-power')]
             ],
             'insulated-glazing' => [
-                'insulated-glazing.paint-work.comments-paintwork.title' => [
+                'paint-work.comments-paintwork' => [
+                    'help' => ['nl' => 'Opmerking over schilderwerk helptext'],
                     'title' => ['nl' => 'Opmerking over schilderwerk'],
-                    'help' => ['nl' => 'Opmerking over schilderwerk helptext']
                 ]
             ]
 
         ];
 
         foreach ($languageLinesData as $group => $languageLines) {
+            $stepId = DB::table('steps')->where('slug', $group)->first()->id;
             foreach ($languageLines as $key => $translation) {
-                dump($translation);
-                if (!DB::table('language_lines')->where('group', $group)->where('key', $key)->first() instanceof stdClass) {
-                    if (count($translation) > 1) {
-                        // todo
-                        foreach ($translation as $question);
+                if (count($translation) > 1) {
+                    $fullHelpKey = $key.'.help';
+                    $fullTitleKey = $key.'.title';
+
+                    // check if the title and help key does not exists.
+                    if (!DB::table('language_lines')->where('group', $group)->where('key', $fullHelpKey)->first() instanceof stdClass && !DB::table('language_lines')->where('group', $group)->where('key', $fullTitleKey)->first() instanceof stdClass) {
+                        $helpLanguageLine = App\Models\LanguageLine::create([
+                            'group' => $group,
+                            'key' => $fullHelpKey,
+                            'text' => $translation['help'],
+                            'step_id' => $stepId
+                        ]);
+                        App\Models\LanguageLine::create([
+                            'group' => $group,
+                            'key' => $fullTitleKey,
+                            'text' => $translation['title'],
+                            'help_language_line_id' => $helpLanguageLine->id,
+                            'step_id' => $stepId
+                        ]);
                     } else {
-                        // use the model so the cache get flushed.
+                        dump('De key ' . $key . ' bestaat al in de group ' . $group . ' wat vervelend!');
+                    }
+                } else {
+                    if (!DB::table('language_lines')->where('group', $group)->where('key', $key)->first() instanceof stdClass) {
                         App\Models\LanguageLine::create([
                             'group' => $group,
                             'key' => $key,
                             'text' => $translation,
-                            'step_id' => DB::table('steps')->where('slug', $group)->first()->id
+                            'step_id' => $stepId
                         ]);
+                    } else {
+                        dump('De key ' . $key . ' bestaat al in de group ' . $group . ' wat vervelend!');
                     }
-                } else {
-                    dump('De key '.$key.' bestaat al in de group '.$group.' wat vervelend!');
                 }
             }
         }

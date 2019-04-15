6a119f959347b1db12f6e7d4f6beb5476d6b9b2b
Bodhi <bodhilooij1998@gmail.com>
2019-04-15 15:56:17 +0200

    Changed the the log translations changed the table for the logs, only the created at and the message Added log for revoke and give access methods.
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- app/Http/Controllers/Cooperation/MyAccount/AccessController.php
+++ app/Http/Controllers/Cooperation/MyAccount/AccessController.php
@@ -8,6 +8,7 @@ use App\Models\Building;
 use App\Models\BuildingCoachStatus;
 use App\Models\BuildingPermission;
 use App\Models\Cooperation;
+use App\Models\Log;
 use App\Models\PrivateMessage;
 use App\Services\BuildingCoachStatusService;
 use Illuminate\Http\Request;
@@ -47,6 +48,14 @@ class AccessController extends Controller
     {
         $coachesWithAccessToResidentBuildingStatuses = BuildingCoachStatus::getConnectedCoachesByBuildingId(HoomdossierSession::getBuilding());
 
+        Log::create([
+            'user_id' => \Auth::id(),
+            'building_id' => HoomdossierSession::getBuilding(),
+            'message' => __('woningdossier.log-messages.user-gave-access', [
+                'full_name' => \Auth::user()->getFullName(),
+            ]),
+        ]);
+
         // we give the coaches that have "permission" to talk to a resident the permissions to access the building from the resident.
         foreach ($coachesWithAccessToResidentBuildingStatuses as $coachWithAccessToResidentBuildingStatus) {
             BuildingPermission::create([
@@ -65,6 +74,15 @@ class AccessController extends Controller
     {
         // get all the connected coaches to the building
         $connectedCoachesToBuilding = BuildingCoachStatus::getConnectedCoachesByBuildingId(HoomdossierSession::getBuilding());
+
+        Log::create([
+            'user_id' => \Auth::id(),
+            'building_id' => HoomdossierSession::getBuilding(),
+            'message' => __('woningdossier.log-messages.user-revoked-access', [
+                'full_name' => \Auth::user()->getFullName(),
+            ]),
+        ]);
+
         // and revoke them the access to the building
         foreach ($connectedCoachesToBuilding as $connectedCoachToBuilding) {
             BuildingCoachStatusService::revokeAccess($connectedCoachToBuilding->coach_id, $connectedCoachToBuilding->building_id);

6aa5644094f8fe7ddbb83ce7b7bb6b5695f2d659
pvankouteren <pvankouteren@gmail.com>
2019-04-25 14:55:02 +0200

    Translatable elements

---
+++ resources/lang/nl/general-data.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+	'element' => [
+		'living-rooms-windows' => [
+			'help' => "Als er meerdere soorten glas voorkomen, kies dan hier de soort met de grootste oppervlakte",
+			'title' => "Ramen in de leefruimtes",
+		],
+		'sleeping-rooms-windows' => [
+			'help' => "Als er meerdere soorten glas voorkomen, kies dan hier de soort met de grootste oppervlakte",
+			'title' => "Ramen in de slaapruimtes",
+		],
+		'wall-insulation' => [
+			'help' => "Denk aan de volgende isolatiemogelijkheden: Gevelisolatie tijdens de bouw, Spouwmuurisolatie, Isolerende voorzetwanden binnen, Buitengevelisolatie.<br>Geen isolatie = gevels met 2 cm isolatie of minder Matige isolatie = gevels met een Rc-waarde van minder dan 2,58 m2K\/W<br>Goede isolatie = gevels met een Rc-waarde van meer dan 2,58 m2K\/W",
+			'title' => "Gevelisolatie",
+		],
+		'floor-insulation' => [
+			'help' => "Denk aan de volgende isolatiemogelijkheden: Tonzonisolatie, isolatie tussen de balken, PUR isolatie, isolatie op de bodem van de kruipruimte.<br>Geen isolatie = vloeren met 2 cm isolatie of minder Matige isolatie = vloeren met een Rc-waarde van minder dan 2,44 m2K\/W<br> Goede isolatie = vloeren met een Rc-waarde van meer dan 2,44 m2K\/W",
+			'title' => "Vloerisolatie",
+		],
+		'roof-insulation' => [
+			'help' => "Denk aan de volgende isolatiemogelijkheden: Isolatie van binnenuit achter gipsplaten, isolatie onder de dakpannen, isolatie onder de dakbedekking.<br>Geen isolatie = daken met 2 cm isolatie of minder Matige isolatie = daken met een Rc-waarde van minder dan 2,44 m2K\/W<br>Goede isolatie = daken met een Rc-waarde van meer dan 2,44 m2K\/W",
+			'title' => "Dakisolatie",
+		],
+
+	],
+];

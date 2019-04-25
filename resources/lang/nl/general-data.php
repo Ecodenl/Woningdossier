33fb48339480cea5f4ed642e8a2590bd5af6017f
pvankouteren <pvankouteren@gmail.com>
2019-04-25 14:58:51 +0200

    Translatable services

--- resources/lang/nl/general-data.php
+++ resources/lang/nl/general-data.php
@@ -22,6 +22,35 @@ return [
 			'help' => "Denk aan de volgende isolatiemogelijkheden: Isolatie van binnenuit achter gipsplaten, isolatie onder de dakpannen, isolatie onder de dakbedekking.<br>Geen isolatie = daken met 2 cm isolatie of minder Matige isolatie = daken met een Rc-waarde van minder dan 2,44 m2K\/W<br>Goede isolatie = daken met een Rc-waarde van meer dan 2,44 m2K\/W",
 			'title' => "Dakisolatie",
 		],
-
+	],
+	'service' => [
+		'hybrid-heat-pump' => [
+			'help' => "Hier kunt u aangeven of u in de huidige situatie een hybride warmtepomp met buitenlucht als warmtebron hebt of niet.",
+			'title' => "Hybride warmtepomp",
+		],
+		'full-heat-pump' => [
+			'help' => "Hier kunt u aangeven of u in de huidige situatie in plaats van een cv-ketel een warmtepomp als enige warmteopwekker in huis hebt. U hebt de keuze uit een warmtepomp met buitenlucht of bodemenergie als warmtebron.",
+			'title' => "Volledige warmtepomp",
+		],
+		'sun-boiler' => [
+			'help' => "Hier kunt u aangeven of u in de huidige situatie een zonneboiler hebt en waarvoor u de warmte gebruikt.",
+			'title' => "Zonneboiler",
+		],
+		'hr-boiler' => [
+			'help' => "Hier kunt u aangeven of er in de huidige situatie een cv ketel aanwezig is en hoe oud deze ongeveer is.\"",
+			'title' => "HR CV ketel",
+		],
+		'boiler' => [
+			'help' => "Hier kunt u aangeven welk type ketel u heeft. Als u het niet weet kies dan HR107 ketel.",
+			'title' => "Type ketel",
+		],
+		'house-ventilation' => [
+			'help' => "Dit veld dient alleen ervoor om de huidige situatie vast te leggen, er wordt niet mee gerekend. Meer informatie over ventilatie en aandachtspunten voor goed ventileren kunt u op de volgende pagina vinden.\r\n Natuurlijke ventilatie:\r\n De lucht in de woning wordt zonder behulp van ventilatoren ververst, bijvoorbeeld door ramen open te doen en\/of ventilatieroosters. Een badkamer ventilator en de keukenafzuiging hoort daar niet bij. Mechanisch:\r\n De lucht wordt door een ventilator continu afgezogen, de lucht komt via roosters of open ramen naar binnen. De ventilatiebox zit vaak op zolder.\r\n Gebalanceerd:\r\n De lucht wordt mechanisch afgevoerd en mechanisch ingeblazen. Dit systeem is vaak in nieuwbouw woningen aanwezig.\r\n Decentraal mechanisch:\r\n De lucht wordt per kamer met een apart apparaat afgezogen en ingeblazen. De ventilatie-unit kan bijvoorbeeld ge\u00efntegreerd zijn in een radiator. Vraaggestuurd:\r\n Bij vraaggestuurde ventilatie gaat het altijd om een mechanisch systeem dat door luchtsensoren in de ruimtes gestuurd wordt. Bijvoorbeeld op basis van co2 of vochtgehalte.",
+			'title' => "Hoe wordt het huis geventileerd",
+		],
+		'total-sun-panels' => [
+			'help' => "Voer hier het aantal zonnepanelen in dat in de huidige situatie geïnstalleerd is. Als u geen panelen hebt vul dan 0 in of laat het veld leeg.",
+			'title' => "Hoeveel zonnepanelen zijn er aanwezig",
+		],
 	],
 ];

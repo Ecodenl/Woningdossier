f6d26b2ab7defdc8b991d149a7eca42a5bb2390d
Bodhi <bodhilooij1998@gmail.com>
2019-04-25 11:41:53 +0200

    Cleaned up code.
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- app/Http/Controllers/Cooperation/ImportController.php
+++ app/Http/Controllers/Cooperation/ImportController.php
@@ -15,14 +15,14 @@ class ImportController extends Controller
     /**
      * Check if a key / column name needs a update.
      *
-     * @param $key | Column name
+     * @param $key  | Column name
      *
      * @return bool
      */
     private function keyNeedsUpdate($key)
     {
         $keysToNotUpdate = ['id', 'building_id', 'input_source_id', 'created_at', 'updated_at', 'comment'];
-        if (! in_array($key, $keysToNotUpdate, true)) {
+        if ( ! in_array($key, $keysToNotUpdate, true)) {
             return true;
         }
 
@@ -35,7 +35,7 @@ class ImportController extends Controller
      * radio buttons they should not be considered empty. We define these
      * fields here so we can test for !empty OR radio input.
      *
-     * @param string $key
+     * @param  string  $key
      *
      * @return bool
      */
@@ -62,18 +62,20 @@ class ImportController extends Controller
             if (empty($inputSourceToUpdate['extra'])) {
                 $updateExtra = $inputSourceToCopy['extra'];
             } else {
-                $inputSourceToCopyExtra = json_decode($inputSourceToCopy['extra'], true);
+                $inputSourceToCopyExtra   = json_decode($inputSourceToCopy['extra'], true);
                 $inputSourceToUpdateExtra = json_decode($inputSourceToUpdate['extra'], true);
 
                 // filter the values which are not considered to be empty.
-                $inputSourceToCopyNotNullExtraValues = array_filter($inputSourceToCopyExtra, function ($extraValue, $extraKey) {
-                    // if the string is not considered empty, then we want to update it.
-                    return ! Str::isConsideredEmptyAnswer($extraValue) && $this->keyNeedsUpdate($extraKey);
-                }, ARRAY_FILTER_USE_BOTH);
+                $inputSourceToCopyNotNullExtraValues = array_filter($inputSourceToCopyExtra,
+                    function ($extraValue, $extraKey) {
+                        // if the string is not considered empty, then we want to update it.
+                        return ! Str::isConsideredEmptyAnswer($extraValue) && $this->keyNeedsUpdate($extraKey);
+                    }, ARRAY_FILTER_USE_BOTH);
 
                 // create the extra column json.
                 // merge those toes
-                $updateExtra = json_encode(array_merge($inputSourceToUpdateExtra, $inputSourceToCopyNotNullExtraValues));
+                $updateExtra = json_encode(array_merge($inputSourceToUpdateExtra,
+                    $inputSourceToCopyNotNullExtraValues));
             }
 
             // add the json to the extra ket
@@ -85,7 +87,7 @@ class ImportController extends Controller
         // now update the "normal" values
         foreach ($inputSourceToCopy as $desiredInputSourceKey => $desiredInputSourceAnswer) {
             // if the answer from the desired input source is not empty and the key needs a update, then we update the resident his answer.
-            if ((! empty($desiredInputSourceAnswer) || $this->isRadioInput($desiredInputSourceKey)) && $this->keyNeedsUpdate($desiredInputSourceKey)) {
+            if (( ! empty($desiredInputSourceAnswer) || $this->isRadioInput($desiredInputSourceKey)) && $this->keyNeedsUpdate($desiredInputSourceKey)) {
                 $updateArray[$desiredInputSourceKey] = $desiredInputSourceAnswer;
             }
         }
@@ -108,15 +110,15 @@ class ImportController extends Controller
         // the tables that have a the where_column is used to query on the resident his answers.
         $tables = [
             'building_features',
-            'building_elements' => [
-                'where_column' => 'element_id',
+            'building_elements'           => [
+                'where_column'            => 'element_id',
                 'additional_where_column' => 'element_value_id',
             ],
-            'building_services' => [
-                'where_column' => 'service_id',
+            'building_services'           => [
+                'where_column'            => 'service_id',
                 'additional_where_column' => 'service_value_id',
             ],
-            'building_roof_types' => [
+            'building_roof_types'         => [
                 'where_column' => 'roof_type_id',
             ],
             'building_insulated_glazings' => [
@@ -125,10 +127,10 @@ class ImportController extends Controller
             'building_user_usages',
             'building_paintwork_statuses',
             'building_user_usages',
-            'user_progresses' => [
+            'user_progresses'             => [
                 'where_column' => 'step_id',
             ],
-            'questions_answers' => [
+            'questions_answers'           => [
                 'where_column' => 'question_id',
             ],
             'building_features',
@@ -140,45 +142,45 @@ class ImportController extends Controller
                 'where_column' => 'measure_application_id',
             ],
             'user_energy_habits',
-            'user_interests' => [
-                'where_column' => 'interested_in_type',
+            'user_interests'           => [
+                'where_column'            => 'interested_in_type',
                 'additional_where_column' => 'interested_in_id',
             ],
         ];
 
         // input sources
         $desiredInputSource = InputSource::findByShort($desiredInputSourceName);
-        $targetInputSource = InputSource::find(HoomdossierSession::getInputSource());
+        $targetInputSource  = InputSource::find(HoomdossierSession::getInputSource());
 
         foreach ($tables as $tableOrInt => $tableOrWhereColumns) {
             // now check if its a int
             // if it isn't a int, the $tableOrId is a table and the $tableOrWhereColumns is a where column
             // else the $tableOrWhereColumns is the table and we do not need to query further.
-            if (! is_int($tableOrInt)) {
-                $table = $tableOrInt;
+            if ( ! is_int($tableOrInt)) {
+                $table       = $tableOrInt;
                 $whereColumn = $tableOrWhereColumns['where_column'];
             } else {
                 $table = $tableOrWhereColumns;
             }
             // building to copy data from
             $building = Building::find(HoomdossierSession::getBuilding());
-            $user = $building->user()->first();
+            $user     = $building->user()->first();
 
             // set the building or user id, depending on which column exists on the table
             if (\Schema::hasColumn($table, 'user_id')) {
-                $buildingOrUserId = $user->id;
+                $buildingOrUserId     = $user->id;
                 $buildingOrUserColumn = 'user_id';
             } else {
-                $buildingOrUserId = $building->id;
+                $buildingOrUserId     = $building->id;
                 $buildingOrUserColumn = 'building_id';
             }
 
 
             // now we get all the answers from the desired input source
             $desiredInputSourceValues = \DB::table($table)
-                ->where('input_source_id', $desiredInputSource->id)
-                ->where($buildingOrUserColumn, $buildingOrUserId)
-                ->get();
+                                           ->where('input_source_id', $desiredInputSource->id)
+                                           ->where($buildingOrUserColumn, $buildingOrUserId)
+                                           ->get();
 
             // now check if the $whereColumn isset
             // if so we need to add it to the query from the resident during the loop from the $desiredInputSourceValues
@@ -188,17 +190,16 @@ class ImportController extends Controller
                     if ($desiredInputSourceValue instanceof \stdClass && isset($desiredInputSourceValue->$whereColumn)) {
                         // now build the query to get the resident his answers
                         $targetInputSourceValueQuery = \DB::table($table)
-                            ->where('input_source_id', $targetInputSource->id)
-                            ->where($buildingOrUserColumn, $buildingOrUserId)
-                            ->where($whereColumn, $desiredInputSourceValue->$whereColumn);
+                                                          ->where('input_source_id', $targetInputSource->id)
+                                                          ->where($buildingOrUserColumn, $buildingOrUserId)
+                                                          ->where($whereColumn, $desiredInputSourceValue->$whereColumn);
 
                         // count the rows
                         $targetInputSourceValueCount = \DB::table($table)
-                            ->where('input_source_id', $targetInputSource->id)
-                            ->where($buildingOrUserColumn, $buildingOrUserId)
-                            ->where($whereColumn, $desiredInputSourceValue->$whereColumn)
-                            ->count();
-
+                                                          ->where('input_source_id', $targetInputSource->id)
+                                                          ->where($buildingOrUserColumn, $buildingOrUserId)
+                                                          ->where($whereColumn, $desiredInputSourceValue->$whereColumn)
+                                                          ->count();
 
 
                         // if there are multiple, then we need to add another where to the query.
@@ -214,12 +215,13 @@ class ImportController extends Controller
                             $targetInputSourceValue = $targetInputSourceValueQuery->first();
 
                             // cast the results to a array
-                            $targetInputSourceValue = (array) $targetInputSourceValue;
+                            $targetInputSourceValue  = (array) $targetInputSourceValue;
                             $desiredInputSourceValue = (array) $desiredInputSourceValue;
 
                             // if it exists, we need to update it. Else we need to insert a new row.
-                            if (! empty($targetInputSourceValue)) {
-                                $targetInputSourceValueQuery->update($this->createUpdateArray($targetInputSourceValue, $desiredInputSourceValue));
+                            if ( ! empty($targetInputSourceValue)) {
+                                $targetInputSourceValueQuery->update($this->createUpdateArray($targetInputSourceValue,
+                                    $desiredInputSourceValue));
                             } else {
                                 // unset the stuff we dont want to insert
                                 unset($desiredInputSourceValue['id'], $desiredInputSourceValue['input_source_id']);
@@ -232,14 +234,13 @@ class ImportController extends Controller
                             $targetInputSourceValue = $targetInputSourceValueQuery->first();
 
                             // cast the results to a array
-                            $targetInputSourceValue = (array) $targetInputSourceValue;
+                            $targetInputSourceValue  = (array) $targetInputSourceValue;
                             $desiredInputSourceValue = (array) $desiredInputSourceValue;
 
                             // YAY! data has been copied so update or create the target input source his records.
                             if ($targetInputSourceValueQuery->first() instanceof \stdClass) {
                                 // check if its empty ornot.
-                                $updateData = $this->createUpdateArray($targetInputSourceValue, $desiredInputSourceValue);
-                                if(!empty($updateData)) {
+                                if ( ! empty($updateData = $this->createUpdateArray((array) $targetInputSourceValue, (array) $desiredInputSourceValue))) {
                                     $targetInputSourceValueQuery->update($updateData);
                                 }
                             } else {
@@ -259,19 +260,17 @@ class ImportController extends Controller
             } else {
                 // get the resident his input
                 $targetInputSourceValueQuery = \DB::table($table)
-                    ->where('input_source_id', $targetInputSource->id)
-                    ->where($buildingOrUserColumn, $buildingOrUserId);
+                                                  ->where('input_source_id', $targetInputSource->id)
+                                                  ->where($buildingOrUserColumn, $buildingOrUserId);
 
 
                 // get the first result from the desired input source
                 $desiredInputSourceValue = $desiredInputSourceValues->first();
-                $targetInputSourceValue = $targetInputSourceValueQuery->first();
+                $targetInputSourceValue  = $targetInputSourceValueQuery->first();
 
-                // if it exists, we need to update it. Else we need to insert a new row.
+
                 if ($targetInputSourceValue instanceof \stdClass) {
-                    // check if its empty ornot.
-                    $updateData = $this->createUpdateArray((array) $targetInputSourceValue, (array) $desiredInputSourceValue);
-                    if(!empty($updateData)) {
+                    if ( ! empty($updateData = $this->createUpdateArray((array) $targetInputSourceValue, (array) $desiredInputSourceValue))) {
                         $targetInputSourceValueQuery->update($updateData);
                     }
                 } else {
@@ -287,7 +286,6 @@ class ImportController extends Controller
             }
         }
 
-        dd('Maat');
         ToolSettingService::setChanged(HoomdossierSession::getBuilding(), $desiredInputSource->id, false);
         HoomdossierSession::stopUserComparingInputSources();
 

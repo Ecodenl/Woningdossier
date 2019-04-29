db5e344da2c0d8e36982c9700e2b6de8c894e909
Bodhi <bodhilooij1998@gmail.com>
2019-04-29 09:44:25 +0200

    Adjusted the ValidatorTrait so we can use validation rules for when the tool is being filled for a user. Removed old methods from the User controller.
    Signed-off-by: Bodhi <bodhilooij1998@gmail.com>

--- app/Http/Requests/ValidatorTrait.php
+++ app/Http/Requests/ValidatorTrait.php
@@ -3,39 +3,44 @@
 namespace App\Http\Requests;
 
 use App\Helpers\HoomdossierSession;
+use App\Models\User;
 use Illuminate\Validation\Factory;
 
 trait ValidatorTrait
 {
-    public $alwaysValidate = false;
+
+    /**
+     * The rules that apply when a user (mostly a coach), is filling the tool for a user (mostly a resident).
+     *
+     * @return array
+     */
+    public function isFillingToolForUserRules(): array
+    {
+        return [];
+    }
 
     /**
      * Validate the request.
      *
      * @param  Factory  $factory
      *
-     * @return \Illuminate\Validation\Validator
+     * @return \Illuminate\Validation\Validator|null
      */
     public function validator(Factory $factory)
     {
         $validate = null;
 
-        if ($this->alwaysValidate) {
-            // validate it like it normaly would
+        // if the session set building != to the Auth user his building, then the Auth user is probably filling the tool for a resident
+        // we don't validate inputs if so
+        if (\Auth::user()->isFillingToolForOtherBuilding()) {
+            // pass empty values and rules so the validation will always pass
+            $validate = $factory->make($this->all(), $this->isFillingToolForUserRules());
+        } else {
+
             $validate = $factory->make(
                 $this->all(), $this->container->call([$this, 'rules']), $this->messages(), $this->attributes()
             );
-
-        } else {
-            // if the session set building != to the Auth user his building, then the Auth user is probably filling the tool for a resident
-            // we don't validate inputs if so
-            if (HoomdossierSession::getBuilding() != \Auth::user()->buildings()->first()->id) {
-                // pass empty values and rules so the validation will always pass
-                $validate = $factory->make([], []);
-            }
         }
-
-
 
         return $validate;
     }

bd56157617c38aeaf3f8c50171699ddec0b2e8cf
pvankouteren <pvankouteren@gmail.com>
2019-05-14 13:31:01 +0200

    Text update

--- resources/lang/nl/mail.php
+++ resources/lang/nl/mail.php
@@ -11,14 +11,14 @@ return [
     'changed-email' => [
         'subject' => 'Hoomdossier: uw e-mail adres is aangepast.',
         'salutation' => 'Beste :first_name :last_name,',
-        'text' => 'U heeft uw e-mail adres aangepast, u kunt dit ongedaan maken door de onderstaande link te volgen: <br><br>:recover_old_email_url<br><br>Als u hierover vragen hebt, kunt u contact opnemen met :cooperation_link',
+        'text' => 'Het e-mailadres voor uw Hoomdossier is aangepast. Indien dit gewenst was hoeft u niets te doen. Als u deze activiteit niet herkent kunt u dit ongedaan maken door de onderstaande link te volgen. Wij raden u aan om vervolgens uw wachtwoord te resetten.<br><br>:recover_old_email_url<br><br>Als u hierover vragen hebt, kunt u contact opnemen met :cooperation_link',
         'kind_regards' => 'Met vriendelijke groet, <br>:app_name support'
     ],
 
     'confirm-account' => [
         'subject' => 'Welkom in het Hoomdossier',
         'salutation' => 'Beste :first_name :last_name,',
-        'text' => 'u heeft een account aangevraagd op :hoomdossier_link<br><br>Bevestig uw account door onderstaande link te volgen:<br><br>:confirm_url<br><br>Als u hierover vragen hebt, kunt u contact opnemen met :cooperation_link',
+        'text' => 'U heeft een account aangevraagd op :hoomdossier_link<br><br>Bevestig uw account door onderstaande link te volgen:<br><br>:confirm_url<br><br>Als u hierover vragen hebt, kunt u contact opnemen met :cooperation_link',
         'kind_regards' => 'Met vriendelijke groet, <br>:app_name support'
     ],
 

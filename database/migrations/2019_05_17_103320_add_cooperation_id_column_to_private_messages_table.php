7c46392a9b7c2da1b8311c583ca2eb9c8b1366b8
Bodhi <bodhilooij1998@gmail.com>
2019-05-17 10:54:54 +0200

    Easier than i thought it would be, every message had a to_cooperation_id.
    Added cooperation_id to the private_messages table
    Filled the cooperation_id.

---
+++ database/migrations/2019_05_17_103320_add_cooperation_id_column_to_private_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCooperationIdColumnToPrivateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('private_messages', function (Blueprint $table) {
+            $table->unsignedInteger('cooperation_id')->nullable()->default(null)->after('building_id');
+            $table->foreign('cooperation_id')->references('id')->on('cooperations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('private_messages', function (Blueprint $table) {
+            $table->dropColumn('cooperation_id');
+            $table->dropForeign('cooperation_id');
+        });
+    }
+}

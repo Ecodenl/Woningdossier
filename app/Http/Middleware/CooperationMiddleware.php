31113bf9b3f1507cbcd0f16d82a2c5b7708daf9f
pvankouteren <pvankouteren@gmail.com>
2019-06-06 11:33:12 +0200

    Restore middleware

--- app/Http/Middleware/CooperationMiddleware.php
+++ app/Http/Middleware/CooperationMiddleware.php
@@ -9,51 +9,49 @@ use Illuminate\Support\Facades\URL;
 
 class CooperationMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
-     *
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $cooperation = $request->route()->parameter('cooperation');
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Closure                 $next
+	 *
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		$cooperation = $request->route()->parameter('cooperation');
 
-        if (! $cooperation instanceof Cooperation) {
-            // No valid cooperation subdomain. Return to global index.
-            \Log::debug('No cooperation found');
+		if (! $cooperation instanceof Cooperation) {
+			// No valid cooperation subdomain. Return to global index.
+			\Log::debug('No cooperation found');
 
-            return redirect()->route('index');
-        }
+			return redirect()->route('index');
+		}
 
-        \Log::debug('Session: cooperation -> '.$cooperation->id.' ('.$cooperation->slug.')');
+		\Log::debug('Session: cooperation -> '.$cooperation->id.' ('.$cooperation->slug.')');
+		HoomdossierSession::setCooperation($cooperation);
 
-        // Set as default URL parameter
-        if (HoomdossierSession::hasCooperation()) {
-            //$cooperation = Cooperation::find($request->session()->get('cooperation'));
-            if ($cooperation instanceof Cooperation) {
-                \Log::debug('Setting default cooperation for URL -> '.$cooperation->id.' ('.$cooperation->slug.')');
-                URL::defaults(['cooperation' => $cooperation->slug]);
-            }
-        } else {
-            // only set it when there is no cooperation.
-            HoomdossierSession::setCooperation($cooperation);
-        }
+		// Set as default URL parameter
+		if (HoomdossierSession::hasCooperation()) {
+			//$cooperation = Cooperation::find($request->session()->get('cooperation'));
+			if ($cooperation instanceof Cooperation) {
+				\Log::debug('Setting default cooperation for URL -> '.$cooperation->id.' ('.$cooperation->slug.')');
+				URL::defaults(['cooperation' => $cooperation->slug]);
+			}
+		}
 
-        if (HoomdossierSession::hasRole() && ! empty(HoomdossierSession::getRole())) {
-            \Log::debug('Session: role -> '.HoomdossierSession::getRole());
+		if (HoomdossierSession::hasRole() && ! empty(HoomdossierSession::getRole())) {
+			\Log::debug('Session: role -> '.HoomdossierSession::getRole());
 
-            if (HoomdossierSession::isUserComparingInputSources()) {
-                \Log::debug('Session: user is comparing his data / value to that from a '.HoomdossierSession::getCompareInputSourceShort());
-            } else {
-                \Log::debug('Session: user is not comparing his data');
-            }
-        } else {
-            \Log::debug('Session: no user role set.');
-        }
+			if (HoomdossierSession::isUserComparingInputSources()) {
+				\Log::debug('Session: user is comparing his data / value to that from a '.HoomdossierSession::getCompareInputSourceShort());
+			} else {
+				\Log::debug('Session: user is not comparing his data');
+			}
+		} else {
+			\Log::debug('Session: no user role set.');
+		}
 
-        return $next($request);
-    }
+		return $next($request);
+	}
 }

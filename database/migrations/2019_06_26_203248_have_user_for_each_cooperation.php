c4904bdc8710912788941f23df6fd9fcdac89bb5
pvankouteren <pvankouteren@gmail.com>
2019-06-27 14:36:52 +0200

    Update model_has_roles table: splitted out users, removed direct cooperation link

--- database/migrations/2019_06_26_203248_have_user_for_each_cooperation.php
+++ database/migrations/2019_06_26_203248_have_user_for_each_cooperation.php
@@ -20,35 +20,87 @@ class HaveUserForEachCooperation extends Migration
             });
         }
 
-        $combinations = DB::table('cooperation_user')->get();
-        foreach ($combinations as $combination) {
-            $user = DB::table('users')->where('id', '=',
-                $combination->user_id)->whereNull('cooperation_id')->first();
-            if ($user instanceof stdClass) {
-                // update the user
-                DB::table('users')->where('id', '=',
-                    $combination->user_id)->update(['cooperation_id' => $combination->cooperation_id]);
-            } else {
-                // find the user (for the account ID)
+        if (Schema::hasTable('cooperation_user')) {
+            $combinations = DB::table('cooperation_user')->get();
+            foreach ($combinations as $combination) {
                 $user = DB::table('users')->where('id', '=',
-                    $combination->user_id)->first();
+                    $combination->user_id)->whereNull('cooperation_id')->first();
                 if ($user instanceof stdClass) {
-                    DB::table('users')->insert([
-                        'account_id'     => $user->account_id,
-                        'cooperation_id' => $combination->cooperation_id,
-                        'created_at'     => $user->created_at,
-                        'updated_at'     => $user->updated_at
-                    ]);
+                    // update the user
+                    DB::table('users')->where('id', '=',
+                        $combination->user_id)->update(['cooperation_id' => $combination->cooperation_id]);
+                } else {
+                    // find the user (for the account ID)
+                    $user = DB::table('users')->where('id', '=',
+                        $combination->user_id)->first();
+                    if ($user instanceof stdClass) {
+                        DB::table('users')->insert([
+                            'first_name'     => $user->first_name,
+                            'last_name'      => $user->last_name,
+                            'phone_number'   => $user->phone_number,
+                            'mobile'         => $user->mobile,
+                            'account_id'     => $user->account_id,
+                            'cooperation_id' => $combination->cooperation_id,
+                            'created_at'     => $user->created_at,
+                            'updated_at'     => $user->updated_at
+                        ]);
+                    }
                 }
+                // delete the cooperation_user row
+                DB::table('cooperation_user')
+                  ->where('user_id', '=', $combination->user_id)
+                  ->where('cooperation_id', '=', $combination->cooperation_id)
+                  ->delete();
             }
-            // delete the cooperation_user row
-            DB::table('cooperation_user')
-              ->where('user_id', '=', $combination->user_id)
-              ->where('cooperation_id', '=', $combination->cooperation_id)
-              ->delete();
+
+            Schema::drop('cooperation_user');
         }
 
-        Schema::drop('cooperation_user');
+        // Correct rights
+
+        $modelsWithRoles = DB::table('model_has_roles')->where('model_type',
+            '=', 'App\Models\User')->get();
+        foreach ($modelsWithRoles as $modelWithRoles) {
+            // get related user
+            $user = DB::table('users')->find($modelWithRoles->model_id);
+
+            /*if (in_array($modelWithRoles->model_id, [470, 519])) {
+                dump($modelWithRoles);
+            }*/
+
+            if ($user instanceof stdClass) {
+                // get actual user by account_id and cooperation_id
+                $sibling = DB::table('users')
+                             ->where('account_id', '=', $user->account_id)
+                             ->where('cooperation_id', '=',
+                                 $modelWithRoles->cooperation_id)
+                             ->first();
+
+                /*if (in_array($modelWithRoles->model_id, [470, 519])) {
+                    dump("Sibling: ");
+                    dump($sibling);
+                }*/
+
+                if ($sibling instanceof stdClass && $sibling->id != $user->id) {
+                    DB::table('model_has_roles')
+                      ->where('model_type', '=', 'App\Models\User')
+                      ->where('model_id', '=', $modelWithRoles->model_id)
+                      ->where('cooperation_id', '=',
+                          $modelWithRoles->cooperation_id)
+                      ->where('role_id', '=', $modelWithRoles->role_id)
+                      ->update(['model_id' => $sibling->id]);
+                }
+            }
+
+        }
+        // Now drop the cooperation column on the roles table
+        if (Schema::hasColumn('model_has_roles', 'cooperation_id')){
+            Schema::table('model_has_roles', function(Blueprint $table){
+                $table->dropForeign(['cooperation_id']);
+                $table->dropColumn('cooperation_id');
+            });
+        }
+
     }
 
     /**
@@ -58,6 +110,18 @@ class HaveUserForEachCooperation extends Migration
      */
     public function down()
     {
+        // Put column back
+        if (!Schema::hasColumn('model_has_roles', 'cooperation_id')){
+            Schema::table('model_has_roles', function (Blueprint $table) {
+                $table->unsignedInteger('cooperation_id')->nullable()->after('model_id');
+                $table->foreign('cooperation_id')->references('id')->on('cooperations')->onDelete('cascade');
+            });
+        }
+
+        // Set rights back
+        // todo
+
+        // Link cooperation and users back
         if ( ! Schema::hasTable('cooperation_user')) {
             Schema::create('cooperation_user', function (Blueprint $table) {
                 $table->integer('cooperation_id')->unsigned();

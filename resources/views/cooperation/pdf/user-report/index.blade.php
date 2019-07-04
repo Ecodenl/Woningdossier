f34845ed09fa5164605a9372a2ea6538185d62cd
Bodhi <bodhilooij1998@gmail.com>
2019-07-04 15:15:32 +0200

    Added font and base frontpage added config for dompdf

--- resources/views/cooperation/pdf/user-report/index.blade.php
+++ resources/views/cooperation/pdf/user-report/index.blade.php
@@ -4,18 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.0.4/tailwind.min.css">
     <title>Document</title>
 
     <style>
-        :root {
-            --main-title-color: #29A082;
+{{--        .center {--}}
+{{--            margin: auto;--}}
+{{--            width: 60%;--}}
+{{--        }--}}
+{{--        .pdf-container {--}}
+{{----}}
+{{--        }--}}
+{{--        .capitalize {--}}
+{{--            .--}}
+{{--        }--}}
+        .turquoise {
+            color: #29A082;
         }
-        h3 {
-            color: var(--main-title-color);
+        h1 {
+            font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+            font-size: 16px;
+            font-weight: bold;
+            color: #29A082;
+        }
+
+        /* koptekst */
+        h4  {
+            font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+            font-size: 10px;
+            color: #29A082;
+        }
+
+        /* tabal kop celachtergrond */
+        p:not(.lead) {
+            font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+            color: #000000;
+            font-size: 10px;
+            line-height: 1.15;
+        }
+
+        p.lead {
+            font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+            font-weight: bold;
+            color: #000000;
+            font-size: 10px;
         }
     </style>
 </head>
 <body>
-<h3>Test</h3>
+
+<div class="container mx-auto">
+
+    @include('cooperation.pdf.user-report.parts.frontpage')
+
+    <p class="lead">inleidings tekst</p>
+    <p>stukje tekst</p>
+    <br>
+    <h1>Koptekst groot</h1>
+    <br>
+    <h4>koptekst klein</h4>
+</div>
 </body>
 </html>
---
+++ resources/views/cooperation/pdf/user-report/parts/frontpage.blade.php
@@ -0,0 +1,15 @@
+<?php
+$user = Auth::user();
+$building = $user->buildings->first();
+?>
+
+<h1>{{$user->getFullName()}}</h1>
+<h1>{{$building->street}} {{$building->number}} {{$building->extension}}</h1>
+<h1>{{$building->postal_code}} {{$building->city}}</h1>
+
+<img src="{{asset('images/pdf-main-images.jpg')}}" alt="">
+
+<div class="capitalize">
+    <h1>Stappenplan voor het verduurzamen van uw woning</h1>
+    <p>U hebt het Hoomdossier voor uw woning ingevuld.</p>
+</div>

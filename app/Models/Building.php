7574cbe1fc97f05d6564432fd3517421dcf09773
pvankouteren <pvankouteren@gmail.com>
2019-08-02 16:43:45 +0200

    Small code fixes based on (incomplete) unit tests. Work needed for the input source traits

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -355,7 +355,7 @@ class Building extends Model
     public function getServiceValue($short)
     {
         /** @var BuildingService $buildingService */
-        $buildingService = $this->getBuildingService($short);
+        $buildingService = $this->getBuildingService($short); dd($this->buildingServices);
         $serviceValue = $buildingService->serviceValue;
 
         return $serviceValue;

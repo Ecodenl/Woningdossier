b59b31105f583e43c3116c54d035c6c4be99e8bb
pvankouteren <pvankouteren@gmail.com>
2019-08-14 09:36:28 +0200

    Remove columns for migrate:fresh run

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,9 +26,6 @@ class CreateUsersTable extends Migration
 
             $table->string('phone_number')->default('');
 
-            $table->dateTime('last_visit')->nullable()->default(null);
-            $table->integer('visit_count')->default(0);
-
             $table->boolean('active')->default(1);
             $table->boolean('is_admin')->default(false);
 

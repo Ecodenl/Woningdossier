3d441c69f4c5f76df278e6dfad4b357a24dcdd40
Bodhi <bodhilooij1998@gmail.com>
2019-08-20 14:41:23 +0200

    Cleanup of link / scrips sources that are not used anyways

--- resources/views/cooperation/my-account/layouts/app.blade.php
+++ resources/views/cooperation/my-account/layouts/app.blade.php
@@ -60,21 +60,12 @@
 @endsection
 
 
-@prepend('css')
-    <link rel="stylesheet" href="{{asset('css/select2/select2.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/datatables/responsive.dataTables.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/datatables/dataTables.bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/datatables/responsive.bootstrap.min.css')}}">
-@endprepend
-@prepend('js')
+@push('js')
 
-    <script src="{{ asset('js/datatables.js') }}"></script>
     <script src="{{ asset('js/disable-auto-fill.js') }}"></script>
-    <script src="{{asset('js/select2.js')}}"></script>
     <script src="{{asset('js/hoomdossier.js')}}"></script>
 
     <script>
-
         $(document).ready(function () {
             Hoomdossier.pollForMessageCount();
 
@@ -84,11 +75,5 @@
                 $(this).parent().find(".glyphicon-chevron-up").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
             });
         });
-
-        $.extend( true, $.fn.dataTable.defaults, {
-            language: {
-                url: "{{asset('js/datatables-dutch.json')}}"
-            },
-        });
     </script>
-@endprepend
+@endpush

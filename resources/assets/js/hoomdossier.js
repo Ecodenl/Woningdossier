3c347cdb86cd239b3034b4f7a5756de621f44380
Bodhi <bodhilooij1998@gmail.com>
2019-08-21 09:15:23 +0200

    wip

--- resources/assets/js/hoomdossier.js
+++ resources/assets/js/hoomdossier.js
@@ -1,38 +1,30 @@
-class Hoomdossier {
+function updateTotalUnreadMessages () {
 
-    'use strict';
-
-    static updateTotalUnreadMessages()
-    {
-        $.ajax({
-            url: window.location.origin + '/messages/count',
-            method: 'GET',
-            success: (response) => {
-                $('#total-unread-message-count').html(response.count);
-            }
-        });
-    }
-
-    static set pollTimeout(timeout)
-    {
-        this.timeout = timeout
-    }
-
-
-    static pollForMessageCount()
-    {
-        // first the timeout is not set, so we set it to 0
-        // after that we timeout to 3 seconds.
-        if (!this.hasOwnProperty('timeout')) {
-            this.pollTimeout = 0;
-        } else {
-            this.pollTimeout = 15000;
+    $.ajax({
+        url: window.location.origin + '/messages/count',
+        method: 'GET',
+        success: function (response) {
+            $('#total-unread-message-count').html(response.count);
         }
+    });
+};
 
-        setTimeout(() => {
-            this.updateTotalUnreadMessages();
-            this.pollForMessageCount();
-        }, this.timeout)
-    }
+function pollForMessageCount () {
 
-}
+    var beenPolled = false;
+    // first the timeout is not set, so we set it to 0
+    // after that we timeout to 3 seconds.
+
+
+    var timeout = 10000;
+
+    setTimeout(function () {
+        if (beenPolled) {
+            console.log(10000)
+            timeout = 10000
+        }
+        beenPolled = true;
+        updateTotalUnreadMessages();
+        pollForMessageCount();
+    }, timeout);
+};

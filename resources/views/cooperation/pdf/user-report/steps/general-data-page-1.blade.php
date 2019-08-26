e581d5fcf627d713d9fb34deb0b6b2023618e146
Bodhi <bodhilooij1998@gmail.com>
2019-08-26 17:43:13 +0200

    Added check around foreach, blind commit.

--- resources/views/cooperation/pdf/user-report/steps/general-data-page-1.blade.php
+++ resources/views/cooperation/pdf/user-report/steps/general-data-page-1.blade.php
@@ -79,22 +79,24 @@
 
     <div class="question-answer-section">
         <p class="lead">{{\App\Helpers\Translation::translate('pdf/user-report.general-data.comment-usage-building')}}</p>
-        @foreach($commentsByStep['general-data'] as $inputSourceName => $commentsCategorizedUnderColumn)
-            {{-- The column can be a category, this will be the case when the comment is stored under a catergory --}}
-            @foreach($commentsCategorizedUnderColumn as $columnOrCategory => $comment)
-                <div class="question-answer">
-                    @if(is_array($comment))
-                        @foreach($comment as $column => $c)
-                            <p class="w-300">{{$inputSourceName}} ({{$columnOrCategory}})</p>
-                            <p>{{$c}}</p>
-                        @endforeach
-                    @else
-                        <p class="w-300">{{$inputSourceName}}</p>
-                        <p>{{$comment}}</p>
-                    @endif
-                </div>
+        @if(array_key_exists('general-data', $commentsByStep))
+            @foreach($commentsByStep['general-data'] as $inputSourceName => $commentsCategorizedUnderColumn)
+                {{-- The column can be a category, this will be the case when the comment is stored under a catergory --}}
+                @foreach($commentsCategorizedUnderColumn as $columnOrCategory => $comment)
+                    <div class="question-answer">
+                        @if(is_array($comment))
+                            @foreach($comment as $column => $c)
+                                <p class="w-300">{{$inputSourceName}} ({{$columnOrCategory}})</p>
+                                <p>{{$c}}</p>
+                            @endforeach
+                        @else
+                            <p class="w-300">{{$inputSourceName}}</p>
+                            <p>{{$comment}}</p>
+                        @endif
+                    </div>
+                @endforeach
             @endforeach
-        @endforeach
+        @endif
     </div>
 </div>
 

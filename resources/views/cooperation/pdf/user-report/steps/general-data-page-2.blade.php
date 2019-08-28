505b8de3baeab5e83c7f665aed29ff34b5a92610
Bodhi <bodhilooij1998@gmail.com>
2019-08-28 11:56:13 +0200

    cleanup

--- resources/views/cooperation/pdf/user-report/steps/general-data-page-2.blade.php
+++ resources/views/cooperation/pdf/user-report/steps/general-data-page-2.blade.php
@@ -11,27 +11,17 @@
         </tr>
         </thead>
         <tbody>
-        {{--
-            todo: these are just all the action plan advices, find out if these need to be the exact same as my plan 'Uw persoonlijke meerjarenonderhoudsplan'
-        --}}
         @foreach($userActionPlanAdvices as $year => $advices)
             @foreach($advices as $measureName => $adviceData)
                 @foreach($adviceData as $advice)
-            <?php
-//                 determine which year we will use
-//                $year = isset($userActionPlanAdvice->planned_year) ? $userActionPlanAdvice->planned_year : $userActionPlanAdvice->year;
-//                if (is_null($year)) {
-//                    $year = $userActionPlanAdvice->getAdviceYear() ?? __('woningdossier.cooperation.tool.my-plan.no-year');
-//                }
-            ?>
-            <tr>
-                <td>{{$year}}</td>
-                <td>{{$advice['interested'] ? 'Ja' : 'Nee'}}</td>
-                <td>{{$measureName}}</td>
-                <td>{{$advice['costs']}}</td>
-                <td>{{$advice['savings_money']}}</td>
-            </tr>
-            @endforeach
+                <tr>
+                    <td>{{$year}}</td>
+                    <td>{{$advice['interested'] ? 'Ja' : 'Nee'}}</td>
+                    <td>{{$measureName}}</td>
+                    <td>{{$advice['costs']}}</td>
+                    <td>{{$advice['savings_money']}}</td>
+                </tr>
+                @endforeach
             @endforeach
         @endforeach
         </tbody>

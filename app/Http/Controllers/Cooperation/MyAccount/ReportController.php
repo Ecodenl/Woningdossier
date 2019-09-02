20690b935f047374ab4a120a82fadbbcd0f9112f
Bodhi <bodhilooij1998@gmail.com>
2019-09-02 16:55:50 +0200

    changed the file name to the time-name-inputsource, so ex: 35464352156-demo-account-resident.pdf

--- app/Http/Controllers/Cooperation/MyAccount/ReportController.php
+++ app/Http/Controllers/Cooperation/MyAccount/ReportController.php
@@ -4,18 +4,14 @@ namespace App\Http\Controllers\Cooperation\MyAccount;
 
 use App\Helpers\Hoomdossier;
 use App\Helpers\HoomdossierSession;
-use App\Helpers\StepHelper;
-use App\Helpers\Str;
 use App\Jobs\PdfReport;
 use App\Models\Cooperation;
 use App\Models\FileStorage;
 use App\Models\FileType;
 use App\Models\FileTypeCategory;
-use App\Models\InputSource;
-use App\Models\User;
 use App\Scopes\AvailableScope;
 use App\Http\Controllers\Controller;
-use function Couchbase\defaultDecoder;
+use Illuminate\Support\Str;
 
 class ReportController extends Controller
 {
@@ -37,15 +33,12 @@ class ReportController extends Controller
             return redirect()->back();
         }
 
-        // create a short hash to prepend on the filename.
-        $substrBycrypted = substr(\Hash::make(Str::uuid()), 7, 5);
-        $substrUuid = substr(Str::uuid(), 0, 8);
-        $hash = $substrUuid.$substrBycrypted;
+        $inputSource = HoomdossierSession::getInputSource(true);
+        $user = Hoomdossier::user();
 
         // we will create the file storage here, if we would do it in the job itself it would bring confusion to the user.
-        // Because if there are multiple jobs in the queue, only the job thats being processed would show up as "generating"
-        // remove the / to prevent unwanted directories
-        $fileName = str_replace('/', '',  $hash . \Illuminate\Support\Str::slug($fileType->name) . '.pdf');
+
+        $fileName = time().'-'.Str::slug($user->getFullName()).'-'.$inputSource->name.'.pdf';
 
         // and delete the other available files, will trigger the observer to delete the file on disk
         foreach ($fileType->files as $fileStorage) {
@@ -55,7 +48,7 @@ class ReportController extends Controller
 
         // and we create the new file
         $fileStorage = FileStorage::create([
-            'user_id' => Hoomdossier::user()->id,
+            'user_id' => $user->id,
             'cooperation_id' => $cooperation->id,
             'file_type_id' => $fileType->id,
             'content_type' => 'application/pdf',
@@ -64,7 +57,7 @@ class ReportController extends Controller
 
         switch ($fileType->short) {
             case 'pdf-report':
-                PdfReport::dispatch(Hoomdossier::user(), HoomdossierSession::getInputSource(true), $fileType, $fileStorage);
+                PdfReport::dispatch(Hoomdossier::user(), $inputSource, $fileType, $fileStorage);
                 break;
 
         }

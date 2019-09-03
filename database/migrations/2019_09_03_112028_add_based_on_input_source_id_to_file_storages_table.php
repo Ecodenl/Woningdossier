b58436c393e0a4e5e3e94d2a99737faf1055c1af
Bodhi <bodhilooij1998@gmail.com>
2019-09-03 11:41:19 +0200

    Added based_on_input_source for the file storage, makes more sense then the input_source_id itself

--- database/migrations/2019_09_03_112028_add_based_on_input_source_id_to_file_storages_table.php
+++ database/migrations/2019_09_03_112028_add_based_on_input_source_id_to_file_storages_table.php
@@ -17,6 +17,16 @@ class AddBasedOnInputSourceIdToFileStoragesTable extends Migration
             $table->integer('based_on_input_source_id')->unsigned()->nullable()->default(1)->after('user_id');
             $table->foreign('based_on_input_source_id')->references('id')->on('input_sources')->onDelete('cascade');
         });
+
+        $fileStorages = DB::table('file_storages')->get();
+
+        $residentInputSourceId = DB::table('input_sources')->where('short', 'resident')->first()->id;
+        foreach ($fileStorages as $fileStorage) {
+            DB::table('file_storages')->where('id', $fileStorage->id)
+                ->update([
+                    'based_on_input_source_id' => $residentInputSourceId
+                ]);
+        }
     }
 
     /**

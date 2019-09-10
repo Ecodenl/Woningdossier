76e2221b5968faa7658840558898c9c1ec9211ea
Bodhi <bodhilooij1998@gmail.com>
2019-09-10 14:19:25 +0200

    Added the calculations for the rooftype

--- resources/views/cooperation/pdf/user-report/parts/indicative-costs-and-measures.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/indicative-costs-and-measures.blade.php
@@ -13,6 +13,20 @@
                     <td class="w-380">{{$translationForAnswer}}</td>
                     <td>{{(\App\Helpers\NumberFormatter::format($calculationResult, 0, true))}} {{\App\Helpers\Hoomdossier::getUnitForColumn($calculationType)}}</td>
                 </tr>
+
+            @elseif($stepSlug == 'roof-insulation')
+                <?php $roofType = $calculationType; ?>
+                @foreach($calculationResult as $calculationType => $result)
+                    @if(!empty($result) && !is_array($result))
+                        <?php
+                            $translationForAnswer = $reportTranslations[$stepSlug . '.calculation.'.$roofType.'.' . $calculationType];
+                        ?>
+                        <tr class="h-20">
+                            <td class="w-380">{{$translationForAnswer}}</td>
+                            <td>{{(\App\Helpers\NumberFormatter::format($result, 0, true))}} {{\App\Helpers\Hoomdossier::getUnitForColumn($calculationType)}}</td>
+                        </tr>
+                    @endif
+                @endforeach
             @endif
         @endforeach
         </tbody>

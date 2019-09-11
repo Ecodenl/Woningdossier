c18d3d8ed4032bbd233d95f831ba2c63e9b1b432
Bodhi <bodhilooij1998@gmail.com>
2019-09-11 11:04:38 +0200

    When there is a category, the width is set to 150. The category may be removed in the feature so then we can clean this up

--- resources/views/cooperation/pdf/user-report/parts/comments.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/comments.blade.php
@@ -4,17 +4,23 @@
     @foreach($commentsByStep[$stepSlug] as $inputSourceName => $commentsCategorizedUnderColumn)
         {{-- The column can be a category, this will be the case when the comment is stored under a catergory --}}
         @foreach($commentsCategorizedUnderColumn as $columnOrCategory => $comment)
-            <div class="question-answer">
-                @if(is_array($comment))
-                    @foreach($comment as $column => $c)
-                        <p class="w-100">{{$inputSourceName}} ({{$columnOrCategory}})</p>
-                        <p>{{$c}}</p>
-                    @endforeach
-                @else
-                    <p class="w-100">{{$inputSourceName}}</p>
-                    <p>{{$comment}}</p>
-                @endif
-            </div>
+            <table class="full-width">
+                <tbody>
+                    @if(is_array($comment))
+                        @foreach($comment as $column => $c)
+                            <tr class="h-20">
+                                <td class="w-150">{{$inputSourceName}} ({{$columnOrCategory}})</td>
+                                <td>{{$c}}</td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr class="h-20">
+                            <td class="w-100">{{$inputSourceName}}</td>
+                            <td>{{$comment}}</td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
         @endforeach
     @endforeach
 </div>

989784c6511bbc34b16e403eb8f9b45139fbe7f3
Bodhi <bodhilooij1998@gmail.com>
2019-09-12 15:39:07 +0200

    Get the most credible value for the interests on the insulated glazing, added todo that needs checking.

--- resources/views/cooperation/tool/insulated-glazing/index.blade.php
+++ resources/views/cooperation/tool/insulated-glazing/index.blade.php
@@ -62,6 +62,7 @@
                                                     @endif
                                                 @elseif(array_key_exists($measureApplication->id, $userInterests) && $interest->id == $userInterests[$measureApplication->id])
                                                     selected="selected"
+                                                {{--This elseif seems useless, todo: check--}}
                                                 @elseif($buildingOwner->getInterestedType('measure_application', $measureApplication->id) != null && $buildingOwner->getInterestedType('measure_application', $measureApplication->id)->interest_id == $interest->id)
                                                     selected="selected"
                                                 @elseif(!array_key_exists($measureApplication->id, $userInterests) && $interest->calculate_value == 4)

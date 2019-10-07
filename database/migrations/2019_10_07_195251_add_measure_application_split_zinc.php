e6917458600951acba9441d7437fb9cb4e2cd0d9
pvankouteren <pvankouteren@gmail.com>
2019-10-07 20:41:56 +0200

    Added down method

--- database/migrations/2019_10_07_195251_add_measure_application_split_zinc.php
+++ database/migrations/2019_10_07_195251_add_measure_application_split_zinc.php
@@ -47,55 +47,55 @@ class AddMeasureApplicationSplitZinc extends Migration
 
         DB::table('measure_applications')
           ->where('short', '=', 'replace-zinc')
-            ->update(
-                [
-                    'measure_name' => $mnUuid,
-                    'short' => 'replace-zinc-pitched',
-                    'costs' => 100,
-                    'cost_unit' => $cuUuid,
-                ]
-            );
+          ->update(
+              [
+                  'measure_name' => $mnUuid,
+                  'short'        => 'replace-zinc-pitched',
+                  'costs'        => 100,
+                  'cost_unit'    => $cuUuid,
+              ]
+          );
 
         // Flat
         // Measure name
         $flatMeasureName = DB::table('translations')
-            ->where('language', '=', 'nl')
-            ->where('translation', '=', 'Zinkwerk plat dak')
-            ->first();
+                             ->where('language', '=', 'nl')
+                             ->where('translation', '=', 'Zinkwerk plat dak')
+                             ->first();
 
-        if (!$flatMeasureName){
+        if ( ! $flatMeasureName) {
             $fmnUuid = Str::uuid();
             \DB::table('translations')->insert([
                 'key'         => $fmnUuid,
                 'language'    => 'nl',
                 'translation' => 'Zinkwerk plat dak',
             ]);
-        }
-        else {
+        } else {
             $fmnUuid = $flatMeasureName->key;
         }
 
         $maintenanceUnit = DB::table('translations')
-            ->where('language', '=', 'nl')
-            ->where('translation', '=', 'jaar')
-            ->first();
+                             ->where('language', '=', 'nl')
+                             ->where('translation', '=', 'jaar')
+                             ->first();
 
-        $step = DB::table('steps')->where('slug', '=', 'roof-insulation')->first();
+        $step = DB::table('steps')->where('slug', '=',
+            'roof-insulation')->first();
 
         DB::table('measure_applications')->updateOrInsert(
             [
                 'short' => 'replace-zinc-flat',
             ],
             [
-                'measure_type' => 'maintenance',
-                'measure_name' => $fmnUuid,
-                'application' => 'replace',
-                'costs' => 25,
-                'cost_unit' => $cuUuid,
-                'minimal_costs' => 250,
+                'measure_type'         => 'maintenance',
+                'measure_name'         => $fmnUuid,
+                'application'          => 'replace',
+                'costs'                => 25,
+                'cost_unit'            => $cuUuid,
+                'minimal_costs'        => 250,
                 'maintenance_interval' => 25,
-                'maintenance_unit' => $maintenanceUnit->key,
-                'step_id' => $step->id,
+                'maintenance_unit'     => $maintenanceUnit->key,
+                'step_id'              => $step->id,
             ]
         );
     }
@@ -107,6 +107,69 @@ class AddMeasureApplicationSplitZinc extends Migration
      */
     public function down()
     {
+        // cost unit creation
+        $costUnit = DB::table('translations')
+                      ->where('language', '=', 'nl')
+                      ->where('translation', '=', 'per m2')
+                      ->first();
 
+        if ( ! $costUnit) {
+            $cuUuid = Str::uuid();
+            \DB::table('translations')->insert([
+                'key'         => $cuUuid,
+                'language'    => 'nl',
+                'translation' => 'per m2',
+            ]);
+        } else {
+            $cuUuid = $costUnit->key;
+        }
+
+        // Pitched
+        // Measure name
+        DB::table('translations')
+          ->where('language', '=', 'nl')
+          ->where('translation', '=', 'Zinkwerk hellend dak')
+          ->update(['translation' => 'Zinkwerk']);
+
+        $mTrans = DB::table('translations')
+                    ->where('language', '=', 'nl')
+                    ->where('translation', '=', 'Zinkwerk')
+                    ->first();
+
+        $mnUuid = $mTrans->key;
+
+        DB::table('measure_applications')
+          ->where('short', '=', 'replace-zinc-pitched')
+          ->update(
+              [
+                  'measure_name' => $mnUuid,
+                  'short'        => 'replace-zinc',
+                  'costs'        => 125,
+                  'cost_unit'    => $cuUuid,
+              ]
+          );
+
+        $replaceZinc = DB::table('measure_applications')->where('short', '=',
+            'replace-zinc')->first();
+
+        $replaceZincFlat = DB::table('measure_applications')->where('short',
+            '=', 'replace-zinc-flat')->first();
+
+        $advicesToUpdate = DB::table('user_action_plan_advices')->where('measure_application_id',
+            '=', $replaceZincFlat->id)->get();
+
+        foreach ($advicesToUpdate as $adviceToUpdate) {
+            // costs replace zinc = 125, replace zinc flat = 25, so: in action plan advice, do costs * 5.
+            DB::table('user_action_plan_advices')
+              ->where('id', '=', $adviceToUpdate->id)
+              ->update([
+                  'measure_application_id' => $replaceZinc->id,
+                  'costs'                  => $adviceToUpdate->costs * 5,
+              ]);
+        }
+
+        // now delete the replace zinc flat measure application
+        DB::table('measure_applications')->where('short', '=',
+            'replace-zinc-flat')->delete();
     }
 }

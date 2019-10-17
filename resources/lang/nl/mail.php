d545066ba8f3c6000b4ef782f2471be9e32a2d32
pvankouteren <pvankouteren@gmail.com>
2019-10-17 16:16:08 +0200

    Name + kind regards translation fix

--- resources/lang/nl/mail.php
+++ resources/lang/nl/mail.php
@@ -25,6 +25,7 @@ return [
 
     'reset_password' => [
         'subject' => 'Wachtwoord reset aangevraagd',
+        'salutation' => 'Beste :first_name :last_name,',
         'why' => 'U ontvangt deze mail omdat iemand een wachtwoord reset heeft aangevraagd voor uw account.',
         'action' => 'U kunt uw wachtwoord resetten via de volgende URL:',
         'not_requested' => 'N.B.: Uit veiligheidsoverwegingen is deze link slechts eenmalig te gebruiken! Als u geen wachtwoord reset heeft aangevraagd hoeft u geen actie te ondernemen.',

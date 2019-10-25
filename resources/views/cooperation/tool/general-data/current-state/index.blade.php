80de51abe8b270bf5eebc73c45086957dd6941b3
Bodhi <bodhilooij1998@gmail.com>
2019-10-25 10:58:51 +0200

    Cleanup of html

--- resources/views/cooperation/tool/general-data/current-state/index.blade.php
+++ resources/views/cooperation/tool/general-data/current-state/index.blade.php
@@ -11,12 +11,9 @@
                     <div class="row">
                 @endif
                         <div class=" col-sm-4 ">
-                            <div class="form-group add-space{{ $errors->has('element.'.$element->id) ? ' has-error' : '' }}">
-                                <label for="element_{{ $element->id }}" class="control-label">
-                                    <i data-toggle="modal" data-target="#element_{{ $element->id }}-info" class="glyphicon glyphicon-info-sign glyphicon-padding collapsed" aria-expanded="false"></i>
-                                    @lang('general-data.element.'.$element->short.'.title')
-                                </label>
 
+
+                            @component('cooperation.tool.components.step-question', ['id' => 'element.'.$element->id, 'translation' => 'general-data.element.'.$element->short])
                                 @component('cooperation.tool.components.input-group', ['inputType' => 'select', 'inputValues' => $element->values, 'userInputValues' => $building->buildingElements()->forMe()->where('element_id', $element->id)->get(), 'userInputColumn' => 'element_value_id'])
                                     <select id="element_{{ $element->id }}" class="form-control" name="element[{{ $element->id }}]">
                                         @foreach($element->values as $elementValue)
@@ -24,17 +21,7 @@
                                         @endforeach
                                     </select>
                                 @endcomponent
-
-                                @component('cooperation.tool.components.help-modal')
-                                    @lang('general-data.element.'.$element->short.'.help')
-                                @endcomponent
-
-                                @if ($errors->has('element.' . $element->id))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('element.' . $element->id) }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @endcomponent
                         </div>
                     @if(!in_array($element->short, ['sleeping-rooms-windows', 'living-rooms-windows']))
                         </div>

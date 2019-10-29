ac24267fc9bdecb984ec675e75c3b668ff578427
Bodhi <bodhilooij1998@gmail.com>
2019-10-29 10:30:45 +0100

    Added migration to migrate the data from the old building services / hybrid - full heatpumps to the new heat pump service

--- app/Http/Controllers/Cooperation/Tool/GeneralData/CurrentStateController.php
+++ app/Http/Controllers/Cooperation/Tool/GeneralData/CurrentStateController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Cooperation\Tool\GeneralData;
 
 use App\Helpers\HoomdossierSession;
+use App\Models\BuildingService;
 use App\Models\Cooperation;
 use App\Models\Element;
+use App\Models\InputSource;
 use App\Models\Service;
 use App\Models\ServiceValue;
 use App\Models\UserInterest;
@@ -16,61 +18,6 @@ class CurrentStateController extends Controller
 {
     public function index(Cooperation $cooperation)
     {
-        $heatPumpService = \DB::table('services')->where('short', 'heat-pump')->first();
-        // the old services
-        $fullHeatPumpService = \DB::table('services')->where('short', 'full-heat-pump')->first();
-        $hybridHeatPump = \DB::table('services')->where('short', 'hybrid-heat-pump')->first();
-//        $serviceIds = $services->pluck('id')->toArray();
-
-        $buildings = \DB::table('buildings')->get();
-
-        foreach ($buildings as $building) {
-            // get the full and hybrid heat pump values for a building
-            $buildingServicesForAllHeatPumps = \DB::table('building_services')
-
-                ->where('building_id', $building->id)
-                ->where(function ($query) use ($fullHeatPumpService, $hybridHeatPump) {
-                    $query->where('service_id', $fullHeatPumpService->id)
-                        ->orWhere('service_id', $hybridHeatPump->id);
-                })->get();
-
-
-
-            // group them by the input source id, we will have to determine the service_value_id for each input source
-            $buildingServicesForHeatPumpGroupedByInputSourceId = [];
-            foreach ($buildingServicesForAllHeatPumps as $buildingServiceForHeatPump) {
-                $buildingServicesForHeatPumpGroupedByInputSourceId[$buildingServiceForHeatPump->input_source_id][$buildingServiceForHeatPump->service_id] = $buildingServiceForHeatPump;
-            }
-
-            foreach ($buildingServicesForHeatPumpGroupedByInputSourceId as $inputSourceId => $buildingServicesForHeatPumps) {
-                $answerForHybridHeatPump = $buildingServicesForHeatPumps[$hybridHeatPump->id];
-                $answerForFullHeatPump = $buildingServicesForHeatPumps[$fullHeatPumpService->id];
-
-                // todo: some exception occurs.
-                // get the calculate values for the selected service values
-                $calculateValueForHybridHeatPump = \DB::table('service_values')->where('id', $answerForHybridHeatPump->service_value_id)->first()->calculate_value;
-                $calculateValueForFullHeatPump = \DB::table('service_values')->where('id', $answerForFullHeatPump->service_value_id)->first()->calculate_value;
-
-                // in this case the user did not select any value so we will just leave it like that
-                if ($calculateValueForHybridHeatPump == 1 && $calculateValueForFullHeatPump == 1) {
-                    $newCalculateValueForAnswer = 1;
-
-
-                } else if ($calculateValueForHybridHeatPump == 2) {
-                    // since the calculate value for the hybrid pump changed assign it manually
-                    $newCalculateValueForAnswer = 4;
-                } else {
-                    // no custom answe for the hybrid pump was provider so we will take the one from the full heat pump
-                    $newCalculateValueForAnswer = $calculateValueForFullHeatPump;
-                }
-
-                dump($newCalculateValueForAnswer);
-            }
-        }
-
-
-
-
         $building = HoomdossierSession::getBuilding(true);
         $buildingOwner = $building->user;
         $userInterestsForMe = UserInterest::forMe()->get();
--- database/migrations/2019_10_28_160907_migrate_old_heat_pump_service_values_to_new_service_values_for_heat_pump_on_service_table.php
+++ database/migrations/2019_10_28_160907_migrate_old_heat_pump_service_values_to_new_service_values_for_heat_pump_on_service_table.php
@@ -13,7 +13,77 @@ class MigrateOldHeatPumpServiceValuesToNewServiceValuesForHeatPumpOnServiceTable
      */
     public function up()
     {
-        DB::table('services')->where('short', 'hybrid-heat-pump')->orWhere('full-heat-pump')->get();
+        // get the new heat pump service values
+        $heatPumpServiceValues = \DB::table('services')
+            ->select('service_values.*')
+            ->where('short', 'heat-pump')
+            ->leftJoin('service_values', 'services.id','=', 'service_values.service_id')
+            ->get()->keyBy('calculate_value');
+
+        // get the old heat pump services
+        $fullHeatPumpService = \DB::table('services')->where('short', 'full-heat-pump')->first();
+        $hybridHeatPump = \DB::table('services')->where('short', 'hybrid-heat-pump')->first();
+
+        $buildings = \DB::table('buildings')->get();
+        // modify data for all the buildings
+        foreach ($buildings as $building) {
+            // get the full and hybrid heat pump values for a building
+            $buildingServicesForAllHeatPumps = \DB::table('building_services')
+                ->where('building_id', $building->id)
+                ->where(function ($query) use ($fullHeatPumpService, $hybridHeatPump) {
+                    $query->where('service_id', $fullHeatPumpService->id)
+                        ->orWhere('service_id', $hybridHeatPump->id);
+                })->get();
+
+            // group them by the input source id, we will have to determine the service_value_id for each input source
+            $buildingServicesForHeatPumpGroupedByInputSourceId = [];
+            foreach ($buildingServicesForAllHeatPumps as $buildingServiceForHeatPump) {
+                $buildingServicesForHeatPumpGroupedByInputSourceId[$buildingServiceForHeatPump->input_source_id][$buildingServiceForHeatPump->service_id] = $buildingServiceForHeatPump;
+            }
+
+            foreach ($buildingServicesForHeatPumpGroupedByInputSourceId as $inputSourceId => $buildingServicesForHeatPumps) {
+                $answerForHybridHeatPump = $buildingServicesForHeatPumps[$hybridHeatPump->id];
+                $answerForFullHeatPump = $buildingServicesForHeatPumps[$fullHeatPumpService->id];
+
+                // when nothing its selected its a example building input source or some old account, skip it.
+                if (is_null($answerForHybridHeatPump->service_value_id) &&  is_null($answerForFullHeatPump->service_value_id)) {
+                    continue;
+                }
+                // get the calculate values for the selected service values
+                $calculateValueForHybridHeatPump = \DB::table('service_values')->where('id', $answerForHybridHeatPump->service_value_id)->first()->calculate_value;
+                $calculateValueForFullHeatPump = \DB::table('service_values')->where('id', $answerForFullHeatPump->service_value_id)->first()->calculate_value;
+
+                // some A.I will happen here.
+                // when both the calc values for the hybrid and full heat pump are 1 they are left with the default values, so we will leave it like that
+                // when the calc value for the hybrid pump is set to two the user has an hybrid pump, so we will set it to 4 since that's the new calc value for the hybrid pump
+                // else we will get the calc value from the full heat pump, those stayed the same
+
+                if ($calculateValueForHybridHeatPump == 1 && $calculateValueForFullHeatPump == 1) {
+                    $newCalculateValueForAnswer = 1;
+                } else if ($calculateValueForHybridHeatPump == 2) {
+                    $newCalculateValueForAnswer = 4;
+                } else {
+                    $newCalculateValueForAnswer = $calculateValueForFullHeatPump;
+                }
+
+                // and delete the old hybrid and full heat pump building services for the building
+                \DB::table('building_services')
+                    ->where('building_id', $building->id)
+                    ->where(function ($query) use ($fullHeatPumpService, $hybridHeatPump) {
+                        $query->where('service_id', $fullHeatPumpService->id)
+                            ->orWhere('service_id', $hybridHeatPump->id);
+                    })->delete();
+
+
+                // and insert the new rows.
+                \DB::table('building_services')->insert([
+                    'building_id' => $building->id,
+                    'input_source_id' => $inputSourceId,
+                    'service_id' => $heatPumpServiceValues->first()->service_id,
+                    'service_value_id' => $heatPumpServiceValues[$newCalculateValueForAnswer]->id
+                ]);
+            }
+        }
     }
 
     /**

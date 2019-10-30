b065f9b92d136abd6cc3b7cc36d15bef20405c6e
Bodhi <bodhilooij1998@gmail.com>
2019-10-30 13:41:21 +0100

    To prevent duplicate data on migrate:fresh

--- database/migrations/2019_10_30_133344_add_data_to_building_heating_applications_table.php
+++ database/migrations/2019_10_30_133344_add_data_to_building_heating_applications_table.php
@@ -13,57 +13,59 @@ class AddDataToBuildingHeatingApplicationsTable extends Migration
      */
     public function up()
     {
-        $buildingHeatingApplications = [
-            [
-                'names' => [
-                    'nl' => 'Met gewone radiatoren',
+        if (\App\Models\Step::where('short', 'general-data')->count() > 0) {
+            $buildingHeatingApplications = [
+                [
+                    'names' => [
+                        'nl' => 'Met gewone radiatoren',
+                    ],
+                    'short' => 'radiators',
+                    'calculate_value' => 1,
+                    'order' => 0,
                 ],
-                'short' => 'radiators',
-                'calculate_value' => 1,
-                'order' => 0,
-            ],
-            [
-                'names' => [
-                    'nl' => 'Met gewone radiatoren en vloerverwarming',
+                [
+                    'names' => [
+                        'nl' => 'Met gewone radiatoren en vloerverwarming',
+                    ],
+                    'short' => 'radiators-with-floor-heating',
+                    'calculate_value' => 2,
+                    'order' => 1,
                 ],
-                'short' => 'radiators-with-floor-heating',
-                'calculate_value' => 2,
-                'order' => 1,
-            ],
-            [
-                'names' => [
-                    'nl' => 'Met lage temperatuur radiatoren',
+                [
+                    'names' => [
+                        'nl' => 'Met lage temperatuur radiatoren',
+                    ],
+                    'short' => 'low-temperature-heater',
+                    'calculate_value' => 3,
+                    'order' => 2,
                 ],
-                'short' => 'low-temperature-heater',
-                'calculate_value' => 3,
-                'order' => 2,
-            ],
-            [
-                'names' => [
-                    'nl' => 'Met vloer- en/of wandverwarming',
+                [
+                    'names' => [
+                        'nl' => 'Met vloer- en/of wandverwarming',
+                    ],
+                    'short' => 'floor-wall-heating',
+                    'calculate_value' => 4,
+                    'order' => 3,
                 ],
-                'short' => 'floor-wall-heating',
-                'calculate_value' => 4,
-                'order' => 3,
-            ],
-        ];
+            ];
 
-        foreach ($buildingHeatingApplications as $buildingHeatingApplication) {
-            $uuid = \App\Helpers\Str::uuid();
-            foreach ($buildingHeatingApplication['names'] as $locale => $name) {
-                \DB::table('translations')->insert([
-                    'key'         => $uuid,
-                    'language'    => $locale,
-                    'translation' => $name,
+            foreach ($buildingHeatingApplications as $buildingHeatingApplication) {
+                $uuid = \App\Helpers\Str::uuid();
+                foreach ($buildingHeatingApplication['names'] as $locale => $name) {
+                    \DB::table('translations')->insert([
+                        'key'         => $uuid,
+                        'language'    => $locale,
+                        'translation' => $name,
+                    ]);
+                }
+
+                \DB::table('building_heating_applications')->insert([
+                    'name' => $uuid,
+                    'short' => $buildingHeatingApplication['short'],
+                    'calculate_value' => $buildingHeatingApplication['calculate_value'],
+                    'order' => $buildingHeatingApplication['order'],
                 ]);
             }
-
-            \DB::table('building_heating_applications')->insert([
-                'name' => $uuid,
-                'short' => $buildingHeatingApplication['short'],
-                'calculate_value' => $buildingHeatingApplication['calculate_value'],
-                'order' => $buildingHeatingApplication['order'],
-            ]);
         }
     }
 

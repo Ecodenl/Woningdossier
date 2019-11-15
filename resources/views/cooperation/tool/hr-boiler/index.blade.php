6b94302b3ec5d7112912e8efc4bdb725322d6551
Bodhi <bodhilooij1998@gmail.com>
2019-11-15 14:29:51 +0100

    Cleaned op the messy prev buttons, pdf is working in the old way.

--- resources/views/cooperation/tool/hr-boiler/index.blade.php
+++ resources/views/cooperation/tool/hr-boiler/index.blade.php
@@ -148,18 +148,6 @@
                         </ol>
                     </div>
                 </div>
-                <hr>
-                @if(!\App\helpers\HoomdossierSession::isUserObserving())
-                <div class="form-group add-space">
-                    <div class="">
-                        <a class="btn btn-success pull-left"
-                           href="{{ route('cooperation.tool.roof-insulation.index', ['cooperation' => $cooperation]) }}">@lang('default.buttons.prev')</a>
-                        <button type="submit" class="btn btn-primary pull-right">
-                            @lang('default.buttons.next')
-                        </button>
-                    </div>
-                </div>
-                    @endif
             </div>
         </div>
     </form>

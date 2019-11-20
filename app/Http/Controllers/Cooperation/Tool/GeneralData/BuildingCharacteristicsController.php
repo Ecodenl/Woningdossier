c0b240f6b03676c3237dfec60f24579388067897
Bodhi <bodhilooij1998@gmail.com>
2019-11-20 10:29:51 +0100

    Adjusted the getNextStep method and other method's that had inpact on the input source id

--- app/Http/Controllers/Cooperation/Tool/GeneralData/BuildingCharacteristicsController.php
+++ app/Http/Controllers/Cooperation/Tool/GeneralData/BuildingCharacteristicsController.php
@@ -29,8 +29,6 @@ class BuildingCharacteristicsController extends Controller
         $building = HoomdossierSession::getBuilding(true);
         $buildingOwner = $building->user;
 
-        dd($buildingOwner->completedQuestionnaires);
-
         $buildingTypes = BuildingType::all();
         $roofTypes = RoofType::all();
         $energyLabels = EnergyLabel::where('country_code', 'nl')->get();

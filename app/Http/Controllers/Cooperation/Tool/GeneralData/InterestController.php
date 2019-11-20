125e93c264b35f9cfe37ae569befa73a8f2d8ba2
Bodhi <bodhilooij1998@gmail.com>
2019-11-20 12:05:34 +0100

    Added some amazing logic to get the services on the left side and elements on the right side.

--- app/Http/Controllers/Cooperation/Tool/GeneralData/InterestController.php
+++ app/Http/Controllers/Cooperation/Tool/GeneralData/InterestController.php
@@ -8,6 +8,7 @@ use App\Helpers\HoomdossierSession;
 use App\Helpers\NumberFormatter;
 use App\Helpers\StepHelper;
 use App\Http\Requests\Cooperation\Tool\GeneralData\InterestFormRequest;
+use App\Models\Cooperation;
 use App\Models\Interest;
 use App\Models\Motivation;
 use App\Models\Service;
@@ -18,8 +19,44 @@ use App\Services\UserInterestService;
 
 class InterestController extends Controller
 {
-    public function index()
+    public function index(Cooperation $cooperation)
     {
+        $steps = $cooperation->getActiveOrderedSteps();
+
+        // we wont show the general data cause we cant give our interest for that
+        $steps = $steps->keyBy('short')->forget('general-data');
+        // used in the forloopt
+        $stepCount = $steps->count();
+
+
+        // we have to display the services on the left side, and elements on right side
+        // collect the steps in a service / element order
+        $servicesStepShorts = array_flip([
+            'high-efficiency-boiler', 'heat-pump', 'solar-panels', 'heater', 'ventilation'
+        ]);
+        $elementsStepShorts = array_flip([
+            'insulated-glazing', 'wall-insulation', 'floor-insulation', 'roof-insulation'
+        ]);
+
+        // filter the shorts
+        // when the step isnt found in the collection
+        // well then its turned of by the cooperation, so do not return it
+        $servicesStepShorts = array_filter($servicesStepShorts, function ($stepShort) use ($steps) {
+            if (is_null($steps->where('short', $stepShort)->first())) {
+                return false;
+            }
+            return true;
+        }, ARRAY_FILTER_USE_KEY);
+
+        $elementsStepShorts = array_filter($elementsStepShorts, function ($stepShort) use ($steps) {
+            if (is_null($steps->where('short', $stepShort)->first())) {
+                return false;
+            }
+            return true;
+        }, ARRAY_FILTER_USE_KEY);
+
+
+
         $building = HoomdossierSession::getBuilding(true);
         $buildingOwner = $building->user->load('stepInterests');
 
@@ -32,9 +69,11 @@ class InterestController extends Controller
 
         $interests = Interest::orderBy('order')->get();
 
+        // because the $steps is loaded with view composers and will be overwriten
+        $filteredSteps = $steps;
         return view('cooperation.tool.general-data.interest.index', compact(
             'interests', 'services', 'elements', 'motivations', 'userMotivations', 'userEnergyHabitsForMe',
-            'buildingOwner'
+            'buildingOwner', 'stepCount', 'servicesStepShorts', 'elementsStepShorts', 'filteredSteps'
         ));
     }
 
--- resources/views/cooperation/tool/general-data/interest/index.blade.php
+++ resources/views/cooperation/tool/general-data/interest/index.blade.php
@@ -6,47 +6,89 @@
 
 @section('step_content')
 
-    <form class="form-horizontal" method="POST" id="main-form" action="{{ route('cooperation.tool.general-data.interest.store') }}" autocomplete="off">
+    <form class="form-horizontal" method="POST" id="main-form"
+          action="{{ route('cooperation.tool.general-data.interest.store') }}" autocomplete="off">
         {{ csrf_field() }}
         <div class="row">
-            <?php
-                /** @var \Illuminate\Support\Collection $steps */
+            @for($i = 0; $i < $stepCount; $i++)
+                <?php
+                $serviceStepShort = key($servicesStepShorts);
+                $elementStepShort = key($elementsStepShorts);
 
-                // we wont show the general data cause we cant give our interest for that
-                // pull the ventilation information and push it back so its the last item in the collection
-                $steps = $steps->keyBy('short')->forget('general-data');
-                $ventilationInformation = $steps->pull('ventilation-information');
-                if (!is_null($ventilationInformation)) {
-                    $steps->push($ventilationInformation);
+                unset($elementsStepShorts[$elementStepShort], $servicesStepShorts[$serviceStepShort]);
+
+                $elementStep = null;
+                $serviceStep = null;
+                if (!empty($elementStepShort)) {
+                    $elementStep = $filteredSteps[$elementStepShort];
                 }
-            ?>
-            @foreach($steps as $step)
+
+                if (!empty($serviceStepShort)) {
+                    $serviceStep = $filteredSteps[$serviceStepShort];
+                }
+                ?>
                 <div class="col-sm-12 col-md-6 mt-20">
                     <div class="row">
-                        <div class="col-md-6">
-                            <img class="img-responsive pr-10 d-inline pull-left" src="{{asset('images/icons/'.$step->short.'.png')}}">
-                            <h4>@lang('cooperation/tool/general-data/interest.index.steps.'.$step->short.'.title')</h4>
-                        </div>
-                        <div class="col-md-6">
-                            @component('cooperation.tool.components.step-question', ['id' => 'user_interest', 'name' => 'user_interests.'.$step->id.'.interest_id'])
-                                @component('cooperation.tool.components.input-group', ['inputType' => 'select', 'inputValues' => $interests, 'userInputValues' => $buildingOwner->userInterestsForSpecificType(get_class($step), $step->id)->forMe()->get(), 'userInputColumn' => 'interest_id'])
-                                    <select id="user_interest" class="form-control" name="user_interests[{{$step->id}}][interest_id]">
-                                        @foreach($interests as $interest)
-                                            <option @if(old('user_interests.interest_id.*', \App\Helpers\Hoomdossier::getMostCredibleValue($buildingOwner->userInterestsForSpecificType(get_class($step), $step->id), 'interest_id')) == $interest->id) selected="selected" @endif value="{{ $interest->id }}">{{ $interest->name}}</option>
-                                        @endforeach
-                                    </select>
+                        @if(!is_null($serviceStep))
+
+                            <div class="col-md-6">
+                                <img class="img-responsive pr-10 d-inline pull-left"
+                                     src="{{asset('images/icons/'.$serviceStep->short.'.png')}}">
+                                <h4>@lang('cooperation/tool/general-data/interest.index.steps.'.$serviceStep->short.'.title')</h4>
+                            </div>
+                            <div class="col-md-6">
+                                @component('cooperation.tool.components.step-question', ['id' => 'user_interest', 'name' => 'user_interests.'.$serviceStep->id.'.interest_id'])
+                                    @component('cooperation.tool.components.input-group', ['inputType' => 'select', 'inputValues' => $interests, 'userInputValues' => $buildingOwner->userInterestsForSpecificType(get_class($serviceStep), $serviceStep->id)->forMe()->get(), 'userInputColumn' => 'interest_id'])
+                                        <select id="user_interest" class="form-control"
+                                                name="user_interests[{{$serviceStep->id}}][interest_id]">
+                                            @foreach($interests as $interest)
+                                                <option @if(old('user_interests.interest_id.*', \App\Helpers\Hoomdossier::getMostCredibleValue($buildingOwner->userInterestsForSpecificType(get_class($serviceStep), $serviceStep->id), 'interest_id')) == $interest->id) selected="selected"
+                                                        @endif value="{{ $interest->id }}">{{ $interest->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    @endcomponent
                                 @endcomponent
-                            @endcomponent
-                        </div>
+                            </div>
+                        @else
+                            <div class="col-md-offset-6"></div>
+                        @endif
                     </div>
                 </div>
-            @endforeach
+
+                <div class="col-sm-12 col-md-6 mt-20">
+                    <div class="row">
+                        @if(!is_null($elementStep))
+                            <div class="col-md-6">
+                                <img class="img-responsive pr-10 d-inline pull-left"
+                                     src="{{asset('images/icons/'.$elementStep->short.'.png')}}">
+                                <h4>@lang('cooperation/tool/general-data/interest.index.steps.'.$elementStep->short.'.title')</h4>
+                            </div>
+                            <div class="col-md-6">
+                                @component('cooperation.tool.components.step-question', ['id' => 'user_interest', 'name' => 'user_interests.'.$elementStep->id.'.interest_id'])
+                                    @component('cooperation.tool.components.input-group', ['inputType' => 'select', 'inputValues' => $interests, 'userInputValues' => $buildingOwner->userInterestsForSpecificType(get_class($elementStep), $elementStep->id)->forMe()->get(), 'userInputColumn' => 'interest_id'])
+                                        <select id="user_interest" class="form-control"
+                                                name="user_interests[{{$elementStep->id}}][interest_id]">
+                                            @foreach($interests as $interest)
+                                                <option @if(old('user_interests.interest_id.*', \App\Helpers\Hoomdossier::getMostCredibleValue($buildingOwner->userInterestsForSpecificType(get_class($elementStep), $elementStep->id), 'interest_id')) == $interest->id) selected="selected"
+                                                        @endif value="{{ $interest->id }}">{{ $interest->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    @endcomponent
+                                @endcomponent
+                            </div>
+                        @else
+                            <div class="col-md-offset-6"></div>
+                        @endif
+                    </div>
+                </div>
+            @endfor
         </div>
 
         <div class="row mt-20">
             <div class="col-sm-12">
                 <h4>
-                    <i data-target="#motivation-info" class="glyphicon glyphicon-info-sign glyphicon-padding collapsed" aria-expanded="false"></i>
+                    <i data-target="#motivation-info" class="glyphicon glyphicon-info-sign glyphicon-padding collapsed"
+                       aria-expanded="false"></i>
                     @lang('cooperation/tool/general-data/interest.index.motivation.title.title')
                 </h4>
             </div>
@@ -54,14 +96,15 @@
                 @lang('cooperation/tool/general-data/interest.index.motivation.title.help')
             @endcomponent
             <?php
-                $oldMotivations = old('motivations');
-                $motivationsToSelect = empty(is_array($oldMotivations) ? $oldMotivations : []) ? $userMotivations->pluck('motivation_id')->toArray() : $motivationsToSelect;
+            $oldMotivations = old('motivations');
+            $motivationsToSelect = empty(is_array($oldMotivations) ? $oldMotivations : []) ? $userMotivations->pluck('motivation_id')->toArray() : $motivationsToSelect;
             ?>
             <div class="col-sm-12">
                 @component('cooperation.tool.components.step-question', ['id' => 'user_motivations.id.*'])
                     <select id="motivation" class="form-control" name="user_motivations[id][]" multiple="multiple">
                         @foreach($motivations as $motivation)
-                            <option @if(in_array($motivation->id, $motivationsToSelect)) selected @endif value="{{$motivation->id}}">{{$motivation->name}}</option>
+                            <option @if(in_array($motivation->id, $motivationsToSelect)) selected
+                                    @endif value="{{$motivation->id}}">{{$motivation->name}}</option>
                         @endforeach
                     </select>
                 @endcomponent
@@ -70,11 +113,11 @@
         <div class="row mt-25">
             <div class="col-sm-6">
                 <?php
-                    $renovationPlanAnswerOptions = [
-                        1 => __('cooperation/tool/general-data/interest.index.motivation.renovation-plans.options.yes-within-2-year'),
-                        2 =>  __('cooperation/tool/general-data/interest.index.motivation.renovation-plans.options.yes-within-5-year'),
-                        0 =>  __('cooperation/tool/general-data/interest.index.motivation.renovation-plans.options.none')
-                    ]
+                $renovationPlanAnswerOptions = [
+                    1 => __('cooperation/tool/general-data/interest.index.motivation.renovation-plans.options.yes-within-2-year'),
+                    2 => __('cooperation/tool/general-data/interest.index.motivation.renovation-plans.options.yes-within-5-year'),
+                    0 => __('cooperation/tool/general-data/interest.index.motivation.renovation-plans.options.none')
+                ]
                 ?>
                 @component('cooperation.tool.components.step-question', ['id' => 'renovation-plans', 'translation' => 'cooperation/tool/general-data/interest.index.motivation.renovation-plans'])
                     @component('cooperation.tool.components.input-group', ['inputType' => 'select', 'inputValues' => $renovationPlanAnswerOptions, 'userInputValues' => $userEnergyHabitsForMe, 'userInputColumn' => 'element_value_id'])
@@ -89,7 +132,8 @@
             <div class="col-sm-6">
                 @component('cooperation.tool.components.step-question', ['id' => 'renovation-plans', 'translation' => 'cooperation/tool/general-data/interest.index.motivation.building-complaints'])
                     @component('cooperation.tool.components.input-group', ['inputType' => 'input', 'userInputValues' => $userEnergyHabitsForMe, 'userInputColumn' => 'element_value_id'])
-                        <input type="text" name="user_energy_habits[building_complaints]" class="form-control" value="{{\App\Helpers\Hoomdossier::getMostCredibleValue($buildingOwner->energyHabit(), 'building_complaints')}}">
+                        <input type="text" name="user_energy_habits[building_complaints]" class="form-control"
+                               value="{{\App\Helpers\Hoomdossier::getMostCredibleValue($buildingOwner->energyHabit(), 'building_complaints')}}">
                     @endcomponent
                 @endcomponent
             </div>

010c978ddc96c4b580fa9f5d3d2592f97d2fc596
Bodhi <bodhilooij1998@gmail.com>
2019-11-22 13:17:29 +0100

    trans for the short comment

--- resources/views/cooperation/pdf/user-report/parts/measure-page/comments.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/measure-page/comments.blade.php
@@ -1,6 +1,6 @@
 @isset($commentsByStep[$stepShort][$subStepShort])
 <div class="question-answer-section">
-    <p class="lead">{{__('pdf/user-report.measure-pages.comments')}}</p>
+    <p class="lead">@lang('pdf/user-report.measure-pages.comments.title')</p>
     @foreach($commentsByStep[$stepShort][$subStepShort] as $inputSourceName => $comment)
         {{-- The column can be a category, this will be the case when the comment is stored under a catergory --}}
         <table class="full-width">
@@ -8,7 +8,7 @@
                 @if(is_array($comment))
                     @foreach($comment as $short => $comment)
                         <tr class="h-20">
-                            <td class="w-100">{{$inputSourceName}} ({{$short}})</td>
+                            <td class="w-100">{{$inputSourceName}} (@lang("pdf/user-report.measure-pages.comments.short-translations.{$short}"))</td>
                             <td>{!!  nl2br($comment)!!}</td>
                         </tr>
                     @endforeach

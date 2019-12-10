569e3b2d270b8625b37fe1d8f4867ad1e767e282
Bodhi <bodhilooij1998@gmail.com>
2019-12-10 17:36:06 +0100

    Added some conditional logic to show or not to show data

--- resources/views/cooperation/pdf/user-report/steps/roof-insulation.blade.php
+++ resources/views/cooperation/pdf/user-report/steps/roof-insulation.blade.php
@@ -27,45 +27,68 @@
 
         @include('cooperation.pdf.user-report.parts.measure-page.insulation-advice')
 
-        @if(!\App\Helpers\Arr::isWholeArrayEmpty($calculationsForStep))
+        <?php
+            // this will check if ALL calcs are empty, we still have to check the specific by typ.
+            // ex: flat can be empty but pitched wont
+            $calculationsAreEmpty = \App\Helpers\Arr::isWholeArrayEmpty($calculationsForStep);
+            $nonEmptyRoofInsulationCalculations = [];
+        ?>
+        @if(!$calculationsAreEmpty)
             {{-- Indicative costs and measures  --}}
             <div class="question-answer-section">
                 @foreach($calculationsForStep as $roofTypeShort => $calculationResultsForRoofType)
-                    <p class="lead">{{__('pdf/user-report.roof-insulation.indicative-costs-and-benefits-for-measure.'.$roofTypeShort)}}</p>
-                    <table class="full-width">
-                        <tbody>
-                        @foreach($calculationResultsForRoofType as $calculationType => $result)
-                            <?php
-                            $calculationTypesThatCantBeShown = ['type'];
-                            $calculationTypeNeedToBeShown = !in_array($calculationType, $calculationTypesThatCantBeShown);
-                            $calculationTypeIsYear = \App\Helpers\Hoomdossier::columnContains($calculationType, 'year');
-                            ?>
-                            @if(!empty($result) && !is_array($result) && $calculationTypeNeedToBeShown)
-
+                    <?php
+                        // we wont show that here
+                        unset($calculationResultsForRoofType['replace']);
+                    ?>
+                    @if(!\App\Helpers\Arr::isWholeArrayEmpty($calculationResultsForRoofType))
+                        <?php $nonEmptyRoofInsulationCalculations[] = $roofTypeShort ?>
+                        <p class="lead">{{__('pdf/user-report.roof-insulation.indicative-costs-and-benefits-for-measure.'.$roofTypeShort)}}</p>
+                        <table class="full-width">
+                            <tbody>
+                            @foreach($calculationResultsForRoofType as $calculationType => $result)
                                 <?php
-                                $translationForAnswer = $reportTranslations[$stepShort . '.' . $subStepShort . '.calculation.' . $roofTypeShort . '.' . $calculationType];
+                                $calculationTypesThatCantBeShown = ['type'];
+                                $calculationTypeNeedToBeShown = !in_array($calculationType, $calculationTypesThatCantBeShown);
+                                $calculationTypeIsYear = \App\Helpers\Hoomdossier::columnContains($calculationType, 'year');
                                 ?>
+                                @if(!empty($result) && $calculationTypeNeedToBeShown)
 
-                                <tr class="h-20">
-                                    <td class="w-380">{{$translationForAnswer}}</td>
-                                    <td>{{$calculationTypeIsYear ? $result : \App\Helpers\NumberFormatter::format($result, 0, true)}} {{\App\Helpers\Hoomdossier::getUnitForColumn($calculationType)}}</td>
-                                </tr>
-                            @endif
-                        @endforeach
-                        </tbody>
-                    </table>
+                                    <?php
+                                    $translationForAnswer = $reportTranslations[$stepShort . '.' . $subStepShort . '.calculation.' . $roofTypeShort . '.' . $calculationType];
+                                    ?>
+
+                                    <tr class="h-20">
+                                        <td class="w-380">{{$translationForAnswer}}</td>
+                                        <td>{{$calculationTypeIsYear ? $result : \App\Helpers\NumberFormatter::format($result, 0, true)}} {{\App\Helpers\Hoomdossier::getUnitForColumn($calculationType)}}</td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 @endforeach
             </div>
+        @endif
+
+        {{-- When there is only 1 calculation  result we can show this, and otherwise its pech hebben. --}}
+        @if(count($calculationsForStep) <= 1 || count($nonEmptyRoofInsulationCalculations) < 2)
+            @include('cooperation.pdf.user-report.parts.measure-page.advices')
+
+            @include('cooperation.pdf.user-report.parts.measure-page.comments')
         @endif
     </div>
 
 @endcomponent
 
-@component('cooperation.pdf.components.new-page')
+{{-- When there are multiple calcualtion results there wont be enough space on 1 page to display all the info --}}
+@if(count($calculationsForStep) > 1 || count($nonEmptyRoofInsulationCalculations) > 1)
+    @component('cooperation.pdf.components.new-page')
 
-    <div class="container">
-        @include('cooperation.pdf.user-report.parts.measure-page.advices')
+        <div class="container">
+            @include('cooperation.pdf.user-report.parts.measure-page.advices')
 
-        @include('cooperation.pdf.user-report.parts.measure-page.comments')
-    </div>
-@endcomponent
+            @include('cooperation.pdf.user-report.parts.measure-page.comments')
+        </div>
+    @endcomponent
+@endif

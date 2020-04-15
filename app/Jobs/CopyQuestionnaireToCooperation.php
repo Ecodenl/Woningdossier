1ccbc0616aeac80f3f61e1e881742b3b2ae7a7c5
Bodhi <bodhilooij1998@gmail.com>
2020-04-15 09:46:19 +0200

    Added test for the copy method of the QuestionnaireService.php. Made the QuestionFactory.php and QuestionnaireFactory.php work, handy with testing. Job uses the services

--- app/Jobs/CopyQuestionnaireToCooperation.php
+++ app/Jobs/CopyQuestionnaireToCooperation.php
@@ -6,6 +6,7 @@ use App\Models\Cooperation;
 use App\Models\Question;
 use App\Models\Questionnaire;
 use App\Models\QuestionOption;
+use App\Services\QuestionnaireService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -34,6 +35,9 @@ class CopyQuestionnaireToCooperation implements ShouldQueue
     /**
      * Create a new job instance.
      *
+     * @param Cooperation $cooperation
+     * @param Questionnaire $questionnaire
+     *
      * @return void
      */
     public function __construct(Cooperation $cooperation, Questionnaire $questionnaire)
@@ -49,38 +53,6 @@ class CopyQuestionnaireToCooperation implements ShouldQueue
      */
     public function handle()
     {
-        $questionnaire = $this->questionnaire;
-        $cooperationId = $this->cooperation->id;
-
-        /** @var Questionnaire $questionnaireToReplicate */
-        $questionnaireToReplicate = $questionnaire->replicate();
-
-        // for now this will do it as there is only one translation which is dutch.
-        // we MUST create a new translation because this will generate a new record in the translations table
-        // this way each cooperation can edit the question names without messing up the other cooperation its questionnaires.
-        $questionnaireToReplicate->cooperation_id = $cooperationId;
-        $questionnaireToReplicate->createTranslations('name', ['nl' => $questionnaire->name]);
-        $questionnaireToReplicate->is_active = false;
-        $questionnaireToReplicate->save();
-
-        // here we will replicate all the questions with the new translation, questionnaire id and question options.
-        foreach ($questionnaire->questions as $question) {
-
-            /** @var Question $questionToReplicate */
-            $questionToReplicate = $question->replicate();
-            $questionToReplicate->questionnaire_id = $questionnaireToReplicate->id;
-            $questionToReplicate->createTranslations('name', ['nl' => $question->name]);
-            $questionToReplicate->save();
-
-            // now replicate the question options and change the question id to the replicated question.
-            foreach ($question->questionOptions as $questionOption) {
-                /** @var QuestionOption $questionOptionToReplicate */
-                $questionOptionToReplicate = $questionOption->replicate();
-                $questionOptionToReplicate->createTranslations('name', ['nl' => $questionOption->name]);
-                $questionOptionToReplicate->question_id = $questionToReplicate->id;
-                $questionOptionToReplicate->save();
-
-            }
-        }
+        QuestionnaireService::copyQuestionnaireToCooperation($this->cooperation, $this->questionnaire);
     }
 }
--- app/Services/QuestionnaireService.php
+++ app/Services/QuestionnaireService.php
@@ -13,6 +13,41 @@ use Ramsey\Uuid\Uuid;
 
 class QuestionnaireService {
 
+    public static function copyQuestionnaireToCooperation(Cooperation $cooperation, Questionnaire $questionnaire)
+    {
+        $cooperationId = $cooperation->id;
+
+        /** @var Questionnaire $questionnaireToReplicate */
+        $questionnaireToReplicate = $questionnaire->replicate();
+
+        // for now this will do it as there is only one translation which is dutch.
+        // we MUST create a new translation because this will generate a new record in the translations table
+        // this way each cooperation can edit the question names without messing up the other cooperation its questionnaires.
+        $questionnaireToReplicate->cooperation_id = $cooperationId;
+        $questionnaireToReplicate->createTranslations('name', ['nl' => $questionnaire->name]);
+        $questionnaireToReplicate->is_active = false;
+        $questionnaireToReplicate->save();
+
+        // here we will replicate all the questions with the new translation, questionnaire id and question options.
+        foreach ($questionnaire->questions as $question) {
+
+            /** @var Question $questionToReplicate */
+            $questionToReplicate = $question->replicate();
+            $questionToReplicate->questionnaire_id = $questionnaireToReplicate->id;
+            $questionToReplicate->createTranslations('name', ['nl' => $question->name]);
+            $questionToReplicate->save();
+
+            // now replicate the question options and change the question id to the replicated question.
+            foreach ($question->questionOptions as $questionOption) {
+                /** @var QuestionOption $questionOptionToReplicate */
+                $questionOptionToReplicate = $questionOption->replicate();
+                $questionOptionToReplicate->createTranslations('name', ['nl' => $questionOption->name]);
+                $questionOptionToReplicate->question_id = $questionToReplicate->id;
+                $questionOptionToReplicate->save();
+
+            }
+        }
+    }
 
     /**
      * Method to create a laravel validation rule for a given question.

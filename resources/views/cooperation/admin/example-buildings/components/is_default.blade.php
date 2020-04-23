c71ec05c12834b8798e872f8d1bfc4e5cbb77c1a
Bodhi <bodhilooij1998@gmail.com>
2020-04-23 09:21:28 +0200

    Translated the is_default.blade.php options translated name, value and the build year input

--- resources/views/cooperation/admin/example-buildings/components/is_default.blade.php
+++ resources/views/cooperation/admin/example-buildings/components/is_default.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
-    <label for="is_default">@lang('cooperation/admin/example-buildings.components.is-default')</label>
+    <label for="is_default">@lang('cooperation/admin/example-buildings.components.is-default.label')</label>
     <select name="is_default" id="is_default" class="form-control">
-        @foreach([0 => 'No', 1 => 'Yes'] as $val => $string)
+        @foreach(__('cooperation/admin/example-buildings.components.is-default.options') as $val => $string)
             <option value="{{ $val }}" @if(isset($exampleBuilding) && $exampleBuilding->is_default == $val)selected="selected"@endif>{{ $string }}</option>
         @endforeach
     </select>

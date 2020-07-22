1d519e82e16e84f5127ab1ec6dd40ed09800868d
Bodhi <bodhilooij1998@gmail.com>
2020-07-22 10:03:18 +0200

    Adds migration to migrate the vrijstad cooperation to rivierenland

---
+++ database/migrations/2020_07_22_094342_migrate_cooperation_vrijstad_to_rivierenland.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrateCooperationVrijstadToRivierenland extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $rivierenLand = DB::table('cooperations')->where('slug', '=', 'hnwr')->first();
+        // cooperation to remove.
+        $vrijstadEnergie = DB::table('cooperations')->where('slug', '=', 'vrijstadenergie')->first();
+
+        // only migrate the pdf file file types.
+        // cvs dont matter at this point
+        DB::table('file_types')
+            ->select('file_storages.*')
+            ->where('content_type', 'application/pdf')
+            ->where('cooperation_id', $vrijstadEnergie->id)
+            ->leftJoin('file_storages', 'file_types.id', '=', 'file_storages.file_type_id')
+            ->update(['cooperation_id' => $rivierenLand->id]);
+
+        // after the update we can delete the remaining file storages
+        DB::table('file_storages')->where('cooperation_id', $vrijstadEnergie->id)->delete();
+
+        // remove the cooperation steps for the old cooperation
+        DB::table('cooperation_steps')->where('cooperation_id', $vrijstadEnergie)->delete();
+
+        // update the users to the new cooperation
+        DB::table('users')
+            ->where('cooperation_id', $vrijstadEnergie)
+            ->update(['cooperation_id' => $rivierenLand->id]);
+
+
+        // and finally delete the cooperation
+        DB::table('cooperations')->where('slug', '=', 'vrijstadenergie')->delete();
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

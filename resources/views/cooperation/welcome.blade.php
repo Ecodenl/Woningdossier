35af3f5dd884c468bb37c4fb01472c3c66c49149
Bodhi <bodhilooij1998@gmail.com>
2020-10-07 16:31:19 +0200

    Removing raleway

--- resources/views/cooperation/welcome.blade.php
+++ resources/views/cooperation/welcome.blade.php
@@ -6,7 +6,6 @@
         html, body {
             background-color: #fff;
             color: #636b6f;
-            font-family: 'Raleway', sans-serif;
             font-weight: 100;
             height: 100vh;
             margin: 0;

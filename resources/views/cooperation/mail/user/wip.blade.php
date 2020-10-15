1a4de5c82b378a2ca7984882f4950f1fb29d2377
Bodhi <bodhilooij1998@gmail.com>
2020-10-15 16:13:35 +0200

    medioker formatting

--- resources/views/cooperation/mail/user/wip.blade.php
+++ resources/views/cooperation/mail/user/wip.blade.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,10 +12,12 @@
 
     <tr>
         <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-            <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+            <table class="content" width="100%" cellpadding="0" cellspacing="0"
+                   style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
                 <tr>
                     <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;">
-                        <a href="http://woondossier.vm" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
+                        <a href="http://woondossier.vm"
+                           style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
                             Hoomdossier
                         </a>
                     </td>
@@ -25,13 +28,23 @@
 
     <!-- Email Body -->
     <tr>
-        <td align="center" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; border-bottom: 1px solid #EDEFF2; border-top: 1px solid #EDEFF2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
+        <td align="center" width="100%" cellpadding="0" cellspacing="0"
+            style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; border-bottom: 1px solid #EDEFF2; border-top: 1px solid #EDEFF2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0"
+                   style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
                 <!-- Body content -->
                 <tr>
-                    <td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                        <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Beste bodhi looij,<br></p>
-                        <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Er is een account voor u aangemaakt op <a href="http://hoom.woondossier.vm/home" target="_blank" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;"></a><a href="http://hoom.woondossier.vm/home" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">http://hoom.woondossier.vm/home</a><br><br> Bevestig uw account door onderstaande link te volgen:</p>
+                    <td class="content-cell"
+                        style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+                        <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+                            Beste bodhi looij,<br></p>
+                        <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+                            Er is een account voor u aangemaakt op <a href="http://hoom.woondossier.vm/home"
+                                                                      target="_blank"
+                                                                      style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;"></a><a
+                                    href="http://hoom.woondossier.vm/home"
+                                    style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">http://hoom.woondossier.vm/home</a><br><br>
+                            Bevestig uw account door onderstaande link te volgen:</p>
                         <!-- START CENTERED BUTTON -->
                         <center>
                             <table width="100%" style="font-family: Avenir, Helvetica, sans-serif;">
@@ -39,19 +52,24 @@
                                     <td>
                                         <table border="0" cellpadding="0" cellspacing="0">
                                             <tr>
-                                                <td height="20" width="100%" style="font-size: 20px; line-height: 20px;">
+                                                <td height="20" width="100%"
+                                                    style="font-size: 20px; line-height: 20px;">
                                                     &nbsp;
                                                 </td>
                                             </tr>
                                         </table>
-                                        <table border="0" align="center" cellpadding="0" cellspacing="0" style="margin:0 auto;">
+                                        <table border="0" align="center" cellpadding="0" cellspacing="0"
+                                               style="margin:0 auto;">
                                             <tbody>
                                             <tr>
                                                 <td align="center">
-                                                    <table border="0" cellpadding="0" cellspacing="0" style="margin:0 auto;">
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                           style="margin:0 auto;">
                                                         <tr>
                                                             <td align="center" bgcolor="#3498DB" width="150">
-                                                                <a href="http://www.example.com" style="padding: 10px;width:150px;display: block;text-decoration: none;border:0;text-align: center ;font-family: Calibri, sans-serif, 'Open Sans';color: #ffffff;background: #3498DB;border: 1px solid #3498DB;line-height:17px;" class="button_link">
+                                                                <a href="http://www.example.com"
+                                                                   style="padding: 10px;width:150px;display: block;text-decoration: none;border:0;text-align: center ;font-family: Calibri, sans-serif, 'Open Sans';color: #ffffff;background: #3498DB;border: 1px solid #3498DB;line-height:17px;"
+                                                                   class="button_link">
                                                                     Bevestig account
                                                                 </a>
                                                             </td>
@@ -63,7 +81,8 @@
                                         </table>
                                         <table border="0" cellpadding="0" cellspacing="0">
                                             <tr>
-                                                <td height="20" width="100%" style="font-size: 20px; line-height: 20px;">
+                                                <td height="20" width="100%"
+                                                    style="font-size: 20px; line-height: 20px;">
                                                     &nbsp;
                                                 </td>
                                             </tr>
@@ -81,17 +100,21 @@
                         <p style="word-break:break-all; font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
 
 
-                            <a href="" style=" display: block !important; max-width: 570px; font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">
+                            <a href=""
+                               style=" display: block !important; max-width: 570px; font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">
                                 http://hoom.woondossier.vm/password/reset/7dc7d4a59eb923606129182d2beaaa361ad696dac8fb77de988a03ca2e7874c4/eyJpdiI6Im5BaklDNVhKMkpqKytERVV6bmk2WkE9PSIsInZhbHVlIjoiTjhFWHpRMkJjZnEzYkNpd1wvU3Yrd0dLS0lnQndjdUZwNEV5aWtTa0Q2bk09IiwibWFjIjoiMDBjOWIyOTEyY2NkYmJlYzYxNWI2ODQzZDc1MTg2NGIwNWU1Njk3MzY3Njc4ZDA3ZWNmZjc5YmM1NDFlMmIwMyJ9
                             </a>
                         </p>
 
                 <tr>
                     <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
+                        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0"
+                               style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
                             <tr>
-                                <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #AEAEAE; font-size: 12px; text-align: center;">© 2020 Hoomdossier. All rights reserved.</p>
+                                <td class="content-cell" align="center"
+                                    style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #AEAEAE; font-size: 12px; text-align: center;">
+                                        © 2020 Hoomdossier. All rights reserved.</p>
                                 </td>
                             </tr>
                         </table>
@@ -99,9 +122,6 @@
                 </tr>
 
                 <br><br>
-
-                </td>
-
 
                 <!-- START CENTERED BUTTON -->
 

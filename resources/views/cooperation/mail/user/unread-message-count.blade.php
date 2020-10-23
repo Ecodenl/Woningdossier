b1ff08394a7e660556d064c8b4e22824e0f9a683
Bodhi <bodhilooij1998@gmail.com>
2020-10-23 15:40:23 +0200

    Unread message email

--- resources/views/cooperation/mail/user/unread-message-count.blade.php
+++ resources/views/cooperation/mail/user/unread-message-count.blade.php
@@ -1,33 +1,40 @@
-@component('mail::message')
+@component('cooperation.mail.components.message')
 
-<?php
-$cooperationHoomdossierLink = route('cooperation.auth.login', ['cooperation' => $userCooperation]);
+    <?php
+    $cooperationHoomdossierLink = route('cooperation.auth.login', ['cooperation' => $userCooperation]);
+    ?>
 
-$href = is_null($userCooperation->cooperation_email) ? $userCooperation->website_url : "mailto:".$userCooperation->cooperation_email;
-$cooperationWebsiteHref = '<a target="_blank" href="'.$href.'">'.$userCooperation->name.'</a>'
-?>
+    @component('cooperation.mail.components.text')
+        @lang('cooperation/mail/unread-message-count.salutation', [
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name
+        ])
+    @endcomponent
 
-@lang('cooperation/mail/unread-message-count.salutation', [
-    'first_name' => $user->first_name,
-    'last_name' => $user->last_name
-])
-@lang('cooperation/mail/unread-message-count.text', [
-    'unread_message_count' => $unreadMessageCount,
-])
+    @component('cooperation.mail.components.text')
+        @lang('cooperation/mail/unread-message-count.text', [
+            'unread_message_count' => $unreadMessageCount,
+        ])
+    @endcomponent
 
-@component('mail::button', ['url' => $cooperationHoomdossierLink])
-    @lang('cooperation/mail/unread-message-count.button')
-@endcomponent
+    @component('cooperation.mail.parts.centered-button', ['href' => $cooperationHoomdossierLink])
+        @lang('cooperation/mail/unread-message-count.button')
+    @endcomponent
 
-@lang('cooperation/mail/unread-message-count.button-does-not-work')
-<br>
-<a target="_blank" href="{{$cooperationHoomdossierLink}}">http://{{$userCooperation->slug}}.{{config('app.domain')}} </a>
-<br>
-<br>
-@lang('cooperation/mail/unread-message-count.if-you-have-any-questions', [
-    'cooperation_link' => $cooperationWebsiteHref
-])
-<br>
-<br>
-@lang('cooperation/mail/unread-message-count.kind_regards', ['app_name' => config('app.name')])
+    @component('cooperation.mail.components.text')
+        @lang('cooperation/mail/unread-message-count.button-does-not-work')
+        @include('cooperation.mail.parts.ahref', ['href' => $cooperationHoomdossierLink])
+    @endcomponent
+
+    @component('cooperation.mail.components.text')
+        @lang('cooperation/mail/unread-message-count.if-you-have-any-questions', [
+            'cooperation_link' =>  View::make('cooperation.mail.parts.ahref', [
+                'href' => is_null($userCooperation->cooperation_email) ? $userCooperation->website_url : "mailto:".$userCooperation->cooperation_email,
+            ])
+        ])
+    @endcomponent
+
+    @component('cooperation.mail.components.text')
+        @lang('cooperation/mail/unread-message-count.kind_regards', ['app_name' => config('app.name')])
+    @endcomponent
 @endcomponent

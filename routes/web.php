9e18d30ae49bf35fab56fe3932e55e17f3b834b9
Bodhi <bodhilooij1998@gmail.com>
2020-10-26 11:06:09 +0100

    Turned of the mail debug, just in case

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,12 @@ Route::domain('{cooperation}.' . config('hoomdossier.domain'))->group(function (
 
         if (app()->environment() == 'local') {
             Route::get('mail', function () {
-
 //            return new \App\Mail\UserCreatedEmail(\App\Models\Cooperation::find(1), \App\Models\User::find(1), 'sdfkhasgdfuiasdgfyu');
 //            return new \App\Mail\UserAssociatedWithCooperation(\App\Models\Cooperation::find(1), \App\Models\User::find(1));
 //            return new \App\Mail\UserChangedHisEmail(\App\Models\User::find(1), \App\Models\Account::find(1), 'demo@eg.com', 'bier@pils.com');
 //            return new  \App\Mail\UnreadMessagesEmail(\App\Models\User::find(1), \App\Models\Cooperation::find(1), 10);
 //            return new \App\Mail\ResetPasswordRequest(\App\Models\Cooperation::find(1), \App\Models\Account::find(1), 'sfklhasdjkfhsjkf');
-            return new \App\Mail\RequestAccountConfirmationEmail(\App\Models\User::find(1), \App\Models\Cooperation::find(1));
+//            return new \App\Mail\RequestAccountConfirmationEmail(\App\Models\User::find(1), \App\Models\Cooperation::find(1));
             });
         }
 

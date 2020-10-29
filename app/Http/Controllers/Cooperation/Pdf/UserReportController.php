768371af8d91fb35a8fededb67b6e07b3baf8e86
pvankouteren <pvankouteren@gmail.com>
2020-10-29 15:57:56 +0100

    icons should be outlined better now

--- app/Http/Controllers/Cooperation/Pdf/UserReportController.php
+++ app/Http/Controllers/Cooperation/Pdf/UserReportController.php
@@ -91,17 +91,17 @@ class UserReportController extends Controller
 
         /** @var \Barryvdh\DomPDF\PDF $pdf */
         $pdf = PDF::loadView('cooperation.pdf.user-report.index', compact(
-            'user', 'building', 'userCooperation', 'stepShorts', 'inputSource', 'measuresToCheckForCorrespondingPlannedYear',
+            'user', 'building', 'userCooperation', 'stepShorts', 'inputSource',
             'commentsByStep', 'reportTranslations', 'reportData', 'userActionPlanAdvices', 'reportForUser', 'noInterest',
             'buildingFeatures', 'measures', 'steps', 'userActionPlanAdviceComments', 'buildingInsulatedGlazings', 'calculations'
         ));
 
-        return $pdf->stream();
+        //return $pdf->stream();
 
         return view('cooperation.pdf.user-report.index', compact(
-            'user', 'building', 'userCooperation', 'stepSlugs', 'inputSource',
-            'commentsByStep', 'reportTranslations', 'reportData', 'userActionPlanAdvices',
-            'buildingFeatures', 'advices', 'steps', 'userActionPlanAdviceComments', 'buildingInsulatedGlazings'
+            'user', 'building', 'userCooperation', 'stepShorts', 'inputSource',
+            'commentsByStep', 'reportTranslations', 'reportData', 'userActionPlanAdvices', 'reportForUser', 'noInterest',
+            'buildingFeatures', 'measures', 'steps', 'userActionPlanAdviceComments', 'buildingInsulatedGlazings', 'calculations'
         ));
     }
 }

a39675af4913f74639a457aa9e47dd6cde818db6
Bodhi <bodhilooij1998@gmail.com>
2020-11-06 10:41:44 +0100

    App url in the header.

--- resources/views/cooperation/mail/components/message.blade.php
+++ resources/views/cooperation/mail/components/message.blade.php
@@ -15,7 +15,7 @@
             <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
                 <tr>
                     <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;">
-                        <a href="http://woondossier.vm" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
+                        <a href="{{config('app.url')}}" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
                             Hoomdossier
                         </a>
                     </td>

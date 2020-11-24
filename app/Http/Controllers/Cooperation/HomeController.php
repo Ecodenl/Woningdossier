09b1253b68974204f5f6030bd308ea0199df10d5
Bodhi <bodhilooij1998@gmail.com>
2020-11-24 09:31:32 +0100

    Removed dd()

--- app/Http/Controllers/Cooperation/HomeController.php
+++ app/Http/Controllers/Cooperation/HomeController.php
@@ -2,22 +2,8 @@
 
 namespace App\Http\Controllers\Cooperation;
 
-use App\Helpers\Cooperation\Tool\HeaterHelper;
-use App\Helpers\Cooperation\Tool\WallInsulationHelper;
-use App\Helpers\Hoomdossier;
-use App\Helpers\StepHelper;
-use App\Helpers\ToolHelper;
 use App\Http\Controllers\Controller;
-use App\Models\Building;
-use App\Models\CompletedStep;
 use App\Models\Cooperation;
-use App\Models\InputSource;
-use App\Models\Step;
-use App\Models\User;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Query\JoinClause;
-use Illuminate\Support\Str;
-use Spatie\TranslationLoader\TranslationLoaders\Db;
 
 class HomeController extends Controller
 {

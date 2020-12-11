635a6a07ee305c62eb43ff270293bd9380d6f68a
Bodhi <bodhilooij1998@gmail.com>
2020-12-11 15:55:07 +0100

    Added translations back for the register form

--- resources/lang/nl/conversation-requests.php
+++ resources/lang/nl/conversation-requests.php
@@ -10,11 +10,13 @@ return [
         'header' => 'Actie ondernemen',
         'request-coach-conversation' => 'Coachgesprek aanvragen',
 
+        'text' => 'De gegevens worden uitsluitend door de coöperatie gebruikt om u in uw bewonersreis te ondersteunen. Uw persoonlijke gegevens worden niet doorgegeven aan derden. Meer informatie over de verwerking van uw data en wat we ermee doen kunt u vinden in ons privacy statement.',
         'form' => [
             'title' => 'Contact over :measure_application_name',
             'are-you-sure' => 'Weet u zeker dat u de Coöperatie geen toegang wilt geven tot uw dossier?',
             'message' => 'Nadere toelichting op uw vraag',
             'submit' => 'Opsturen <span class="glyphicon glyphicon-envelope"></span>',
+            'allow_access' => 'Ik geef toesteming aan :cooperation om de gegevens uit mijn Hoomdossier in te zien en in overleg met mij deze gegevens aan te passen.',
 
             'selected-option' => 'Waar kunnen we u bij helpen?:',
         ],

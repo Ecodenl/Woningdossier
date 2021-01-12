2daf45c625bf2cccef30a16acb9f0e2fafe96181
Bodhi <bodhilooij1998@gmail.com>
2021-01-12 13:13:56 +0100

    wip

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,7 @@ use App\Policies\PrivateMessagePolicy;
 use App\Policies\QuestionnairePolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -43,6 +44,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        \Auth::userResolver(function ($x) {
+            dd($x);
+        });
+//        \Auth::viaRequest('session', function (Request $request) {
+//            $request->user();
+//            return \Auth::guard()->user() instanceof Account ? \Auth::guard()->user()->user() : null;
+//        });
+
         Gate::define('talk-to-resident', BuildingPolicy::class.'@talkToResident');
         Gate::define('access-building', BuildingPolicy::class.'@accessBuilding');
         Gate::define('set-appointment', BuildingPolicy::class.'@setAppointment');
@@ -57,9 +66,6 @@ class AuthServiceProvider extends ServiceProvider
 
     public function register()
     {
-        // custom user resolver via account
-        \Auth::resolveUsersUsing(function ($guard = null) {
-            return \Auth::guard($guard)->user() instanceof Account ? \Auth::guard()->user()->user() : null;
-        });
+
     }
 }

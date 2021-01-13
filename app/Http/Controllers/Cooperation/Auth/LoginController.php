49ed34b1b20b1be17051f0525c0bd86998564881
Bodhi <bodhilooij1998@gmail.com>
2021-01-13 09:23:52 +0100

    Some consistency fixes for the policies

--- app/Http/Controllers/Cooperation/Auth/LoginController.php
+++ app/Http/Controllers/Cooperation/Auth/LoginController.php
@@ -154,7 +154,7 @@ class LoginController extends Controller
         // if the login attempt was unsuccessful we will increment the number of attempts
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
-//        $this->incrementLoginAttempts($request);
+        $this->incrementLoginAttempts($request);
 
         return $this->sendFailedLoginResponse($request);
     }

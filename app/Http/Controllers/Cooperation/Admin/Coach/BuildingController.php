c2e63123a0a46598d795e9e8ea1447e17d77466a
Bodhi <bodhilooij1998@gmail.com>
2021-05-06 12:04:01 +0200

    Added query to get the appointment date and status directly on the building model itself, this will reduce queries and increase speed. It also fixes the status problem in general

--- app/Http/Controllers/Cooperation/Admin/Coach/BuildingController.php
+++ app/Http/Controllers/Cooperation/Admin/Coach/BuildingController.php
@@ -5,26 +5,37 @@ namespace App\Http\Controllers\Cooperation\Admin\Coach;
 use App\Helpers\Hoomdossier;
 use App\Http\Controllers\Controller;
 use App\Models\Building;
-use App\Models\Cooperation;
 use App\Services\BuildingCoachStatusService;
+use Illuminate\Support\Facades\DB;
 
 class BuildingController extends Controller
 {
-    public function index(Cooperation $cooperation)
+    public function index()
     {
-        $connectedBuildingsForUser = BuildingCoachStatusService::getConnectedBuildingsByUser(Hoomdossier::user())->pluck('building_id');
+        $connectedBuildingsForUser = BuildingCoachStatusService::getConnectedBuildingsByUser(
+            Hoomdossier::user()
+        )->pluck('building_id');
 
-        $buildings = Building::whereIn('buildings.id', $connectedBuildingsForUser)
-           ->with([
-               'user',
-               'buildingStatuses' => function ($query) {
-                   $query->with('status');
-               }]
-           )->get();
 
+        $recentBuildingStatuses = DB::table('building_statuses')
+            ->selectRaw('building_id, max(created_at) as max_created_at, max(id) AS max_id')
+            ->groupByRaw('building_id');
+
+        $buildings = Building::select([
+            'buildings.*',
+            'translations.translation as status_translation',
+            'appointment_date',
+        ])->leftJoin('building_statuses as bs', 'bs.building_id', '=', 'buildings.id')
+            ->rightJoinSub($recentBuildingStatuses, 'bs2', 'bs2.max_id', '=', 'bs.id')
+            ->leftJoin('statuses', 'bs.status_id', '=', 'statuses.id')
+            ->leftJoin('translations', 'statuses.name', '=', 'translations.key')
+            ->whereIn('buildings.id', $connectedBuildingsForUser)
+            ->where('translations.language', '=', 'nl')
+            ->orderByDesc('appointment_date')
+            ->with('user')
+            ->get();
 
         return view('cooperation.admin.coach.buildings.index', compact('buildings', 'buildings'));
     }
 }
-
 

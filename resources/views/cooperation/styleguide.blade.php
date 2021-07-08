365bef494a6d2434d2588c197f60a806697df38a
Bodhi <bodhilooij1998@gmail.com>
2021-07-08 16:55:15 +0200

    WIP!

--- resources/views/cooperation/styleguide.blade.php
+++ resources/views/cooperation/styleguide.blade.php
@@ -10,30 +10,44 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-8 flex">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-8 flex flex-wrap">
 
-        <div class="w-3/12 space-y-4">
-
-            <div class="grid grid-cols-3 grid-rows-1 grid-flow-row gap-4">
-                <div class="text-center py-8 bg-green text-white rounded">#2CA982 bg-green</div>
-                <div class="text-center py-8 bg-purple text-white rounded">#622181 bg-purple </div>
-                <div class="text-center py-8 bg-white text-black border-blue-100 border-2 rounded">#FFFFFF bg-white</div>
-            </div>
-
-            <div class="grid grid-cols-3 grid-rows-1 grid-flow-row gap-4">
-                <div class="text-center py-8 bg-blue text-white rounded">#414C57 bg-blue</div>
-                <div class="text-center py-8 bg-blue-500 text-white rounded">#647585 bg-blue-500</div>
-                <div class="text-center py-8 bg-blue-100 text-black border-blue-100 border-2 rounded">#F0F1F2 bg-blue-100</div>
+        <div class="w-full">
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <div class="relative">
+                  <span class="pr-3 bg-white text-lg font-medium text-gray-900">
+                  Projects
+                </span>
+                <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div class="w-full border-t border-gray-300"></div>
+                </div>
             </div>
         </div>
-        <div class="w-1/12"></div>
-        <div class="w-8/12">
-            <div class="grid grid-cols-5 grid-rows-1 grid-flow-row gap-4">
-                <div class="text-center py-8 bg-orange text-white rounded">#FF7F4A <br>bg-organe</div>
-                <div class="text-center py-8 bg-yellow text-white rounded">#FFBD5A <br>bg-yellow</div>
-                <div class="text-center py-8 bg-red text-white border-blue-100 border-2 rounded">#E31440 <br> bg-red</div>
-                <div class="text-center py-8 bg-blue-800 text-white rounded">#3781F0 <br>bg-blue-800</div>
-                <div class="text-center py-8 bg-blue-900 text-white rounded">#1122C8 <br>bg-blue-900</div>
+        <div class="flex">
+
+            <div class="w-3/12 space-y-4">
+
+                <div class="grid grid-cols-3 grid-rows-1 grid-flow-row gap-4">
+                    <div class="text-center py-8 bg-green text-white rounded">#2CA982 bg-green</div>
+                    <div class="text-center py-8 bg-purple text-white rounded">#622181 bg-purple </div>
+                    <div class="text-center py-8 bg-white text-black border-blue-100 border-2 rounded">#FFFFFF bg-white</div>
+                </div>
+
+                <div class="grid grid-cols-3 grid-rows-1 grid-flow-row gap-4">
+                    <div class="text-center py-8 bg-blue text-white rounded">#414C57 bg-blue</div>
+                    <div class="text-center py-8 bg-blue-500 text-white rounded">#647585 bg-blue-500</div>
+                    <div class="text-center py-8 bg-blue-100 text-black border-blue-100 border-2 rounded">#F0F1F2 bg-blue-100</div>
+                </div>
+            </div>
+            <div class="w-1/12"></div>
+            <div class="w-8/12">
+                <div class="grid grid-cols-5 grid-rows-1 grid-flow-row gap-4">
+                    <div class="text-center py-8 bg-orange text-white rounded">#FF7F4A <br>bg-organe</div>
+                    <div class="text-center py-8 bg-yellow text-white rounded">#FFBD5A <br>bg-yellow</div>
+                    <div class="text-center py-8 bg-red text-white border-blue-100 border-2 rounded">#E31440 <br> bg-red</div>
+                    <div class="text-center py-8 bg-blue-800 text-white rounded">#3781F0 <br>bg-blue-800</div>
+                    <div class="text-center py-8 bg-blue-900 text-white rounded">#1122C8 <br>bg-blue-900</div>
+                </div>
             </div>
         </div>
     </div>

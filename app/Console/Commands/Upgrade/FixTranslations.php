9d0da7d50cbe03a06ccba3b3460806be55018f62
Yinci <yannisch@hotmail.com>
2021-07-27 09:04:07 +0200

    Translation fix command for upgrade

---
+++ app/Console/Commands/Upgrade/FixTranslations.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands\Upgrade;
+
+use App\Models\LanguageLine;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class FixTranslations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'upgrade:fix-translations';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove outdated stuff, add new stuff';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * The groups to delete
+     *
+     * @var array|string[]
+     */
+    protected array $groups = ['home'];
+
+    /**
+     * The specific keys to delete
+     *
+     * @var array|string[]
+     */
+    protected array $keys = [];
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Deleting old translations from database...');
+
+        $groupsDeleted = LanguageLine::whereIn('group', $this->groups)->delete();
+        $keysDeleted = LanguageLine::whereIn('keys', $this->keys)->delete();
+        $total = $groupsDeleted + $keysDeleted;
+
+        $this->info("Removed {$total} old translations removed, re-importing...");
+
+        Artisan::call('cache:clear');
+        Artisan::call('translations:import');
+
+        $this->info('All done.');
+    }
+}

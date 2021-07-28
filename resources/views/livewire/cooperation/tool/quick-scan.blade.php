6f413cb23ed4876948d0cbba54905dc4439fbef2
Bodhi <bodhilooij1998@gmail.com>
2021-07-28 08:38:39 +0200

    fix

--- resources/views/livewire/cooperation/tool/quick-scan.blade.php
+++ resources/views/livewire/cooperation/tool/quick-scan.blade.php
@@ -1,16 +1,6 @@
-@extends('cooperation.frontend.layouts.app')
+@extends('cooperation.frontend.layouts.tool')
 
-
-{{-- Remove BG image --}}
-@section('main_style', '')
-
-@section('main')
-<div>
-
-    <a class="btn btn-purple" href="/quick-scan/woninggegevens/2">Test 2</a>
-    <a class="btn btn-purple" href="/quick-scan/woninggegevens/3">Test 3</a>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-</div>
-
+@section('content')
+    <a href="quick-scan/woninggegevens/3" class="btn btn-purple">stap 3</a>
+    <a href="quick-scan/woninggegevens/2" class="btn btn-purple">stap 2</a>
 @endsection
-

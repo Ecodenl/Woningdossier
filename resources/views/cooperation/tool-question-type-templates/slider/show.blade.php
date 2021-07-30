5b069e6ed6c3ac495944e72bfbdf6620e7493613
Bodhi <bodhilooij1998@gmail.com>
2021-07-30 14:55:18 +0200

    Something for my fren

--- resources/views/cooperation/tool-question-type-templates/slider/show.blade.php
+++ resources/views/cooperation/tool-question-type-templates/slider/show.blade.php
@@ -1,6 +1,22 @@
-@include('cooperation.frontend.layouts.parts.slider', [
-    'min' => $toolQuestion->options['min'],
-    'max' => $toolQuestion->options['max'],
-    'step' => $toolQuestion->options['step'],
-    'unit' => $toolQuestion->unit_of_measure,
-])
+@php
+    $min = $toolQuestion->options['min'];
+    $max = $toolQuestion->options['max'];
+    // Set proper values
+    $min = $min < 0 ? 0 : $min;
+    $max = $max <= $min ? $min + 10 : $max;
+
+    $step = $toolQuestion->options['step'];
+    $unit = $toolQuestion->unit_of_measure;
+@endphp
+<div class="flex flex-wrap items-center w-full mt-12" x-data="slider()">
+    <p class="w-1/12 flex justify-end pr-5">{{$min}}{!! $unit !!}</p>
+    <div class="w-10/12 relative flex justify-center items-center">
+        <input type="range" min="{{$min}}" max="{{$max}}" step="{{$step}}" name="{{$inputName ?? ''}}" class="slider"
+               wire:model="filledInAnswers.{{$toolQuestion['id']}}"
+               x-ref="slider" x-on:input="updateVisuals()" x-model="value">
+        <div class="slider-bubble" x-show="initialized" x-ref="slider-bubble">
+            <span x-text="value"></span>{!! $unit !!}
+        </div>
+    </div>
+    <p class="w-1/12 flex justify-start pl-5" x-on:click="document.querySelector('.slider').value = 4;">{{$max}}{!! $unit !!}</p>
+</div>

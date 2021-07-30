033e5cc0265e435b947cce3b4c458fd90e299f8f
Bodhi <bodhilooij1998@gmail.com>
2021-07-30 14:51:53 +0200

    Used flexbox for the radio-icon, worked out better than grid in this case

--- resources/views/cooperation/tool-question-type-templates/radio-icon/show.blade.php
+++ resources/views/cooperation/tool-question-type-templates/radio-icon/show.blade.php
@@ -1,10 +1,11 @@
-<div class="w-full grid grid-rows-2 grid-cols-4 grid-flow-row justify-items-center">
+{{--<div class="w-full grid grid-rows-1 grid-cols-8 grid-flow-row gap-4 ">--}}
+<div class="w-full flex justify-start space-x-8">
 @foreach($toolQuestion->getQuestionValues() as $toolQuestionValue)
-    <div class="radio-wrapper media-wrapper">
+    <div class="radio-wrapper media-wrapper @if($toolQuestions->count() > 1) media-wrapper-small @endif">
         <input type="radio"
                id="{{$toolQuestionValue['short'] ?? $toolQuestionValue['calculate_value']}}"
                wire:model="filledInAnswers.{{$toolQuestion['id']}}"
-               value="{{$toolQuestionValue['id']}}"
+               value="{{$toolQuestionValue['short'] ?? $toolQuestionValue['id']}}"
         >
         <label for="{{$toolQuestionValue['short'] ?? $toolQuestionValue['calculate_value']}}">
                             <span class="media-icon-wrapper">

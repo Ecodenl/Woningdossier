a3f98c6e7d147de4ad70203337d4b439cf8215fd
Yinci <yannisch@hotmail.com>
2021-08-11 16:31:38 +0200

    Is already in the form group

--- resources/views/cooperation/tool-question-type-templates/textarea/show.blade.php
+++ resources/views/cooperation/tool-question-type-templates/textarea/show.blade.php
@@ -1,4 +1,2 @@
-    <div class="input-group">
-        <textarea wire:model="filledInAnswers.{{$toolQuestion['id']}}" id="{{$toolQuestion->short}}" class="form-input"
-                  placeholder="{{$toolQuestion->placeholder}}"></textarea>
-    </div>
+    <textarea wire:model="filledInAnswers.{{$toolQuestion['id']}}" id="{{$toolQuestion->short}}" class="form-input"
+              placeholder="{{$toolQuestion->placeholder}}"></textarea>

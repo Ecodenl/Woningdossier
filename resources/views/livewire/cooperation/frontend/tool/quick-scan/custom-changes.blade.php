7cad7535ab874ea185e9d3f59c4642b1bfc784c1
Bodhi <bodhilooij1998@gmail.com>
2021-08-13 13:21:21 +0200

    wip

--- resources/views/livewire/cooperation/frontend/tool/quick-scan/custom-changes.blade.php
+++ resources/views/livewire/cooperation/frontend/tool/quick-scan/custom-changes.blade.php
@@ -34,7 +34,9 @@
                        'id' => 'new-measure-subject',
                        'withInputSource' => false,
                    ])
-                        <input class="form-input" wire:model="customMeasureApplications.{{$index}}.name" id="new-measure-subject" placeholder="@lang('livewire/cooperation/frontend/tool/quick-scan/custom-changes.name.placeholder')">
+                        <input class="form-input" wire:model="customMeasureApplications.{{$index}}.name" id="new-measure-subject"
+
+                               placeholder="@lang('livewire/cooperation/frontend/tool/quick-scan/custom-changes.na')">
                     @endcomponent
                     <div class="w-full flex items-center">
                         <i class="icon-sm icon-info mr-3"></i>

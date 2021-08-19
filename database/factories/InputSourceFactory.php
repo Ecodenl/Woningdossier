5b1197d10f04784fd874c15d5c372c19c3a0a3bc
Yinci <yannisch@hotmail.com>
2021-08-19 10:55:23 +0200

    New factories

--- database/factories/InputSourceFactory.php
+++ database/factories/InputSourceFactory.php
@@ -5,11 +5,9 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\InputSource::class, function (Faker $faker) {
     $name = $faker->word;
     $short = \Illuminate\Support\Str::slug($name);
-
-    $order = DB::table('input_sources')->max('order') + 1;
     return [
         'name' => $name,
         'short' => $short,
-        'order' => $order,
+        'order' => $faker->randomNumber(2),
     ];
 });

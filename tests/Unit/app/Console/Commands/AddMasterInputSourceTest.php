7c4bc64f509ace40569b49549aac8b3d38b2959d
Yinci <yannisch@hotmail.com>
2021-08-19 09:57:02 +0200

    Add comments

--- tests/Unit/app/Console/Commands/AddMasterInputSourceTest.php
+++ tests/Unit/app/Console/Commands/AddMasterInputSourceTest.php
@@ -15,15 +15,19 @@ class AddMasterInputSourceTest extends TestCase
 
     public function testSearchCollectionForValue()
     {
+        // Instantiate new command so we can call the function
         $command = new AddMasterInputSource();
 
+        // Make a new input source
         $inputSource = factory(InputSource::class)->create();
 
+        // Define our test object
         $test1 = (object) [
             'input_source_id' => $inputSource->id,
             'value' => $this->faker->word,
         ];
 
+        // Add extra data
         $collection = collect([
             $test1,
             (object) [
@@ -36,11 +40,15 @@ class AddMasterInputSourceTest extends TestCase
             ],
         ]);
 
+        // Call search
         $search = $command->searchCollectionForValue($collection, $inputSource);
 
+        // It has to be an object, else we cannot check the object property
         if ($search instanceof \stdClass) {
+            // Ensure the value is the same
             $this->assertEquals($search->value, $test1->value);
 
+            // Repeat same test, except with a search this time
             $test2 = (object) [
                 'input_source_id' => $inputSource->id,
                 'extra_to_search' => $this->faker->uuid,
@@ -48,11 +56,7 @@ class AddMasterInputSourceTest extends TestCase
             ];
 
             $collection = collect([
-                $test2 = (object) [
-                    'input_source_id' => $inputSource->id,
-                    'extra_to_search' => $this->faker->uuid,
-                    'value' => $this->faker->word,
-                ],
+                $test2,
                 (object) [
                     'input_source_id' => $inputSource->id,
                     'extra_to_search' => $this->faker->uuid,
@@ -73,6 +77,7 @@ class AddMasterInputSourceTest extends TestCase
                 $this->fail('Result for test2 is not an object');
             }
         } else {
+            // If it's not an object, we will manually fail
             $this->fail('Result for test1 is not an object');
         }
     }

f1cd8d5739d412eee4e3d7fcef258ce3fbb17dc5
Yinci <yannisch@hotmail.com>
2021-08-24 15:39:04 +0200

    Also for rating slider

--- resources/views/cooperation/sub-step-templates/template-summary/show.blade.php
+++ resources/views/cooperation/sub-step-templates/template-summary/show.blade.php
@@ -58,7 +58,7 @@
                     @endphp
                     @if ($showToolQuestion)
                         <div class="flex flex-row flex-wrap w-full">
-                            <div class="@if($toolQuestionToSummarize->toolQuestionType->short === 'rating-slider') w-full @else w-1/2 @endif">
+                            <div class="@if($toolQuestionToSummarize->toolQuestionType->short === 'rating-slider') w-full pb-2 @else w-1/2 @endif">
                                 <a href="{{ $subStepRoute }}" class="no-underline">
                                     <h4 class="heading-4">
                                         - {{ $toolQuestionToSummarize->name }}
@@ -70,12 +70,22 @@
                                 @foreach($toolQuestionToSummarize->options as $option)
                                     <div class="w-1/2">
                                         <h5 class="heading-5">
-                                            {{ $option['name'] }}
+                                            - {{ $option['name'] }}
                                         </h5>
                                     </div>
                                     <div class="w-1/2 text-right">
                                         <p class="font-semibold">
+                                            @php
+                                                $humanReadableAnswer = __('cooperation/frontend/tool.no-answer-given');
+                                                $answer = $answers->where($toolQuestionToSummarize->short, '!=', null)->first();
 
+                                                if (! empty($answer)) {
+                                                    $answerValue = $answer[$toolQuestionToSummarize->short];
+                                                    $humanReadableAnswer = json_decode($answerValue, true)[$option['short']];
+                                                }
+                                            @endphp
+
+                                            {{ $humanReadableAnswer }}
                                         </p>
                                     </div>
                                 @endforeach

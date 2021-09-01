4cb94ebefb7bcdd87663d00c413cb4fc66672a08
pvankouteren <pvankouteren@gmail.com>
2021-09-01 06:57:22 +0200

    optimize imports, use facade

--- app/Services/BuildingDataService.php
+++ app/Services/BuildingDataService.php
@@ -6,12 +6,13 @@ use App\Models\Building;
 use App\Models\InputSource;
 use App\Models\User;
 use App\Scopes\GetValueScope;
+use Illuminate\Support\Facades\Log;
 
 class BuildingDataService
 {
     public static function clearBuildingFromInputSource(Building $building, InputSource $inputSource)
     {
-        \Log::debug(__CLASS__." Clearing data from input source '".$inputSource->name."'");
+        Log::debug(__CLASS__." Clearing data from input source '".$inputSource->name."'");
 
         // Delete all building elements
         $building->buildingElements()->withoutGlobalScope(GetValueScope::class)->where('input_source_id', $inputSource->id)->delete();

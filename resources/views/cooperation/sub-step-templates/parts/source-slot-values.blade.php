d00cf9cdee761278dc6e175a7ef42d22cb3f0d1f
Yinci <yannisch@hotmail.com>
2021-09-01 13:06:20 +0200

    Formatting, spelling

--- resources/views/cooperation/sub-step-templates/parts/source-slot-values.blade.php
+++ resources/views/cooperation/sub-step-templates/parts/source-slot-values.blade.php
@@ -1,40 +1,41 @@
 @if(!empty($values))
-@foreach($values as $inputSourceShort => $answersForInputSources)
-    @foreach($answersForInputSources as $answerForInputSource)
-    @php
+    @foreach($values as $inputSourceShort => $answersForInputSources)
+        @foreach($answersForInputSources as $answerForInputSource)
+            @php
 
-        $answer = $answerForInputSource['answer'];
-        $humanReadableAnswer = null;
+                $answer = $answerForInputSource['answer'];
+                $humanReadableAnswer = null;
 
-        if (! \App\Helpers\Str::isValidJson($answer)) {
-            $humanReadableAnswer = $answer;
-        } else {
-            $json = json_decode($answer, true);
-            $formatted = [];
+                if (! \App\Helpers\Str::isValidJson($answer)) {
+                    $humanReadableAnswer = $answer;
+                } else {
+                    $json = json_decode($answer, true);
+                    $formatted = [];
 
-            // We try to build the input source names based off the options, but if they aren't available,
-            // we will use the shorts provided
-            // TODO: Check how often a check is done for rating-slider, perhaps change this to a function to
-            // check if a tool question has "sub questions"
-            if ($toolQuestion->toolQuestionType->short === 'rating-slider') {
-                foreach ($toolQuestion->options as $option) {
-                    $formatted[$option['short']] = "{$option['name']}: {$json[$option['short']]}";
+                    // We try to build the input source names based off the options, but if they aren't available,
+                    // we will use the shorts provided
+                    // TODO: Check how often a check is done for rating-slider, perhaps change this to a function to
+                    // check if a tool question has "sub questions"
+                    if ($toolQuestion->toolQuestionType->short === 'rating-slider') {
+                        foreach ($toolQuestion->options as $option) {
+                            $formatted[$option['short']] = "{$option['name']}: {$json[$option['short']]}";
+                        }
+                        // Follow order of input source
+                        $formatted = array_merge($json, $formatted);
+                    } else {
+                        foreach ($json as $short => $value) {
+                            $formatted[$short] = "${short}: {$value}";
+                        }
+                    }
+
+                    $humanReadableAnswer = implode(', ', $formatted);
                 }
-                // Follow order of input source
-                $formatted = array_merge($json, $formatted);
-            } else {
-                foreach ($json as $short => $value) {
-                    $formatted[$short] = "${short}: {$value}";
-                }
-            }
+            @endphp
 
-            $humanReadableAnswer = implode(', ', $formatted);
-        }
-    @endphp
-
-    <li class="change-input-value" data-input-source-short="{{$inputSourceShort}}" data-input-value="{{$answerForInputSource['value']}}">
-        {{\App\Models\InputSource::findByShort($inputSourceShort)->name}}: {{$humanReadableAnswer}}
-    </li>
-@endforeach
-@endforeach
+            <li class="change-input-value" data-input-source-short="{{$inputSourceShort}}"
+                data-input-value="{{$answerForInputSource['value']}}">
+                {{\App\Models\InputSource::findByShort($inputSourceShort)->name}}: {{$humanReadableAnswer}}
+            </li>
+        @endforeach
+    @endforeach
 @endif

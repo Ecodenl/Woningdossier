09dbdd7d2ee93f876f60b1f403ccdf0454e3aadb
Yinci <yannisch@hotmail.com>
2021-09-13 14:38:14 +0200

    Cleanup of templates

--- routes/web.php
+++ routes/web.php
@@ -28,19 +28,8 @@ Route::domain('{cooperation}.' . config('hoomdossier.domain'))->group(function (
             });
         }
 
-
-        // TODO: Figure out how to handle these routes; Move to frontend.php?
         Route::view('styleguide', 'cooperation.frontend.styleguide');
         Route::view('input-guide', 'cooperation.frontend.input-guide');
-        Route::prefix('step')->group(function () {
-            Route::view('7', 'cooperation.frontend.templates.step-7');
-            Route::view('9', 'cooperation.frontend.templates.step-9');
-            Route::view('11', 'cooperation.frontend.templates.step-11');
-            Route::view('12', 'cooperation.frontend.templates.step-12');
-            Route::view('18', 'cooperation.frontend.templates.step-18');
-            Route::view('23', 'cooperation.frontend.templates.step-23');
-        });
-        // TODO END
 
         Route::get('/', function () {
             return view('cooperation.welcome');

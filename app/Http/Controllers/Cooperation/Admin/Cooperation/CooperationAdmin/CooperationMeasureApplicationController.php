fc843b88213687b1c1cd361f4bb8a991f38f579b
Bodhi <bodhilooij1998@gmail.com>
2021-09-15 14:51:27 +0200

    Added soft delete to the measure application table Added de logic for deleting a CooperationMeasureApplication to a job; MoveCooperationMeasureApplicationToCustomMeasureApplications.php

--- app/Http/Controllers/Cooperation/Admin/Cooperation/CooperationAdmin/CooperationMeasureApplicationController.php
+++ app/Http/Controllers/Cooperation/Admin/Cooperation/CooperationAdmin/CooperationMeasureApplicationController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Cooperation\Admin\Cooperation\CooperationAdmin;
 
 use App\Helpers\Cache\Cooperation as CooperationCache;
+use App\Helpers\Queue;
 use App\Helpers\StepHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Cooperation\Admin\Cooperation\CooperationAdmin\CooperationMeasureApplicationFormRequest;
+use App\Jobs\MoveCooperationMeasureApplicationToCustomMeasureApplications;
 use App\Models\Building;
 use App\Models\Cooperation;
 use App\Models\CooperationMeasureApplication;
@@ -59,69 +61,11 @@ class CooperationMeasureApplicationController extends Controller
 
     public function destroy(Cooperation $cooperation, CooperationMeasureApplication $cooperationMeasureApplication)
     {
-        // We can't delete the cooperation measure application straight away. We need to check
-        // if it's used in any UserActionPlanAdvices.
-        $advices = $cooperationMeasureApplication->userActionPlanAdvices()->allInputSources()->get();
-        $processedUserIds = [];
 
-        // We need the master
-        $masterInputSource = InputSource::findByShort(InputSource::MASTER_SHORT);
-
-        foreach ($advices as $advice) {
-            // The master input source makes this a massive pain
-            // First we check if we haven't already processed this set
-            // We need a valid building for this to work
-            $user = $advice->user;
-            if (! in_array($user->id, $processedUserIds) && $user->building instanceof Building) {
-                // Get all advices for this user id
-                $advicesForUserId = $advices->where('user_id', $user->id);
-                $inputSourceIds = $advicesForUserId->where('input_source_id', '!=', $masterInputSource->id)
-                    ->pluck('input_source_id');
-
-                $hash = Str::uuid();
-                $createData = [
-                    'building_id' => $user->building->id,
-                    'name' => $cooperationMeasureApplication->name,
-                    'info' => $cooperationMeasureApplication->info,
-                    'hash' => $hash,
-                ];
-                foreach ($inputSourceIds as $inputSourceId) {
-                    $createData['input_source_id'] = $inputSourceId;
-
-                    // Create a custom measure with the data of the cooperation measure
-                    $customMeasure = CustomMeasureApplication::create($createData);
-                    $adviceForInputSource = $advicesForUserId->where('input_source_id', $inputSourceId)->first();
-                    if ($adviceForInputSource instanceof UserActionPlanAdvice) {
-                        // Update the advice from the cooperation measure to the custom measure
-                        $adviceForInputSource->update([
-                            'user_action_plan_advisable_type' => CustomMeasureApplication::class,
-                            'user_action_plan_advisable_id' => $customMeasure->id,
-                        ]);
-                    }
-                }
-
-                // The master updates the custom measure automatically, but it doesn't update
-                // the user action plan advice. It instead generates a new one. We delete the old advice if it
-                // exists.
-                $adviceForMaster = UserActionPlanAdvice::forUser($user)
-                    ->forInputSource($masterInputSource)
-                    ->whereHasMorph('userActionPlanAdvisable', CooperationMeasureApplication::class,
-                        function ($query) use ($cooperationMeasureApplication) {
-                            $query->where('id', $cooperationMeasureApplication->id);
-                        }
-                    )
-                    ->first();
-
-                if ($adviceForMaster instanceof UserActionPlanAdvice) {
-                    $adviceForMaster->delete();
-                }
-
-                $processedUserIds[] = $user->id;
-            }
-        }
-
-        // Finally, delete the measure
+        // first we soft delete it, this makes it impossible for users to add it.
         $cooperationMeasureApplication->delete();
+
+        MoveCooperationMeasureApplicationToCustomMeasureApplications::dispatch();
 
         return redirect()->route('cooperation.admin.cooperation.cooperation-admin.cooperation-measure-applications.index')
             ->with('success', __('cooperation/admin/cooperation/cooperation-admin/cooperation-measure-applications.destroy.success'));

6d354f1336230965c43744ce33c9441c2b8dac39
pvankouteren <pvankouteren@gmail.com>
2021-09-24 10:32:08 +0200

    syncing update migration and class name

--- database/migrations/2021_09_16_140522_create_considerables_table.php
+++ database/migrations/2021_09_16_140522_create_considerables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserConsiderationsTable extends Migration
+class CreateConsiderablesTable extends Migration
 {
     /**
      * Run the migrations.

bb8bfb7d39c821226e49bc739ac4d368a58c05d2
Bodhi <bodhilooij1998@gmail.com>
2021-09-30 10:35:36 +0200

    wip on making the pdf downloadable

---
+++ app/Http/Livewire/Cooperation/Frontend/Tool/QuickScan/MyPlan/DownloadPdf.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire\Cooperation\Frontend\Tool\QuickScan\MyPlan;
+
+use Livewire\Component;
+
+class DownloadPdf extends Component
+{
+    public function mount()
+    {
+
+    }
+
+    public function render()
+    {
+        return view('livewire.cooperation.frontend.tool.quick-scan.my-plan.download-pdf');
+    }
+
+    /**
+     * This method is responsible for generating the pdf.
+     */
+    public function generatePdf()
+    {
+
+    }
+}
---
+++ resources/views/livewire/cooperation/frontend/tool/quick-scan/my-plan/download-pdf.blade.php
@@ -0,0 +1,7 @@
+<div class="w-full flex">
+    <div class="flex w-full justify-end">
+        <button class="btn btn-purple" type="button" wire:click="generatePdf">
+            @lang('cooperation/frontend/tool.my-plan.create-report')
+        </button>
+    </div>
+</div>

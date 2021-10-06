f7ae06a0aa6433d0041b54b6c97fb722a89062a5
Yinci <yannisch@hotmail.com>
2021-10-06 11:07:28 +0200

    New logic to handle secondary rooftypes when primary roof type is changed

--- app/Observers/BuildingFeatureObserver.php
+++ app/Observers/BuildingFeatureObserver.php
@@ -2,8 +2,10 @@
 
 namespace App\Observers;
 
+use App\Helpers\HoomdossierSession;
 use App\Models\Building;
 use App\Models\BuildingFeature;
+use App\Models\BuildingRoofType;
 use App\Models\BuildingType;
 use App\Models\ExampleBuilding;
 use App\Models\InputSource;
@@ -16,15 +18,56 @@ class BuildingFeatureObserver
 {
     public function saving(BuildingFeature $buildingFeature)
     {
-        $primaryRoofType = $buildingFeature->roofType;
+        $masterInputSource = InputSource::findByShort(InputSource::MASTER_SHORT);
 
-        if ($primaryRoofType instanceof RoofType) {
-            $building = $buildingFeature->building;
+        // Check if we need to manipulate the secondary roof types when the roof type id is changed
+        // We don't need to execute this if the building feature is from the master source. That will be handled by the
+        // getMyValuesTrait
+        if ($buildingFeature->isDirty('roof_type_id') && $buildingFeature->input_source_id != $masterInputSource->id) {
+            if (($building = $buildingFeature->building) instanceof Building) {
+                if (($primaryRoofType = $buildingFeature->roofType) instanceof RoofType) {
+                    $secondaryRoofTypes = $building->roofTypes;
 
-            if ($building->roofTypes()->count() === 0) {
-                // No roof types defined yet. Let's set them.
-                $roofTypeToLink = RoofType::findByShort(RoofType::PRIMARY_TO_SECONDARY_MAP[$primaryRoofType->short]);
-                $building->roofTypes()->create(['roof_type_id' => $roofTypeToLink->id]);
+                    if (($roofTypeToLink = RoofType::findByShort(RoofType::PRIMARY_TO_SECONDARY_MAP[$primaryRoofType->short])) instanceof RoofType) {
+                        $currentInputSource = HoomdossierSession::getInputSource(true);
+                        $shouldCreate = false;
+
+                        if ($secondaryRoofTypes->count() === 0) {
+                            // No roof types defined yet. Let's set them.
+                            $shouldCreate = true;
+                        } else {
+                            // There are roof types present. We need to check if the primary one is included. If it's not,
+                            // we delete the other ones, and then built a new one.
+                            if ($secondaryRoofTypes->where('roof_type_id', $roofTypeToLink->id)->count() === 0) {
+                                // It's not present.
+
+                                // Delete old ones
+                                foreach ($secondaryRoofTypes as $secondaryRoofType) {
+                                    $secondaryRoofType->delete();
+                                }
+
+                                // Enable creating a new one
+                                $shouldCreate = true;
+                            }
+                        }
+
+                        if ($shouldCreate) {
+                            $exampleBuildingRoofType = $building->roofTypes()
+                                ->where('roof_type_id', $roofTypeToLink->id)
+                                ->forInputSource(InputSource::findByShort(InputSource::EXAMPLE_BUILDING))
+                                ->first();
+
+                            if ($exampleBuildingRoofType instanceof BuildingRoofType) {
+                                $exampleBuildingRoofType->replicate()
+                                    ->fill(['input_source_id' => $currentInputSource->id])
+                                    ->save();
+                            } else {
+                                // No example building data, we just built a new one
+                                $building->roofTypes()->create(['roof_type_id' => $roofTypeToLink->id]);
+                            }
+                        }
+                    }
+                }
             }
         }
     }

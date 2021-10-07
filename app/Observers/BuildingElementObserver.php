4ee63801500778eb5fc2e306647284bba4563b17
Yinci <yannisch@hotmail.com>
2021-10-07 08:38:21 +0200

    Apply insulating glazing properly when glass type is changed

--- app/Observers/BuildingElementObserver.php
+++ app/Observers/BuildingElementObserver.php
@@ -2,13 +2,13 @@
 
 namespace App\Observers;
 
-use App\Calculations\InsulatedGlazing;
 use App\Helpers\HoomdossierSession;
 use App\Models\Building;
 use App\Models\BuildingElement;
+use App\Models\Element;
 use App\Models\ElementValue;
 use App\Models\InputSource;
-use App\Models\ToolQuestion;
+use App\Models\InsulatingGlazing;
 use Illuminate\Support\Facades\Log;
 
 class BuildingElementObserver
@@ -17,11 +17,11 @@ class BuildingElementObserver
     {
         $masterInputSource = InputSource::findByShort(InputSource::MASTER_SHORT);
 
-        $livingRoomsWindows = ToolQuestion::findByShort('living-rooms-windows');
-        $sleepingRoomsWindows = ToolQuestion::findByShort('sleeping-rooms-windows');
+        $livingRoomsWindows = Element::findByShort('living-rooms-windows');
+        $sleepingRoomsWindows = Element::findByShort('sleeping-rooms-windows');
 
         // Check if we need to manipulate the insulated glazings. There's 2 elements that are relevant, so we check
-        // if it's either of that element, and then if the value is dirty, we can continue
+        // if it's either of those elements, and then if the value is dirty, we can continue
         // We don't need to execute this if the building element is from the master source. That will be handled by the
         // getMyValuesTrait
         if (($buildingElement->element_id === $livingRoomsWindows->id || $buildingElement->element_id === $sleepingRoomsWindows->id)
@@ -32,12 +32,29 @@ class BuildingElementObserver
 
                 // Let's see which insulated glazing we need to apply
                 if (($elementValue = $buildingElement->elementValue) instanceof ElementValue) {
-                    // TODO: Check other element value, we need to grab the LOWEST comfort
                     $comfort = $elementValue->configurations['comfort'] ?? 0;
 
-                    if (($singlePaneGlass = InsulatedGlazing::where('name->nl', 'Enkelglas')->first()) instanceof InsulatedGlazing) {
+                    // We need to check the lowest glass type, so we need to check the other element value also
+                    $elementToCompare = $buildingElement->element_id === $livingRoomsWindows->id
+                        ? $livingRoomsWindows : $sleepingRoomsWindows;
+
+                    $otherElementValue = $building->buildingElements()
+                        ->where('element_id', $elementToCompare->id)
+                        ->forInputSource($currentInputSource)
+                        ->first();
+
+                    // Get the lowest comfort score
+                    if ($otherElementValue instanceof ElementValue) {
+                        $comfort = min($comfort, ($otherElementValue->configurations['comfort'] ?? 0));
+                    }
+
+                    if (($singlePaneGlass = InsulatingGlazing::where('name->nl', 'Enkelglas')->first())
+                        instanceof InsulatingGlazing
+                    ) {
                         // This assumes there's no other insulated glazings, which there are not currently
-                        if (($doublePaneGlass = InsulatedGlazing::where('id', '!=', $singlePaneGlass->id)->first()) instanceof InsulatedGlazing) {
+                        if (($doublePaneGlass = InsulatingGlazing::where('id', '!=', $singlePaneGlass->id)->first())
+                            instanceof InsulatingGlazing
+                        ) {
                             // If comfort is below 3, then it's single pane glass. If it's higher/equal to 3, then it's
                             // double pane glass.
                             $glassToApply = $comfort < 3 ? $singlePaneGlass : $doublePaneGlass;
@@ -45,12 +62,15 @@ class BuildingElementObserver
                             // Get all the building insulated glazings which don't already have this glass type selected
                             $insulatedGlazings = $building->currentInsulatedGlazing()
                                 ->forInputSource($currentInputSource)
-                                ->where('insulated_glazing_id', '!=', $glassToApply->id)
+                                ->where('insulating_glazing_id', '!=', $glassToApply->id)
                                 ->get();
 
-                            // TODO: Apply the glass to the glazings
-
-
+                            // Update them with the new glass type
+                            foreach ($insulatedGlazings as $insulatedGlazing) {
+                                $insulatedGlazing->update([
+                                    'insulating_glazing_id' => $glassToApply->id,
+                                ]);
+                            }
                         }
                     } else {
                         Log::alert('Insulated glazing name changed for "Enkelglas"!');

cd7d494ad2658fa2297ff1111350a7ffe5c8ca48
Bodhi <bodhilooij1998@gmail.com>
2021-10-14 16:11:31 +0200

    Removing debug

--- app/Console/Commands/LegacyCleanup/DeleteExampleBuildingContentKeys.php
+++ app/Console/Commands/LegacyCleanup/DeleteExampleBuildingContentKeys.php
@@ -45,7 +45,7 @@ class DeleteExampleBuildingContentKeys extends Command
             'solar-panels.-.building_pv_panels.total_installed_power',
         ];
 
-        foreach(ExampleBuildingContent::all()->where('id', ) as $exampleBuildingContent) {
+        foreach(ExampleBuildingContent::all() as $exampleBuildingContent) {
             $content = $exampleBuildingContent->content;
             // forget the keys we need to remove
             foreach($keysToRemove as $keyToRemove) {

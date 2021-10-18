e06764f0c4cfae75a8ef9f13cf17381bc3377569
Bodhi <bodhilooij1998@gmail.com>
2021-10-18 16:26:06 +0200

    This still needs some work

--- app/Observers/ToolQuestionAnswer/BuildingTypeCategory.php
+++ app/Observers/ToolQuestionAnswer/BuildingTypeCategory.php
@@ -3,7 +3,10 @@
 namespace App\Observers\ToolQuestionAnswer;
 
 use App\Jobs\ApplyExampleBuildingForChanges;
+use App\Models\Building;
+use App\Models\BuildingFeature;
 use App\Models\BuildingType;
+use App\Models\InputSource;
 use App\Models\ToolQuestionAnswer;
 use Illuminate\Support\Facades\Log;
 
@@ -17,19 +20,36 @@ class BuildingTypeCategory implements ShouldApply
         // when there is only one building type, we have to save that for the building
         if ($buildingTypes->count() <= 1) {
             $buildingTypeCategory = \App\Models\BuildingTypeCategory::find($toolQuestionAnswer->answer);
+            /** @var Building $building */
             $building = $toolQuestionAnswer->building;
-            $buildingFeature = $building
+
+            $buildingFeatureForMasterInputSource = $building
                 ->buildingFeatures()
-                ->forInputSource($toolQuestionAnswer->inputSource)
+                ->forInputSource(InputSource::findByShort(InputSource::MASTER_SHORT))
                 ->first();
 
             $buildingType = $buildingTypes->first();
             Log::debug("Only 1 building type found for category {$buildingTypeCategory->name}, lets set the $buildingType->name as building type.");
-            $buildingFeature->update(['building_type_id' => $buildingType->id]);
+
+            // check if the current input source has an building feature.
+            $buildingFeatureForCurrentInputSource = $building->buildingFeatures()->forInputSource($toolQuestionAnswer->inputSource)->first();
+
+
+            if ($buildingFeatureForCurrentInputSource instanceof BuildingFeature) {
+                $buildingFeatureForCurrentInputSource->update(['building_type_id' => $buildingType->id]);
+            } else {
+                // if the current input source has no building feature
+                // for example; this can happen if the coach starts filling for the resident
+
+                // todo: replicate the $buildingFeatureForMasterInputSource for the current input source
+                // this way the current inputsource  has all the building feautures from the master, build_year, surfaces etc.
+            }
+
+
 
             // now we will try to apply the example building based on the only available building type, but only if the user has a build_year from pico.
-            if (!is_null($buildingFeature->build_year)) {
-                ApplyExampleBuildingForChanges::dispatchNow($buildingFeature, ['building_type_id' => $buildingType->id], $toolQuestionAnswer->inputSource);
+            if (!is_null($buildingFeatureForMasterInputSource->build_year)) {
+                ApplyExampleBuildingForChanges::dispatchNow($buildingFeatureForMasterInputSource, ['building_type_id' => $buildingType->id], $toolQuestionAnswer->inputSource);
             }
         }
     }

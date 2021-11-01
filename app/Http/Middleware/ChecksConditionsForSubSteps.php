9864a7a0c46c4db81c878c1c2b2a2786291bd755
Yinci <yannisch@hotmail.com>
2021-11-01 12:31:13 +0100

    Make example building steps mandatory

--- app/Http/Middleware/ChecksConditionsForSubSteps.php
+++ app/Http/Middleware/ChecksConditionsForSubSteps.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\ExampleBuildingHelper;
+use App\Helpers\HoomdossierSession;
 use App\Helpers\QuickScanHelper;
+use App\Models\CompletedSubStep;
+use App\Models\InputSource;
 use App\Models\SubStep;
 use Closure;
 
@@ -25,6 +29,27 @@ class ChecksConditionsForSubSteps
         if ($returnToNextStep) {
             // this indeed only covers the next step
             return redirect()->to(QuickScanHelper::getNextStepUrl($request->route('step'), $subStep));
+        }
+
+        // We can show this step according to the sub step conditionals, but have we answered the example building yet?
+        if (! in_array($subStep->getTranslation('slug', 'nl'), ExampleBuildingHelper::RELEVANT_SUB_STEPS)) {
+            // Not an example building sub step, let's check...
+
+            $masterInputSource = InputSource::findByShort(InputSource::MASTER_SHORT);
+            $building = HoomdossierSession::getBuilding(true);
+            foreach (ExampleBuildingHelper::RELEVANT_SUB_STEPS as $subStepSlug) {
+                $subStep = SubStep::where('slug->nl', $subStepSlug)->first();
+                // If valid sub step and showable (could be unanswerable)
+                if ($subStep instanceof SubStep && $request->user()->can('show', $subStep)) {
+                    if (! $building->completedSubSteps()->forInputSource($masterInputSource)->where('sub_step_id', $subStep->id)->first() instanceof CompletedSubStep) {
+                        // Not answered, redirect back
+                        return redirect()->route('cooperation.frontend.tool.quick-scan.index', [
+                            'step' => $subStep->step,
+                            'subStep' => $subStep,
+                        ]);
+                    }
+                }
+            }
         }
 
         return $next($request);

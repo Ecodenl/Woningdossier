59d7254ea910398ee9a675f185c0ac5753ee00d2
Yinci <yannisch@hotmail.com>
2021-11-08 16:15:35 +0100

    Coach could be filling for other user

--- app/Policies/UserActionPlanAdvicePolicy.php
+++ app/Policies/UserActionPlanAdvicePolicy.php
@@ -28,17 +28,24 @@ class UserActionPlanAdvicePolicy
      * Determine whether the user can view the user action plan advice.
      *
      * @param  \App\Models\Account  $user
-     * @param  \App\UserActionPlanAdvice  $userActionPlanAdvice
+     * @param  \App\Models\UserActionPlanAdvice  $userActionPlanAdvice
      * @return mixed
      */
-    public function view(Account $user, UserActionPlanAdvice $userActionPlanAdvice)
+    public function view(Account $user, UserActionPlanAdvice $userActionPlanAdvice): bool
     {
         $authorize = $user->users()->pluck('id')->contains($userActionPlanAdvice->user_id);
 
         // we want to log this, as this is not meant to happen
         // it means the user is trying to fuck up hoomdossier
         // or something is wrong with our own code.
-        if (!$authorize) {
+        if (! $authorize) {
+            // Okay, so the user isn't a direct owner of the advice, but perhaps it's a coach, editing for the user...
+            // We check if the user has permission to edit as a coach
+            $building = $userActionPlanAdvice->user->building;
+            if ($user->can('access-building', $building) && Hoomdossier::user()->hasRoleAndIsCurrentRole('coach')) {
+                return true;
+            }
+
             $userId = Hoomdossier::user()->id;
             (new DiscordNotifier())->notify("Account id {$user->id} with **current** user id {$userId} tried to access (denied) `{$userActionPlanAdvice->toJson()}`");
         }
@@ -60,7 +67,7 @@ class UserActionPlanAdvicePolicy
      * Determine whether the user can update the user action plan advice.
      *
      * @param  \App\Models\Account  $user
-     * @param  \App\UserActionPlanAdvice  $userActionPlanAdvice
+     * @param  \App\Models\UserActionPlanAdvice  $userActionPlanAdvice
      * @return mixed
      */
     public function update(Account $user, UserActionPlanAdvice $userActionPlanAdvice)
@@ -72,7 +79,7 @@ class UserActionPlanAdvicePolicy
      * Determine whether the user can delete the user action plan advice.
      *
      * @param  \App\Models\Account  $user
-     * @param  \App\UserActionPlanAdvice  $userActionPlanAdvice
+     * @param  \App\Models\UserActionPlanAdvice  $userActionPlanAdvice
      * @return mixed
      */
     public function delete(Account $user, UserActionPlanAdvice $userActionPlanAdvice)
@@ -84,7 +91,7 @@ class UserActionPlanAdvicePolicy
      * Determine whether the user can restore the user action plan advice.
      *
      * @param  \App\Models\Account  $user
-     * @param  \App\UserActionPlanAdvice  $userActionPlanAdvice
+     * @param  \App\Models\UserActionPlanAdvice  $userActionPlanAdvice
      * @return mixed
      */
     public function restore(Account $user, UserActionPlanAdvice $userActionPlanAdvice)
@@ -96,7 +103,7 @@ class UserActionPlanAdvicePolicy
      * Determine whether the user can permanently delete the user action plan advice.
      *
      * @param  \App\Models\Account  $user
-     * @param  \App\UserActionPlanAdvice  $userActionPlanAdvice
+     * @param  \App\Models\UserActionPlanAdvice  $userActionPlanAdvice
      * @return mixed
      */
     public function forceDelete(Account $user, UserActionPlanAdvice $userActionPlanAdvice)

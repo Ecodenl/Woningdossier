487156116bc1aed93b5b996f32944c9d5dbd8343
Bodhi <bodhilooij1998@gmail.com>
2021-11-11 11:14:55 +0100

    Cleaner code, also takes into account a user has a specific one selected

--- app/Helpers/QuestionValues/SpecificExampleBuilding.php
+++ app/Helpers/QuestionValues/SpecificExampleBuilding.php
@@ -11,13 +11,33 @@ class SpecificExampleBuilding implements ShouldReturnQuestionValues
 {
     public static function getQuestionValues(Collection $questionValues, Building $building, InputSource $inputSource): Collection
     {
+        // get the current example building for the user
+        $buildingFeature = $building->buildingFeatures()->forInputSource($inputSource)->first();
+
         $conditionalQuestion = ToolQuestion::findByShort('building-type');
         $cooperationId = $building->user->cooperation_id;
 
         $buildingTypeId = $building->getAnswer($inputSource, $conditionalQuestion);
 
+
         $specificExampleBuildings = $questionValues->where('building_type_id', $buildingTypeId)->where('cooperation_id', $cooperationId);
-        $genericExampleBuildings = $questionValues->where('building_type_id', $buildingTypeId)->whereNull('cooperation_id');
+        $genericExampleBuildings = $questionValues
+            ->where('building_type_id', $buildingTypeId)
+            ->whereNull('cooperation_id')
+            ->where('value', '!=', $buildingFeature->example_building_id);
+
+        // now we will change the current selected GENERIC example building name to "Geen van deze"
+        $currentExampleBuilding = $questionValues
+            ->where('building_type_id', $buildingTypeId)
+            ->whereNull('cooperation_id')
+            ->where('value', $buildingFeature->example_building_id)
+            ->first();
+
+        // its an array when its found.
+        if (is_array($currentExampleBuilding)) {
+            $currentExampleBuilding['name'] = "Geen van de overige opties.";
+            $genericExampleBuildings->push($currentExampleBuilding);
+        }
 
         return $genericExampleBuildings->merge($specificExampleBuildings);
     }

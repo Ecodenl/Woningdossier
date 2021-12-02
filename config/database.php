8908e94d74bb4035cdb540f3be77726c68a1bdbc
Bodhi <bodhilooij1998@gmail.com>
2021-12-02 16:44:08 +0100

    Sorted out the tables in sensible array's

--- config/database.php
+++ config/database.php
@@ -61,6 +61,26 @@ return [
             ]) : [],
         ],
 
+        'migration' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_MIGRATION_DATABASE', 'forge'),
+            'username' => env('DB_MIGRATION_USERNAME', 'forge'),
+            'password' => env('DB_MIGRATION_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'testing' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),

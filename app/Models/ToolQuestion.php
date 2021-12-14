6cd130da7dd7c8df626056b5201110b2264737eb
Yinci <yannisch@hotmail.com>
2021-12-14 15:50:44 +0100

    Valuable MUST be a model

--- app/Models/ToolQuestion.php
+++ app/Models/ToolQuestion.php
@@ -100,16 +100,23 @@ class ToolQuestion extends Model
                 ->map(function (ToolQuestionValuable $toolQuestionValuable) {
                     // so now get the actual morphed model.
                     $valuable = $toolQuestionValuable->tool_question_valuable;
+                    if ($valuable instanceof Model) {
 
-                    // these will also be available in the frontend, to the user.
-                    // be careful choosing what you allow.
-                    $questionValue = Arr::only($valuable->toArray(), ['calculate_value', 'short', 'building_type_id', 'cooperation_id']);
-                    $questionValue['extra'] = $toolQuestionValuable->extra;
-                    // the humane readable name is either set in the name or value column.
-                    $questionValue['name'] = $valuable->name ?? $valuable->value;
-                    $questionValue['value'] = $valuable->id;
+                        // these will also be available in the frontend, to the user.
+                        // be careful choosing what you allow.
+                        $questionValue = Arr::only($valuable->toArray(), ['calculate_value', 'short', 'building_type_id', 'cooperation_id']);
+                        $questionValue['extra'] = $toolQuestionValuable->extra;
+                        // the humane readable name is either set in the name or value column.
+                        $questionValue['name'] = $valuable->name ?? $valuable->value;
+                        $questionValue['value'] = $valuable->id;
 
-                    return $questionValue;
+                        return $questionValue;
+                    }
+
+                    return null;
+                })
+                ->filter(function ($value) {
+                    return ! is_null($value);
                 });
         }
         return $this->toolQuestionCustomValues()

7532340cd6aa05c0995acb408caf47d0071c871c
Bodhi <bodhilooij1998@gmail.com>
2021-12-17 10:44:44 +0100

    Removes the "geen van de bovenstaande opties" from the example building selector

--- app/Helpers/QuestionValues/SpecificExampleBuilding.php
+++ app/Helpers/QuestionValues/SpecificExampleBuilding.php
@@ -12,12 +12,10 @@ class SpecificExampleBuilding implements ShouldReturnQuestionValues
 {
     public static function getQuestionValues(Collection $questionValues, Building $building, InputSource $inputSource): Collection
     {
-        // get the current example building for the user
-        $buildingFeature = $building->buildingFeatures()->forInputSource($inputSource)->first();
-
         $conditionalQuestion = ToolQuestion::findByShort('building-type');
         $cooperationId = $building->user->cooperation_id;
         $buildingTypeId = $building->getAnswer($inputSource, $conditionalQuestion);
+
 
         // Get all available example buildings
         $exampleBuildings = ExampleBuilding::where('building_type_id', $buildingTypeId)
@@ -27,19 +25,15 @@ class SpecificExampleBuilding implements ShouldReturnQuestionValues
             })
             ->get();
 
-        // Get the current selected
-        $currentExampleBuilding = $exampleBuildings->find($buildingFeature->example_building_id);
-        $currentExampleBuildingId = optional($currentExampleBuilding)->id;
-
         // Map it to question values
-        return $exampleBuildings->map(function ($exampleBuilding) use ($currentExampleBuildingId) {
+        return $exampleBuildings->map(function ($exampleBuilding) {
             return [
                 'building_type_id' => $exampleBuilding->building_type_id,
                 'cooperation_id' => $exampleBuilding->cooperation_id,
                 'extra' => [
                     'icon' => 'icon-not-relevant',
                 ],
-                'name' => $currentExampleBuildingId === $exampleBuilding->id ? __('cooperation/frontend/tool/quick-scan/question-values.specific-example-building.no-option') : $exampleBuilding->name,
+                'name' => $exampleBuilding->name,
                 'value' => $exampleBuilding->id,
             ];
         });

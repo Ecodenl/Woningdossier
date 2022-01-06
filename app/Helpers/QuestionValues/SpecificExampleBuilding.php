a213e92cf18054faf0f67216074b2ca45fa5cb8f
Yinci <yannisch@hotmail.com>
2022-01-06 08:42:44 +0100

    Fix exception due to null building type id

--- app/Helpers/QuestionValues/SpecificExampleBuilding.php
+++ app/Helpers/QuestionValues/SpecificExampleBuilding.php
@@ -16,30 +16,42 @@ class SpecificExampleBuilding implements ShouldReturnQuestionValues
         $cooperationId = $building->user->cooperation_id;
         $buildingTypeId = $building->getAnswer($inputSource, $conditionalQuestion);
 
-        // There should only ever be one building. If there's more, well, then they fucked it up themselves
-        $genericBuilding = ExampleBuilding::where('building_type_id', $buildingTypeId)
-            ->whereNull('cooperation_id')
-            ->first();
+        // Building type ID can be null, for example if we use $building->getAnswerForAllInputSources, it can
+        // end up with an input source that might not have answered this question yet. Since we only want
+        // example buildings for the given type anyway, we don't have to do anything if the ID is null
+        if (! is_null($buildingTypeId)) {
+            // There should only ever be one building. If there's more, well, then they fucked it up themselves
+            $genericBuilding = ExampleBuilding::where('building_type_id', $buildingTypeId)
+                ->whereNull('cooperation_id')
+                ->first();
 
-        // Get all available example buildings
-        $exampleBuildings = ExampleBuilding::where('building_type_id', $buildingTypeId)
-            ->where('cooperation_id', $cooperationId)
-            ->get();
+            // Get all available example buildings
+            $exampleBuildings = ExampleBuilding::where('building_type_id', $buildingTypeId)
+                ->where('cooperation_id', $cooperationId)
+                ->get();
 
-        // Always add generic building last
-        $exampleBuildings->add($genericBuilding);
+            // If it's not an example building, it will add null to the collection which isn't
+            // great when trying to use it as object. Even though this shouldn't happen, we do this
+            // as a precaution.
+            if ($genericBuilding instanceof ExampleBuilding) {
+                // Always add generic building last
+                $exampleBuildings->add($genericBuilding);
+            }
 
-        // Map it to question values
-        return $exampleBuildings->map(function ($exampleBuilding) use ($genericBuilding) {
-            return [
-                'building_type_id' => $exampleBuilding->building_type_id,
-                'cooperation_id' => $exampleBuilding->cooperation_id,
-                'extra' => [
-                    'icon' => 'icon-not-relevant',
-                ],
-                'name' => $genericBuilding->id === $exampleBuilding->id ? __('cooperation/frontend/tool/quick-scan/question-values.specific-example-building.no-option') : $exampleBuilding->name,
-                'value' => $exampleBuilding->id,
-            ];
-        });
+            // Map it to question values
+            return $exampleBuildings->map(function ($exampleBuilding) use ($genericBuilding) {
+                return [
+                    'building_type_id' => $exampleBuilding->building_type_id,
+                    'cooperation_id' => $exampleBuilding->cooperation_id,
+                    'extra' => [
+                        'icon' => 'icon-not-relevant',
+                    ],
+                    'name' => optional($genericBuilding)->id === $exampleBuilding->id ? __('cooperation/frontend/tool/quick-scan/question-values.specific-example-building.no-option') : $exampleBuilding->name,
+                    'value' => $exampleBuilding->id,
+                ];
+            });
+        }
+
+        return collect([]);
     }
 }

81f70d2f77012d08fd458bc89ce6e226f4bbf1ae
Yinci <yannisch@hotmail.com>
2022-08-26 11:03:34 +0200

    Add data_type to tool questions

---
+++ app/Helpers/DataTypes/Caster.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers\DataTypes;
+
+class Caster
+{
+    public const STRING = 'string';
+    public const INT = 'int';
+    public const FLOAT = 'float';
+    public const BOOL = 'bool';
+    public const ARRAY = 'array';
+    public const JSON = 'json';
+    public const IDENTIFIER = 'identifier';
+}

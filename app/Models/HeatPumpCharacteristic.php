39fab81a25ed8c94d2bdb61d365a7edc9be573c0
pvankouteren <pvankouteren@gmail.com>
2022-08-27 14:09:30 +0200

    Some scopes for key figure data fetching

--- app/Models/HeatPumpCharacteristic.php
+++ app/Models/HeatPumpCharacteristic.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class HeatPumpCharacteristic extends Model
 {
     const TYPE_HYBRID = 'hybrid';
     const TYPE_FULL = 'full';
+
+    public function scopeForHeatPumpConfigurable(
+        Builder $query,
+        Model $configurable
+    ) {
+        // todo
+    }
 }
--- app/Models/KeyFigureInsulationFactor.php
+++ app/Models/KeyFigureInsulationFactor.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class KeyFigureInsulationFactor extends Model
 {
-    //
+
+    public function scopeForInsulationFactor(Builder $query, $factor)
+    {
+        $factor = number_format($factor, 2, '', '.');
+
+        return $query->where('insulation_factor', '=', $factor);
+    }
 }

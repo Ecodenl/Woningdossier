86552af7909537f746fc741ebbc86ff5c16924a0
Yinci <yannisch@hotmail.com>
2022-08-29 13:32:04 +0200

    When the window doesn't focus and you type somewhere else

--- database/migrations/2022_08_29_132953_remove_conditions_table_from_tool_questions_table.php
+++ database/migrations/2022_08_29_132953_remove_conditions_table_from_tool_questions_table.php
@@ -26,7 +26,7 @@ class RemoveConditionsTableFromToolQuestionsTable extends Migration
      * @return void
      */
     public function down()
-    {R
+    {
         if (! Schema::hasColumn('tool_questions', 'conditions')) {
             Schema::table('tool_questions', function (Blueprint $table) {
                 $table->json('conditions')->nullable()->default(null)->after('for_specific_input_source_id');

e2226a83c901605bf6f12eb047bf6f71d8c525f6
Yinci <yannisch@hotmail.com>
2022-08-30 16:34:19 +0200

    Apparently a single foreach was very confusing

--- resources/views/livewire/cooperation/frontend/tool/expert-scan/form.blade.php
+++ resources/views/livewire/cooperation/frontend/tool/expert-scan/form.blade.php
@@ -1,8 +1,5 @@
 <div>
-    @foreach($failedValidationForSubSteps as $failedValidationForSubStep)
-        <h1>Failed {{$failedValidationForSubStep}}</h1>
-    @endforeach
-    <div x-data="{ active: '{{$step->subSteps->first()->slug}}'}">
+    <div x-data="{ active: '{{$step->subSteps->first()->slug}}'}" wire:ignore.self>
         <div class="hidden sm:block">
             <nav class="flex space-x-4" aria-label="Tabs">
                 <!-- Current: "bg-indigo-100 text-indigo-700", Default: "text-green-500 hover:text-green-700" -->
@@ -16,8 +13,8 @@
 
 
         @foreach($step->subSteps as $subStep)
-             <div x-show="active == '{{$subStep->slug}}'">
-                @livewire('cooperation.frontend.tool.expert-scan.sub-steppable', ['step' => $step, 'subStep' => $subStep])
+             <div x-show="active == '{{$subStep->slug}}'" wire:ignore.self>
+                @livewire('cooperation.frontend.tool.expert-scan.sub-steppable', ['step' => $step, 'subStep' => $subStep], key($subStep->id))
              </div>
          @endforeach
     </div>

2dfa86da89e88198c76c00cdc2b430570468911d
Yinci <yannisch@hotmail.com>
2022-09-01 15:06:19 +0200

    Fix styling to be consistent with other dropdowns

--- resources/views/livewire/cooperation/frontend/layouts/parts/alerts.blade.php
+++ resources/views/livewire/cooperation/frontend/layouts/parts/alerts.blade.php
@@ -1,22 +1,29 @@
-<div x-data="{alertOpen: false}" class="relative" x-on:click.outside="alertOpen = false;">
-    <input type="hidden" wire:model="alertOpen" x-on:element:updated.window="if ($event.detail.field === 'alertOpen') { alertOpen = $event.detail.value; }">
-    <button x-on:click="alertOpen = !alertOpen" class="flex flex-wrap justify-center items-center">
+<div x-data="dropdown()" x-ref="dropdown-wrapper"
+     class="dropdown-wrapper w-inherit flex items-center">
+    <input type="hidden" wire:model="alertOpen" x-on:element:updated.window="if ($event.detail.field === 'alertOpen') { open = $event.detail.value; }">
+    <a href="#" x-on:click="toggle()" x-ref="dropdown-toggle" class="dropdown-toggle select-none flex mr-1">
         <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-500 h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
         </svg>
         <div class="absolute flex-shrink-0 flex -right-1 -top-1">
-            <span class="h-3 w-3 rounded-full bg-green" aria-hidden="true"></span>
+            <span class="h-3 w-3 rounded-full @if($alerts->isNotEmpty()) border border-white bg-red @endif" aria-hidden="true"></span>
         </div>
-    </button>
-
-    <div x-show="alertOpen" wire:ignore.self
-         class="border border-blue-500 rounded absolute right-1 mt-2 bg-white rounded-md shadow-lg overflow-hidden z-20 w-96">
-        <div class="divide-y divide-blue-500">
-            @foreach($alerts as $alert)
-                <p class="{{ $typeMap[$alert['type']] }} text-sm p-4">
-                    {{$alert->text}}
+    </a>
+    <ul x-cloak x-show="open" x-ref="dropdown" class="dropdown w-96" x-on:click.outside="close()" wire:ignore.self>
+        @if($alerts->isEmpty())
+            <li>
+                <p class="text-green text-sm">
+                    @lang('cooperation/frontend/layouts.navbar.alerts.empty')
                 </p>
+            </li>
+        @else
+            @foreach($alerts as $alert)
+                <li>
+                    <p class="{{ $typeMap[$alert['type']] }} text-sm">
+                        {{$alert->text}}
+                    </p>
+                </li>
             @endforeach
-        </div>
-    </div>
+        @endif
+    </ul>
 </div>

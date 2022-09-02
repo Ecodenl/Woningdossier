1a069ff939a87b9af99fe5db1ca6e5752345e68f
Yinci <yannisch@hotmail.com>
2022-09-02 12:25:39 +0200

    Fix alpine select doing weird janky shit

--- resources/views/cooperation/frontend/tool/expert-scan/parts/tool-label.blade.php
+++ resources/views/cooperation/frontend/tool/expert-scan/parts/tool-label.blade.php
@@ -1,1 +1,3 @@
-<h4 class="text-blue text-md font-bold pb-4 border-b border-blue border-opacity-50 {{$subSteppable->size}}">{{$subSteppable->subSteppable->name}}</h4>
+<h4 class="text-blue text-md font-bold pb-4 border-b border-blue border-opacity-50 {{$subSteppable->size}}">
+    {{$subSteppable->subSteppable->name}}
+</h4>
--- resources/views/cooperation/tool-question-type-templates/multi-dropdown/show.blade.php
+++ resources/views/cooperation/tool-question-type-templates/multi-dropdown/show.blade.php
@@ -7,7 +7,8 @@
     );
 @endphp
 @component('cooperation.frontend.layouts.components.alpine-select')
-    <select multiple id="{{$toolQuestion->short}}" class="form-input" wire:model="filledInAnswers.{{$toolQuestion->id}}">
+    <select multiple id="{{$toolQuestion->short}}" class="form-input hidden"
+            wire:model="filledInAnswers.{{$toolQuestion->id}}">
         @foreach($questionValues as $toolQuestionValue)
             <option value="{{ $toolQuestionValue['value'] }}"
                     @if(! empty($toolQuestionValue['extra']['icon'])) data-icon="{{ $toolQuestionValue['extra']['icon'] }}" @endif>

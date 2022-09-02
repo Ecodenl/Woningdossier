34b5d04133e798f72dd37da1cb5e42b3d9a6d928
Yinci <yannisch@hotmail.com>
2022-09-02 10:22:58 +0200

    Clean up

--- database/factories/ToolQuestionFactory.php
+++ database/factories/ToolQuestionFactory.php
@@ -7,12 +7,9 @@ use App\Models\ToolQuestionType;
 use Faker\Generator as Faker;
 
 $factory->define(ToolQuestion::class, function (Faker $faker) {
-    $toolQuestionType = ToolQuestionType::inRandomOrder()->first();
-
     return [
         'name' => json_encode(['nl' => $faker->text(60)]),
         'help_text' => json_encode(['nl' => $faker->text(240)]),
-        'tool_question_type_id' => $toolQuestionType->id,
         'save_in' => null,
         'unit_of_measure' => $faker->randomElement(['m2', 'graden']),
         'coach' => $faker->boolean,

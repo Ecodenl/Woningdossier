b6d1add041803a895859c8867424aa0a0869df65
Bodhi <bodhilooij1998@gmail.com>
2022-10-12 15:29:52 +0200

    Lazy on the text input

--- resources/views/cooperation/tool-question-type-templates/text/show.blade.php
+++ resources/views/cooperation/tool-question-type-templates/text/show.blade.php
@@ -1,5 +1,5 @@
 <input class="form-input @if(!empty($toolQuestion->unit_of_measure)) with-append @endif"
-       id="{{$toolQuestion->short}}" wire:model="filledInAnswers.{{$toolQuestion['short']}}"
+       id="{{$toolQuestion->short}}" wire:model.lazy="filledInAnswers.{{$toolQuestion['short']}}"
        placeholder="{{$toolQuestion->placeholder}}" type="text" @if(($disabled ?? false)) disabled @endif>
 @if(!empty($toolQuestion->unit_of_measure))
     <div class="input-group-append">

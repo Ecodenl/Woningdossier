f785a0089ff58849d662b2b1dd78c95d889ba75a
Yinci <yannisch@hotmail.com>
2022-10-14 13:02:47 +0200

    Fix some input source inconsistency

--- app/Http/Livewire/Cooperation/Admin/ExampleBuildings/CsvExport.php
+++ app/Http/Livewire/Cooperation/Admin/ExampleBuildings/CsvExport.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Livewire\Cooperation\Admin\ExampleBuildings;
 
+use App\Helpers\HoomdossierSession;
 use App\Jobs\GenerateExampleBuildingCsv;
 use App\Models\Cooperation;
 use App\Models\FileStorage;
 use App\Models\FileType;
-use App\Models\User;
 use App\Services\FileTypeService;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
@@ -14,16 +14,18 @@ use Livewire\Component;
 class CsvExport extends Component
 {
     use AuthorizesRequests;
+
     public $cooperation;
     public $fileType;
     public $fileStorage;
-
+    public $inputSource;
 
     public function mount(Cooperation $cooperation)
     {
         $this->cooperation = $cooperation;
         $this->fileType = FileType::findByShort('example-building-overview');
         $this->fileStorage = $this->fileType->files()->mostRecent()->first();
+        $this->inputSource = HoomdossierSession::getInputSource(true);
     }
 
     public function render()
@@ -33,16 +35,17 @@ class CsvExport extends Component
 
     public function generate()
     {
+        $this->authorize('store', [$this->fileStorage, $this->fileType]);
+
         // and we create the new file
-        $this->fileStorage = new FileStorage([
+        $this->fileStorage = FileStorage::create([
             'cooperation_id' => $this->cooperation->id,
             'file_type_id' => $this->fileType->id,
             'filename' => (new FileTypeService($this->fileType))->niceFileName(),
+            'input_source_id' => $this->inputSource->id,
             'is_being_processed' => true,
         ]);
 
-        $this->authorize('store', [$this->fileStorage, $this->fileType]);
-        $this->fileStorage->save();
         GenerateExampleBuildingCsv::dispatch($this->cooperation, $this->fileType, $this->fileStorage);
     }
 }

34b68279302c225eaac2c82d9ac9d0528e290a19
Yinci <yannisch@hotmail.com>
2022-10-20 12:06:06 +0200

    Add complex and to test case

--- tests/Unit/app/Helpers/Conditions/ConditionEvaluatorTest.php
+++ tests/Unit/app/Helpers/Conditions/ConditionEvaluatorTest.php
@@ -2,100 +2,21 @@
 
 namespace Tests\Unit\app\Helpers\Conditions;
 
+use App\Helpers\Conditions\Clause;
 use App\Helpers\Conditions\ConditionEvaluator;
 use PHPUnit\Framework\TestCase;
 
 class ConditionEvaluatorTest extends TestCase
 {
-
-    protected function simpleAnd($correct = true)
-    {
-        // heat-source contains heat-pump
-        // AND
-        // cook-type = gas
-        return [
-            [
-                [
-                    'column' => 'heat-source',
-                    'value'  => ($correct ? 'heat-pump' : 'allesbrander'),
-                ],
-                [
-                    'column'   => 'cook-type',
-                    'operator' => '=',
-                    'value'    => 'gas',
-                ],
-            ],
-        ];
-    }
-
-    protected static function simpleOr($correct = true)
-    {
-        // heat-source contains heat-pump
-        // OR
-        // cook-type = gas
-
-        return [
-            [
-                [
-                    'column' => 'heat-source',
-                    'value'  => ($correct ? 'heat-pump' : 'allesbrander'),
-                ],
-            ],
-            [
-                [
-                    'column'   => 'cook-type',
-                    'operator' => '=',
-                    'value'    => ($correct ? 'gas' : 'electrisch'),
-                ],
-            ],
-        ];
-    }
-
-
-    protected static function combineAndOr()
-    {
-        return [
-            [
-                [
-                    'column' => 'heat-source',
-                    'value'  => 'heat-pump',
-                ],
-            ],
-            [
-                [
-                    'column' => 'heat-source',
-                    'value'  => 'allesbrander',
-                ],
-                [
-                    'column'   => 'cook-type',
-                    'operator' => '=',
-                    'value'    => 'gas',
-                ],
-            ],
-        ];
-    }
-
-    protected function getTestCollection()
-    {
-        return collect([
-            "heat-source" => collect([
-                "hr-boiler",
-                "heat-pump",
-
-            ]),
-            "cook-type"   => 'gas',
-        ]);
-    }
-
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function testSimpleCollectionEvaluation()
+    public function test_simple_collection_evaluation()
     {
         $answers   = $this->getTestCollection();
-        $evaluator = new ConditionEvaluator();
+        $evaluator = ConditionEvaluator::init();
         $evaluated = $evaluator->evaluateCollection(
             $this->simpleAnd(),
             $answers
@@ -104,6 +25,30 @@ class ConditionEvaluatorTest extends TestCase
 
         $evaluated = $evaluator->evaluateCollection(
             $this->simpleAnd(false),
+            $answers
+        );
+        $this->assertFalse($evaluated);
+
+        $evaluated = $evaluator->evaluateCollection(
+            $this->complexAnd(true, false),
+            $answers
+        );
+        $this->assertTrue($evaluated);
+
+        $evaluated = $evaluator->evaluateCollection(
+            $this->complexAnd(true, true),
+            $answers
+        );
+        $this->assertTrue($evaluated);
+
+        $evaluated = $evaluator->evaluateCollection(
+            $this->complexAnd(false, true),
+            $answers
+        );
+        $this->assertTrue($evaluated);
+
+        $evaluated = $evaluator->evaluateCollection(
+            $this->complexAnd(false, false),
             $answers
         );
         $this->assertFalse($evaluated);
@@ -121,7 +66,7 @@ class ConditionEvaluatorTest extends TestCase
         $this->assertFalse($evaluated);
     }
 
-    public function testComplexerCollectionEvaluation()
+    public function test_complexer_collection_evaluation()
     {
         $answers = [
             [
@@ -129,7 +74,6 @@ class ConditionEvaluatorTest extends TestCase
                 collect([
                     "heat-source" => collect([
                         "heat-pump",
-
                     ]),
                     "cook-type"   => 'electrisch',
                 ]),
@@ -140,7 +84,6 @@ class ConditionEvaluatorTest extends TestCase
                 collect([
                     "heat-source" => collect([
                         "allesbrander",
-
                     ]),
                     "cook-type"   => 'gas',
                 ]),
@@ -152,7 +95,6 @@ class ConditionEvaluatorTest extends TestCase
                     "heat-source" => collect([
                         "heat-pump",
                         "allesbrander",
-
                     ]),
                     "cook-type"   => 'gas',
                 ]),
@@ -163,7 +105,6 @@ class ConditionEvaluatorTest extends TestCase
                 collect([
                     "heat-source" => collect([
                         "hr-boiler",
-
                     ]),
                     "cook-type"   => 'electrisch',
                 ]),
@@ -171,13 +112,124 @@ class ConditionEvaluatorTest extends TestCase
             ],
         ];
 
+        $evaluator = ConditionEvaluator::init();
+
         foreach($answers as $info) {
-            $evaluator = new ConditionEvaluator();
             $evaluated = $evaluator->evaluateCollection(
                 $this->combineAndOr(),
                 $info[0],
             );
             $this->assertEquals($info[1], $evaluated);
         }
+    }
+
+    protected function simpleAnd($correct = true)
+    {
+        // heat-source contains heat-pump
+        // AND
+        // cook-type = gas
+        return [
+            [
+                [
+                    'column' => 'heat-source',
+                    'operator' => Clause::CONTAINS,
+                    'value'  => ($correct ? 'heat-pump' : 'allesbrander'),
+                ],
+                [
+                    'column'   => 'cook-type',
+                    'operator' => Clause::EQ,
+                    'value'    => 'gas',
+                ],
+            ],
+        ];
+    }
+
+    protected function complexAnd($firstCorrect = true, $secondCorrect = false)
+    {
+        // heat-source contains heat-pump OR hr-boiler
+        // AND
+        // cook-type = gas
+        return [
+            [
+                [
+                    [
+                        'column' => 'heat-source',
+                        'operator' => Clause::CONTAINS,
+                        'value'  => ($firstCorrect ? 'heat-pump' : 'allesbrander'),
+                    ],
+                    [
+                        'column' => 'heat-source',
+                        'operator' => Clause::CONTAINS,
+                        'value'  => ($secondCorrect ? 'hr-boiler' : 'gaskanon'),
+                    ],
+                ],
+                [
+                    'column'   => 'cook-type',
+                    'operator' => Clause::EQ,
+                    'value'    => 'gas',
+                ],
+            ],
+        ];
+    }
+
+
+    protected function simpleOr($correct = true)
+    {
+        // heat-source contains heat-pump
+        // OR
+        // cook-type = gas
+
+        return [
+            [
+                [
+                    'column' => 'heat-source',
+                    'operator' => Clause::CONTAINS,
+                    'value'  => ($correct ? 'heat-pump' : 'allesbrander'),
+                ],
+            ],
+            [
+                [
+                    'column'   => 'cook-type',
+                    'operator' => Clause::EQ,
+                    'value'    => ($correct ? 'gas' : 'electrisch'),
+                ],
+            ],
+        ];
+    }
+
+    protected function combineAndOr()
+    {
+        return [
+            [
+                [
+                    'column' => 'heat-source',
+                    'operator' => Clause::CONTAINS,
+                    'value'  => 'heat-pump',
+                ],
+            ],
+            [
+                [
+                    'column' => 'heat-source',
+                    'operator' => Clause::CONTAINS,
+                    'value'  => 'allesbrander',
+                ],
+                [
+                    'column'   => 'cook-type',
+                    'operator' => Clause::EQ,
+                    'value'    => 'gas',
+                ],
+            ],
+        ];
+    }
+
+    protected function getTestCollection()
+    {
+        return collect([
+            "heat-source" => collect([
+                "hr-boiler",
+                "heat-pump",
+            ]),
+            "cook-type"   => 'gas',
+        ]);
     }
 }

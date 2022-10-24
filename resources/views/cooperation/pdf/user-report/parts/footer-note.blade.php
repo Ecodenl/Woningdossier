4cf3ef711317ac1c907831577c66ccb9b26cc580
Yinci <yannisch@hotmail.com>
2022-10-24 15:04:03 +0200

    Updated config, fixed footer

--- resources/views/cooperation/pdf/user-report/parts/footer-note.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/footer-note.blade.php
@@ -6,12 +6,14 @@
     if ( isset($pdf) ) {
         $cooperationFooterText = "{$GLOBALS['_inputSource']->name} - {$GLOBALS['_cooperation']->name} - Pagina {PAGE_NUM}";
 
+        // 595.28 == $pdf->get_width()
         $xForCooperationFooterText = 595 - strlen($cooperationFooterText) * 3;
 
-        $font = $fontMetrics->get_font("Arial, Helvetica, sans-serif", "normal");
+        $font = $fontMetrics->getFont("Arial, Helvetica, sans-serif", "normal");
         $date = date('d-m-Y');
+        // 841.89 == $pdf->get_height()
         $y = 810;
         $pdf->page_text($xForCooperationFooterText, $y, $cooperationFooterText, $font, 6, array(0,0,0));
-        $pdf->page_text(531, $y + 10, $date, $font, 6, array(0,0,0));
+        $pdf->page_text(541, $y + 10, $date, $font, 6, array(0,0,0));
     }
 </script>

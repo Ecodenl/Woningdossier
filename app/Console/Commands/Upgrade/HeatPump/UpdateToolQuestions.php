8d2bf6b750919cedd2cfaae7ff74b530734688eb
Yinci <yannisch@hotmail.com>
2022-10-24 16:21:26 +0200

    Updated mapping for new considerable logic

--- app/Console/Commands/Upgrade/HeatPump/UpdateToolQuestions.php
+++ app/Console/Commands/Upgrade/HeatPump/UpdateToolQuestions.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands\Upgrade\HeatPump;
 
 use App\Helpers\Conditions\ConditionEvaluator;
 use App\Models\Building;
+use App\Models\ComfortLevelTapWater;
 use App\Models\CompletedStep;
 use App\Models\Service;
 use App\Models\ServiceValue;
@@ -346,6 +347,43 @@ class UpdateToolQuestions extends Command
             SubStep::where('slug->nl', 'zonnenboiler')->limit(1)->delete();
         }
 
+        $newWaterQuestion = ToolQuestion::findByShort('new-water-comfort');
+        $newWaterComforts = $newWaterQuestion->toolQuestionCustomValues->keyBy('id');
+        $calculateMap = $newWaterComforts->pluck('id', 'extra.calculate_value');
+
+        $waterComforts = ComfortLevelTapWater::all()->keyBy('id');
+        $waterComfortQuery = DB::table('user_energy_habits')
+            ->whereNotNull('water_comfort_id');
+
+        $total = $waterComfortQuery->count();
+        $this->infoLog("Starting 'old' water comfort to new water comfort for {$total} habits.");
+
+        $i = 0;
+
+        $waterComfortQuery->orderBy('id')->chunkById(100, function ($habits) use (&$i, $total, $newWaterQuestion, $newWaterComforts, $calculateMap, $waterComforts) {
+            foreach ($habits as $habit) {
+                $building = DB::table('buildings')->where('user_id', $habit->user_id)->first();
+
+                $waterComfort = $waterComforts[$habit->water_comfort_id];
+                $tqcvId = $calculateMap[$waterComfort->calculate_value];
+                $tqcv = $newWaterComforts[$tqcvId];
+
+                DB::table('tool_question_answers')
+                    ->updateOrInsert([
+                        'building_id' => $building->id,
+                        'input_source_id' => $habit->input_source_id,
+                        'tool_question_id' => $newWaterQuestion->id,
+                        'tool_question_custom_value_id' => $tqcv->id,
+                    ], ['answer' => $tqcv->short]);
+
+                ++$i;
+
+                if ($i % 1000 === 0) {
+                    $this->infoLog("{$i} / {$total}");
+                }
+            }
+        });
+
         $heatPump = Service::findByShort('heat-pump');
         $collectiveValue = ServiceValue::where('service_id', $heatPump->id)->byValue('Collectieve warmtepomp')->first();
 
@@ -441,60 +479,90 @@ class UpdateToolQuestions extends Command
             $collectiveValue->delete();
         }
 
-        $this->infoLog("Starting considerable to 'new situation' considerable question map");
+        $this->infoLog("Starting considerable to 'new situation' 'considerable' question map");
 
         // Map considerables to new question
-        // TODO: Get mapping
-        //$considerableQuestion = ToolQuestion::findByShort('heat-source-considerable');
-        //$hrBoilerCustomValue = ToolQuestionCustomValue::where('tool_question_id', $considerableQuestion->id)
-        //    ->whereShort('hr-boiler')
-        //    ->first();
-        //$sunBoilerCustomValue = ToolQuestionCustomValue::where('tool_question_id', $considerableQuestion->id)
-        //    ->whereShort('sun-boiler')
-        //    ->first();
-        //$steps = [
-        //    Step::findByShort('high-efficiency-boiler'),
-        //    Step::findByShort('heater'),
-        //];
-        //
-        //foreach ($steps as $considerableStep) {
-        //    $customValueForStep = $considerableStep->short === 'heater' ? $sunBoilerCustomValue : $hrBoilerCustomValue;
-        //
-        //    $usersThatConsiderStepQuery = DB::table('considerables')
-        //        ->where('considerable_type', Step::class)
-        //        ->where('considerable_id', $considerableStep->id)
-        //        ->where('is_considering', 1);
-        //
-        //    $total = $usersThatConsiderStepQuery->count();
-        //    $this->infoLog("{$total} consider {$considerableStep->short}");
-        //
-        //    $i = 0;
-        //
-        //    $usersThatConsiderStepQuery->orderBy('id')->chunkById(100, function ($usersThatConsiderStep) use (&$i, $total, $considerableQuestion, $customValueForStep) {
-        //        foreach ($usersThatConsiderStep as $user) {
-        //            $building = DB::table('buildings')->where('user_id', $user->user_id)->first();
-        //
-        //            DB::table('tool_question_answers')
-        //                ->updateOrInsert([
-        //                    'building_id' => $building->id,
-        //                    'input_source_id' => $user->input_source_id,
-        //                    'tool_question_id' => $considerableQuestion->id,
-        //                    'tool_question_custom_value_id' => $customValueForStep->id,
-        //                ], ['answer' => $customValueForStep->short]);
-        //
-        //            ++$i;
-        //
-        //            if ($i % 1000 === 0) {
-        //                $this->infoLog("{$i} / {$total}");
-        //            }
-        //        }
-        //    });
-        //
-        //    DB::table('considerables')
-        //        ->where('considerable_type', Step::class)
-        //        ->where('considerable_id', $considerableStep->id)
-        //        ->delete();
-        //}
+        $steps = [
+            Step::findByShort('high-efficiency-boiler'),
+            Step::findByShort('heater'),
+        ];
+        $questionsToFillFor = [
+            ToolQuestion::findByShort('new-heat-source'),
+            ToolQuestion::findByShort('new-heat-source-warm-tap-water'),
+        ];
+
+        foreach ($steps as $considerableStep) {
+            $customValueForStep = $considerableStep->short === 'heater' ? 'sun-boiler' : 'hr-boiler';
+
+            $usersThatConsiderStepQuery = DB::table('considerables')
+                ->where('considerable_type', Step::class)
+                ->where('considerable_id', $considerableStep->id)
+                ->where('is_considering', 1);
+
+            $total = $usersThatConsiderStepQuery->count();
+            $this->infoLog("{$total} consider {$considerableStep->short}");
+
+            $i = 0;
+
+            $usersThatConsiderStepQuery->orderBy('id')->chunkById(100, function ($usersThatConsiderStep) use (&$i, $total, $questionsToFillFor, $customValueForStep) {
+                foreach ($usersThatConsiderStep as $user) {
+                    $building = DB::table('buildings')->where('user_id', $user->user_id)->first();
+
+                    foreach ($questionsToFillFor as $tq) {
+                        $tqcv = ToolQuestionCustomValue::where('tool_question_id', $tq->id)
+                            ->whereShort($customValueForStep)
+                            ->first();
+
+                        DB::table('tool_question_answers')
+                            ->updateOrInsert([
+                                'building_id' => $building->id,
+                                'input_source_id' => $user->input_source_id,
+                                'tool_question_id' => $tq->id,
+                                'tool_question_custom_value_id' => $tqcv->id,
+                            ], ['answer' => $tqcv->short]);
+                    }
+
+                    // Because we're mapping we only need to check for the heat-source question
+                    $heatSourceQuestion = ToolQuestion::findByShort('heat-source');
+                    $oldTqcv = ToolQuestionCustomValue::where('tool_question_id', $heatSourceQuestion->id)
+                        ->whereShort($customValueForStep)
+                        ->first();
+
+                    $exists = DB::table('tool_question_answers')
+                        ->where('building_id', $building->id)
+                        ->where('input_source_id', $user->input_source_id)
+                        ->where('tool_question_id', $heatSourceQuestion->id)
+                        ->where('tool_question_custom_value_id', $oldTqcv->id)
+                        ->exists();
+
+                    if ($exists) {
+                        $replaceQuestion = ToolQuestion::findByShort("{$customValueForStep}-replace");
+                        $replaceTqcv = ToolQuestionCustomValue::where('tool_question_id', $replaceQuestion->id)
+                            ->whereShort(1)
+                            ->first();
+
+                        DB::table('tool_question_answers')
+                            ->updateOrInsert([
+                                'building_id' => $building->id,
+                                'input_source_id' => $user->input_source_id,
+                                'tool_question_id' => $replaceQuestion->id,
+                                'tool_question_custom_value_id' => $replaceTqcv->id,
+                            ], ['answer' => $replaceTqcv->short]);
+                    }
+
+                    ++$i;
+
+                    if ($i % 1000 === 0) {
+                        $this->infoLog("{$i} / {$total}");
+                    }
+                }
+            });
+
+            DB::table('considerables')
+                ->where('considerable_type', Step::class)
+                ->where('considerable_id', $considerableStep->id)
+                ->delete();
+        }
 
         $residentialStatus = Step::findByShort('residential-status');
         $completedStepsQuery = CompletedStep::allInputSources()->where('step_id', $residentialStatus->id);

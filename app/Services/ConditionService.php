28a61edebcba9626c2c7822cb536a049420dddd6
Yinci <yannisch@hotmail.com>
2022-11-02 08:07:03 +0100

    Change conditional logic to fix flawed assumption

--- app/Services/ConditionService.php
+++ app/Services/ConditionService.php
@@ -84,10 +84,7 @@ class ConditionService
             $conditions = array_merge($conditions, $this->model->subStep->conditions ?? []);
         } else {
             // So it isn't a simple case. We will have to iterate. In this case it's a ToolQuestion or
-            // ToolCalculationResult. We fetch all SubSteps and SubSteppables that have conditions to create one
-            // giant conditional array.
-            $conditions = [];
-
+            // ToolCalculationResult. We fetch all SubSteps and SubSteppables to check their conditions.
             $subSteps = $this->model->subSteps()
                 ->where('sub_steps.conditions', '!=', null)
                 ->where('sub_steps.conditions', '!=', DB::raw("cast('[]' as json)"))
@@ -98,15 +95,67 @@ class ConditionService
                 ->wherePivot('conditions', '!=', DB::raw("cast('[]' as json)"))
                 ->get();
 
+            // We sadly can't make one big array; consider the case that both a SubStep and SubSteppable have
+            // conditions; it might be that the SubSteppable passes, but the SubStep does not. It however does
+            // create a truthy OR statement. We don't want that, so we will evaluate per SubStep(pable), and combine
+            // if needed...
+            $passes = false;
             foreach ($subSteps as $subStep) {
-                $conditions = array_merge($conditions, $subStep->conditions);
-            }
-            foreach ($subSteppables as $subSteppable) {
-                $conditions = array_merge($conditions, $subSteppable->pivot->conditions);
+                $conditions = $subStep->conditions;
+                $answers = $evaluator->getToolAnswersForConditions($conditions)->merge($answers);
+
+                $passes = $evaluator->evaluateCollection($conditions, $answers);
+
+                if ($passes) {
+                    if (! empty($subStep->pivot->conditions)) {
+                        $conditions = $subStep->pivot->conditions;
+                        $answers = $evaluator->getToolAnswersForConditions($conditions)->merge($answers);
+
+                        $passes = $evaluator->evaluateCollection($conditions, $answers);
+                        if ($passes) {
+                            // We're done here
+                            break;
+                        }
+                    } else {
+                        // We're done here
+                        break;
+                    }
+                }
             }
 
-            // In the scenario that NONE of the SubSteppables or SubSteps have conditions, they will remain empty, and
-            // evaluation will pass
+            if (! $passes) {
+                // We haven't passed yet, so we will check the SubSteppables
+                foreach ($subSteppables as $subSteppable) {
+                    $conditions = $subSteppable->pivot->conditions;
+                    $answers = $evaluator->getToolAnswersForConditions($conditions)->merge($answers);
+
+                    $passes = $evaluator->evaluateCollection($conditions, $answers);
+
+                    if ($passes) {
+                        if (! empty($subStep->conditions)) {
+                            $conditions = $subStep->conditions;
+                            $answers = $evaluator->getToolAnswersForConditions($conditions)->merge($answers);
+
+                            $passes = $evaluator->evaluateCollection($conditions, $answers);
+                            if ($passes) {
+                                // We're done here
+                                break;
+                            }
+                        } else {
+                            // We're done here
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // In the scenario that NONE of the SubSteppables or SubSteps have conditions, passes will remain false.
+            // We will know if both collections are empty.
+            if (! $passes) {
+                $passes = $subSteps->count() === 0 && $subSteppables->count() === 0;
+            }
+
+            return $passes;
         }
 
         $answers = $evaluator->getToolAnswersForConditions($conditions)->merge($answers);

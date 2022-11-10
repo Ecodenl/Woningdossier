d7c00c77e80a27f88958b4475d77fb1a1a2412c1
Bodhi <bodhilooij1998@gmail.com>
2022-11-10 16:09:27 +0100

    Added the AddressService.php which deprecates the PicoHelper.php.

---
+++ app/Services/AddressService.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Services;
+
+use Ecodenl\LvbagPhpWrapper\Client;
+use Ecodenl\LvbagPhpWrapper\Lvbag;
+use Illuminate\Support\Facades\Log;
+
+class AddressService {
+
+    public static function list($postalCode, $number, string $houseNumberExtension = ""): array
+    {
+        $client = Client::init(config('hoomdossier.services.bag.secret'), 'epsg:28992');
+
+        $attributes = [
+            'postcode' => $postalCode,
+            'huisnummer' => $number,
+            // we always want a exact match, rather no result than wrong one.
+            'exacteMatch' => true,
+        ];
+        if (!empty($houseNumberExtension)) {
+            $attributes['huisletter'] = $houseNumberExtension;
+        }
+        $addresses = Lvbag::init($client)
+            ->adresUitgebreid()
+            ->list($attributes);
+
+        $result = [];
+
+        if (!is_null($addresses)) {
+            $address = array_shift($addresses);
+            // best match
+            $result = [
+                'id' => $address['nummeraanduidingIdentificatie'] ?? '',
+                'street' => $address['openbareRuimteNaam'] ?? '',
+                'number' => $address['huisnummer'] ?? '',
+                'postal_code' => $address['postcode'] ?? '',
+                'house_number_extension' => $address['huisletter'] ?? $houseNumberExtension,
+                'city' => $address['woonplaatsNaam'] ?? '',
+                'build_year' => $address['bouwjaar'] ?? 1930,
+                'surface' => $address['oppervlakte'] ?? 0,
+            ];
+            Log::debug($result);
+        }
+
+        return $result;
+    }
+}

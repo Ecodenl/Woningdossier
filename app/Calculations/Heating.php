65ce3d78d017ac1511c17d898bfff58d9ebbce49
pvankouteren <pvankouteren@gmail.com>
2022-11-23 00:10:45 +0100

    Some logging for debugging, factored in the (new-)boiler-type, but unsure if it's useful..

--- app/Calculations/Heating.php
+++ app/Calculations/Heating.php
@@ -8,10 +8,13 @@ use App\Helpers\KeyFigures\Heater\KeyFigures;
 use App\Models\ComfortLevelTapWater;
 use App\Models\HeatPumpCharacteristic;
 use App\Models\KeyFigureConsumptionTapWater;
+use App\Models\Service;
+use App\Models\ServiceValue;
 use App\Models\ToolQuestion;
 use App\Models\ToolQuestionCustomValue;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class Heating extends Calculator
 {
@@ -25,29 +28,35 @@ class Heating extends Calculator
             'heat-source-warm-tap-water',
             'water-comfort',
             'heat-pump-type',
-            'boiler-setting-comfort-heat'
+            'boiler-setting-comfort-heat',
+            'boiler-type'
         );
         $wtwNew     = $this->calculateTapWater(
             'new-heat-source',
             'new-heat-source-warm-tap-water',
             'new-water-comfort',
             'new-heat-pump-type',
-            'new-boiler-setting-comfort-heat'
+            'new-boiler-setting-comfort-heat',
+            'new-boiler-type'
         );
 
         $wtw = [
             'current' => $wtwCurrent,
             'new'     => $wtwNew,
         ];
+        Log::debug(__METHOD__ . " - wtw calculated: " . json_encode($wtw));
 
         $cooking = [
             'current' => $this->energyUsageForCooking('cook-type'),
             'new'     => $this->energyUsageForCooking('new-cook-type'),
         ];
 
+        Log::debug(__METHOD__ . " - cooking calculated: " . json_encode($wtw));
+
         $heatingCurrent = $this->calculateHeating(
             'heat-source',
             'heat-source-warm-tap-water',
+            'boiler-type',
             $amountGas = $this->getAnswer('amount-gas') ?? 0,
             $cooking['current'],
             $wtw['current']
@@ -55,6 +64,7 @@ class Heating extends Calculator
         $heatingNew     = $this->calculateHeating(
             'new-heat-source',
             'new-heat-source-warm-tap-water',
+            'new-boiler-type',
             data_get($heatingCurrent, 'gas', 0),
             $cooking['new'],
             $wtw['new']
@@ -64,6 +74,31 @@ class Heating extends Calculator
             'current' => $heatingCurrent,
             'new'     => $heatingNew,
         ];
+
+        Log::debug(__METHOD__ . " - heating calculated: " . json_encode($heating));
+
+        // correct wtw heating in the case the heat-source is not hr-boiler or
+        // district-heating: gas for heating is 0. wtw = amount gas - cooking
+        // because cooking is fairly stable / always around 37, the wtw gas
+        // usage should be the leftover of the amount gas - cooking.
+        /** @var array $heatSources */
+        foreach(['current' => 'heat-source', 'new' => 'new-heat-source'] as $situation => $heatSourceShort) {
+            $heatSources = $this->getAnswer($heatSourceShort) ?? [];
+            // if $heatSources does not contain 'hr-boiler', we cannot calculate.
+            if ( ! in_array('hr-boiler', $heatSources) && ! in_array(
+                    'district-heating',
+                    $heatSources
+                )) {
+                Log::debug(__METHOD__ . ' - correcting situation (because no hr-boiler / district-heating => assign gas rest to wtw and not to heating');
+                $wtw[$situation]['gas'] = $amountGas - $cooking[$situation]['gas'];
+            }
+        }
+
+        Log::debug("End result: " . json_encode( [
+                'heating'   => $heating,
+                'tap_water' => $wtw,
+                'cooking'   => $cooking,
+            ]));
 
         return [
             'heating'   => $heating,
@@ -75,6 +110,7 @@ class Heating extends Calculator
     protected function calculateHeating(
         string $heatSourceShort,
         string $heatSourceWtwShort,
+        string $boilerTypeShort,
         int $amountGas,
         array $cookingUsage,
         array $wtwUsage
@@ -90,6 +126,7 @@ class Heating extends Calculator
         $heatSources = $this->getAnswer($heatSourceShort) ?? [];
         // if $heatSources does not contain 'hr-boiler', we cannot calculate.
         if ( ! in_array('hr-boiler', $heatSources)) {
+            Log::debug(__METHOD__ . ' - No HR boiler, returning' . json_encode($result));
             return $result;
         }
 
@@ -101,11 +138,30 @@ class Heating extends Calculator
         );
 
         $heatingGasUsage = $amountGas - data_get($cookingUsage, 'gas', 0);
+        Log::debug(__METHOD__ . ' - heatingGasUsage = ' . $heatingGasUsage . ' = ' . $amountGas . ' - ' . data_get($cookingUsage, 'gas', 0));
 
         if (in_array(
             $primaryWtwHeatSourceShort,
             ['hr-boiler', 'kitchen-geyser', 'district-heating']
         )) {
+            // At the moment I'm not sure about this: there is a boiler
+            // efficiency, BUT if we apply this (for both wtw and heating) to the
+            // amount gas, we end up with two times netto gas usage which doesn't
+            // add up to the amount gas (obviously) and which could lead to less
+            // than 100% savings when you completely ditch the usage of gas.
+
+            /*
+            $boiler = ToolHelper::getServiceValueByCustomValue('boiler', $boilerTypeShort,
+                $this->getAnswer($boilerTypeShort));
+            if (!$boiler instanceof ServiceValue) {
+                // if even the current boiler wasn't present, the user probably already
+                // has a heat pump, so we will calculate with the most efficient boiler
+                $boiler = Service::findByShort('boiler')->values()->orderByDesc('calculate_value')->limit(1)->first();
+            }
+            */
+
+            Log::debug(__METHOD__ . ' - hr-boiler/kitchen-geyser/district-heating is primary wtw');
+            Log::debug('heatingGasUsage = ' . $heatingGasUsage . ' - ' . data_get($wtwUsage, 'gas', 0));
             $heatingGasUsage -= data_get($wtwUsage, 'gas', 0);
         }
 
@@ -119,7 +175,8 @@ class Heating extends Calculator
         string $heatSourceWtwShort,
         string $waterComfortShort,
         string $heatPumpTypeShort = '',
-        string $boilerSettingComfortHeatShort = ''
+        string $boilerSettingComfortHeatShort = '',
+        string $boilerTypeShort = ''
     ): array {
         // Note the energy usage for tap water will be EITHER gas (m3) or
         // electricity (kWh). Which means one of the two array keys will be 0
@@ -133,6 +190,7 @@ class Heating extends Calculator
         $heatSources = $this->getAnswer($heatSourceShort) ?? [];
         // if $heatSources does not contain 'hr-boiler', we cannot calculate.
         if ( ! in_array('hr-boiler', $heatSources)) {
+            Log::debug(__METHOD__ . ' - No HR boiler, returning' . json_encode($result));
             return $result;
         }
 
@@ -144,6 +202,7 @@ class Heating extends Calculator
         );
 
         if (empty($primaryWtwHeatSourceShort) || $primaryWtwHeatSourceShort == 'none') {
+            Log::debug(__METHOD__ . ' - No primary wtw heat source, returning' . json_encode($result));
             return $result;
         }
 
@@ -168,9 +227,11 @@ class Heating extends Calculator
             $comfortLevelTapWater
         );
         if ( ! $consumption instanceof KeyFigureConsumptionTapWater) {
+            Log::debug(__METHOD__ . ' - No key figure for tap water consumption, returning' . json_encode($result));
             return $result;
         }
         $gasUsageWtw = $consumption->energy_consumption;
+        Log::debug(__METHOD__ . ' - gasUsageWtw: ' . $gasUsageWtw);
 
         // solar boiler (deduction) amount comes into play: if there's a solar
         // boiler
@@ -179,6 +240,7 @@ class Heating extends Calculator
             'electricity' => 0,
         ];
         if (in_array('sun-boiler', $heatSourcesWtw)) {
+            Log::debug(__METHOD__ . ' - There is a solar boiler');
             // there is a solar-boiler. Calculate the yield.
             $solarBoilerInfo  = Heater::calculate(
                 $this->building,
@@ -188,6 +250,7 @@ class Heating extends Calculator
                 'gas'         => $solarBoilerInfo['savings_gas'],
                 'electricity' => $solarBoilerInfo['production_heat'],
             ];
+            Log::debug(__METHOD__ . ' - solar boiler yield: ' . json_encode($solarBoilerYield));
         }
 
         // 8.972 = Kengetallen::gasKwhPerM3();
@@ -195,6 +258,24 @@ class Heating extends Calculator
             $primaryWtwHeatSourceShort,
             ['hr-boiler', 'kitchen-geyser', 'district-heating']
         )) {
+            // At the moment I'm not sure about this: there is a boiler
+            // efficiency, BUT if we apply this (for both wtw and heating) to the
+            // amount gas, we end up with two times netto gas usage which doesn't
+            // add up to the amount gas (obviously) and which could lead to less
+            // than 100% savings when you completely ditch the usage of gas.
+
+            /*
+            $boiler = ToolHelper::getServiceValueByCustomValue('boiler', $boilerTypeShort,
+                $this->getAnswer($boilerTypeShort));
+            if (!$boiler instanceof ServiceValue) {
+                // if even the current boiler wasn't present, the user probably already
+                // has a heat pump, so we will calculate with the most efficient boiler
+                $boiler = Service::findByShort('boiler')->values()->orderByDesc('calculate_value')->limit(1)->first();
+            }
+            */
+
+            Log::debug(__METHOD__ . ' - primary wtw hr-boiler/kitchen-geyser/district-heating');
+
             // step 1: Gas usage wtw from table
             $energyConsumption = $gasUsageWtw;
 
@@ -204,6 +285,7 @@ class Heating extends Calculator
             $result['gas'] = round($energyConsumption, 4);
         }
         if (in_array($primaryWtwHeatSourceShort, ['electric-boiler'])) {
+            Log::debug(__METHOD__ . ' - primary wtw electric-boiler');
             // step 1: Gas usage wtw from table * gasKwhPerM3 (m3 -> kWh)
             $energyConsumption = $gasUsageWtw * Kengetallen::gasKwhPerM3();
 
@@ -216,6 +298,7 @@ class Heating extends Calculator
             $primaryWtwHeatSourceShort,
             ['heat-pump', 'heat-pump-boiler']
         )) {
+            Log::debug(__METHOD__ . ' - primary wtw heat-pump/heat-pump-boiler');
             // step 1: Gas usage wtw from table * gasKwhPerM3 / SCOP (m3 -> kWh)
             $heatPumpConfigurable = ToolHelper::getServiceValueByCustomValue(
                 'heat-pump',
@@ -236,10 +319,13 @@ class Heating extends Calculator
                 $heatPumpConfigurable,
                 $heatingTemperature
             );
+            Log::debug(__METHOD__ . ' - characteristic id = ' . optional($characteristics)->id);
             $scop                 = 1;
             if ($characteristics instanceof HeatPumpCharacteristic) {
                 $scop = max($characteristics, 1); // prevent divide by 0
             }
+
+            Log::debug(__METHOD__ . ' - scop = ' . $scop);
 
             $energyConsumption = $gasUsageWtw * Kengetallen::gasKwhPerM3(
                 ) / $scop;
@@ -249,6 +335,8 @@ class Heating extends Calculator
 
             $result['electricity'] = round($energyConsumption, 4);
         }
+
+        Log::debug(__METHOD__ . ' - end result: ' . json_encode($result));
 
         return $result;
     }

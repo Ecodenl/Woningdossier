7e0ba7374e932a824fcbc3357a4b8d9b3fc2bf83
Yinci <yannisch@hotmail.com>
2022-12-06 15:49:08 +0100

    Revert to original state

--- resources/views/cooperation/pdf/user-report/parts/footer-note.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/footer-note.blade.php
@@ -6,21 +6,19 @@
     if ( isset($pdf) ) {
         $cooperationFooterText = "{$GLOBALS['_inputSource']->name} - {$GLOBALS['_cooperation']->name} - Pagina {PAGE_NUM}";
 
-        {{-- 595.28 == $pdf->get_width() --}}
+        // 595.28 == $pdf->get_width()
         $xForCooperationFooterText = 595 - strlen($cooperationFooterText) * 3;
-        \Log::debug($cooperationFooterText);
-        \Log::debug($xForCooperationFooterText);
 
         $font = $fontMetrics->getFont("Arial, Helvetica, sans-serif", "normal");
         $date = date('d-m-Y');
-        {{-- 841.89 == $pdf->get_height() --}}
+        // 841.89 == $pdf->get_height()
         $y = 810;
         $pdf->page_text($xForCooperationFooterText, $y, $cooperationFooterText, $font, 6, array(0,0,0));
 
-        {{-- So due to how DomPdf renders fixed elements, the content will be overlapped if we do it like this. --}}
-        {{-- For now we will use a fixed size for a single digit page. --}}
-        {{--$xForDate = 533.5 + strlen($PAGE_NUM) * 3;--}}
-        $xForDate = 534;
+        // So due to how DomPdf renders fixed elements, the content will be overlapped if we do it like this.
+        // For now we will use a fixed size for a single digit page.
+        //$xForDate = 533.5 + strlen($PAGE_NUM) * 3;
+        $xForDate = 536.5;
         $pdf->page_text($xForDate, ($y + 10), $date, $font, 6, array(0,0,0));
     }
 </script>

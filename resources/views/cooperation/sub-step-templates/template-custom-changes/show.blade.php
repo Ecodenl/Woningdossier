069dd5e0909dc4e79bb4fd60f0888ed52e1dbe90
Yinci <yannisch@hotmail.com>
2022-12-08 10:47:01 +0100

    Labels per scan

--- resources/views/cooperation/sub-step-templates/template-custom-changes/show.blade.php
+++ resources/views/cooperation/sub-step-templates/template-custom-changes/show.blade.php
@@ -1,12 +1,12 @@
 <div class="w-full divide-y-2 divide-blue-500 divide-opacity-20 space-y-5">
     @component('cooperation.frontend.layouts.components.form-group', [
         'class' => 'form-group-heading',
-        'label' => __('livewire/cooperation/frontend/tool/quick-scan/custom-changes.question.label'),
+        'label' => __("livewire/cooperation/frontend/tool/simple-scan/custom-changes.question.{$scan->short}.label"),
         'withInputSource' => false,
     ])
         @slot('modalBodySlot')
             <p>
-                @lang('livewire/cooperation/frontend/tool/quick-scan/custom-changes.question.help')
+                @lang("livewire/cooperation/frontend/tool/simple-scan/custom-changes.question.{$scan->short}.help")
             </p>
         @endslot
 

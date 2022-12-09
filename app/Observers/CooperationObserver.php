41f376a0d723c163505434d84449d0e6b5b2bfcb
Bodhi <bodhilooij1998@gmail.com>
2022-12-09 15:26:16 +0100

    Adjusted the cooperation observer so upon creating it will receive the quick-scan.

--- app/Observers/CooperationObserver.php
+++ app/Observers/CooperationObserver.php
@@ -3,12 +3,16 @@
 namespace App\Observers;
 
 use App\Models\Cooperation;
+use App\Services\CooperationScanService;
 use Database\Seeders\CooperationMeasureApplicationsTableSeeder;
 
 class CooperationObserver
 {
     public function created(Cooperation $cooperation)
     {
+        // Give the cooperation the default quick-scan upon register.
+        CooperationScanService::init($cooperation)->syncScan('quick-scan');
+
         $cooperation->cooperationMeasureApplications()->createMany(CooperationMeasureApplicationsTableSeeder::MEASURES);
     }
 }

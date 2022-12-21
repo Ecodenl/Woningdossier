6471d47af0b7f95f111ec84f9133bddf31effe1e
Yinci <yannisch@hotmail.com>
2022-12-21 13:22:40 +0100

    Update mapping

--- app/Console/Commands/Upgrade/HeatPump/WaterCalc/UpdateToolQuestions.php
+++ app/Console/Commands/Upgrade/HeatPump/WaterCalc/UpdateToolQuestions.php
@@ -41,189 +41,128 @@ class UpdateToolQuestions extends Command
      */
     public function handle()
     {
-        $buildingsToRecalculate = [];
-
         $relevantInputSources = DB::table('input_sources')->get();
 
-        $heatSourceQuestion = ToolQuestion::findByShort('heat-source');
-        $heatSourceQuestionOther = ToolQuestion::findByShort('heat-source-other');
         $heatSourceWaterQuestion = ToolQuestion::findByShort('heat-source-warm-tap-water');
-        $heatSourceWaterQuestionOther = ToolQuestion::findByShort('heat-source-warm-tap-water-other');
-        $newHeatSourceQuestion = ToolQuestion::findByShort('new-heat-source');
-        $newHeatSourceWaterQuestion = ToolQuestion::findByShort('new-heat-source-warm-tap-water');
-
-        $this->infoLog("Deleting all (new)-heat-source-warm-tap-water answers except for none and sun-boiler");
-        DB::table('tool_question_answers')
-            ->where('tool_question_id', $heatSourceWaterQuestion->id)
-            ->where('answer', '!=', 'sun-boiler')
-            ->where('answer', '!=', 'none')
-            ->delete();
-
-        DB::table('tool_question_answers')
-            ->where('tool_question_id', $newHeatSourceWaterQuestion->id)
-            ->where('answer', '!=', 'sun-boiler')
-            ->delete();
 
         foreach ($relevantInputSources as $inputSource) {
-            $buildingsQuery = DB::table('tool_question_answers')
-                ->where('tool_question_id', $heatSourceQuestion->id)
+            $buildings = DB::table('tool_question_answers')
+                ->select('building_id')
+                ->where('tool_question_id', $heatSourceWaterQuestion->id)
                 ->where('input_source_id', $inputSource->id)
-                ->select('id', 'building_id', 'input_source_id')
-                ->distinct();
+                ->groupBy('building_id')
+                ->havingRaw('COUNT(*) > 1')
+                ->get();
 
-            $total = $buildingsQuery->count();
-            $this->infoLog("Starting heat-source to single heat-source-warm-tap-water map for input source {$inputSource->id} for a total of {$total} answers");
+            $total = $buildings->count();
+            $this->infoLog("{$total} buildings have more than 1 answer for heat-source-warm-tap-water for source {$inputSource->short}");
 
             $i = 0;
 
-            $buildingsQuery->orderBy('id')->chunkById(100, function ($buildings) use (&$buildingsToRecalculate, &$i, $total, $heatSourceQuestion, $heatSourceWaterQuestion, $heatSourceQuestionOther, $heatSourceWaterQuestionOther) {
-                // Map relevant answers from heat-source to heat-source-warm-tap-water
-                // $building is a ToolQuestionCustomValue
-                foreach ($buildings as $building) {
-                    $hasNone = DB::table('tool_question_answers')
-                        ->where('tool_question_id', $heatSourceWaterQuestion->id)
-                        ->where('input_source_id', $building->input_source_id)
-                        ->where('building_id', $building->building_id)
-                        ->where('answer', 'none')
-                        ->first() instanceof \stdClass;
+            foreach ($buildings as $building) {
+                $answers = DB::table('tool_question_answers')
+                    ->where('tool_question_id', $heatSourceWaterQuestion->id)
+                    ->where('building_id', $building->building_id)
+                    ->where('input_source_id', $inputSource->id)
+                    ->get();
 
-                    $answersForBuilding = DB::table('tool_question_answers')
-                        ->where('tool_question_id', $heatSourceQuestion->id)
-                        ->where('input_source_id', $building->input_source_id)
-                        ->where('building_id', $building->building_id)
-                        ->pluck('answer')
-                        ->toArray();
+                $idsToKeep = [];
 
-                    $newAnswerForBuilding = [];
-
-                    if (in_array('hr-boiler', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['hr-boiler'];
-                    } elseif (in_array('heat-pump', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['heat-pump'];
-                    } elseif (in_array('district-heating', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['district-heating'];
-                    } elseif (in_array('infrared', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['electric-boiler'];
-                    } elseif (in_array('none', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['none'];
-
-                        if (! $hasNone) {
-                            // User has no different answer, so we set the one from heat source
-
-                            $otherAnswer =  DB::table('tool_question_answers')
-                                ->where("building_id", $building->building_id)
-                                ->where('input_source_id', $building->input_source_id)
-                                ->where('tool_question_id', $heatSourceQuestionOther->id)
-                                ->first()->answer ?? null;
-
-                            DB::table('tool_question_answers')
-                                ->updateOrInsert([
-                                    'building_id' => $building->building_id,
-                                    'input_source_id' => $building->input_source_id,
-                                    'tool_question_id' => $heatSourceWaterQuestionOther->id,
-                                ], ['answer' => $otherAnswer, 'created_at' => now(), 'updated_at' => now()]);
-                        }
-                    }
-
-                    // Always add sun-boiler as extra
-                    if (in_array('sun-boiler', $answersForBuilding)) {
-                        $newAnswerForBuilding[] = 'sun-boiler';
-                    }
-
-                    // Ensure we delete this if it's not in the new array
-                    if ($hasNone && ! in_array('none', $newAnswerForBuilding)) {
-                        DB::table('tool_question_answers')
-                            ->where('tool_question_id', $heatSourceWaterQuestion->id)
-                            ->where("building_id", $building->building_id)
-                            ->where('input_source_id', $building->input_source_id)
-                            ->where('answer', '=', 'none')
-                            ->delete();
-                    }
-
-                    foreach ($newAnswerForBuilding as $newAnswer) {
-                        $customValueId = ToolQuestionCustomValue::where('tool_question_id', $heatSourceWaterQuestion->id)
-                            ->whereShort($newAnswer)
-                            ->first()->id;
-
-                        DB::table('tool_question_answers')
-                            ->updateOrInsert([
-                                'building_id' => $building->building_id,
-                                'input_source_id' => $building->input_source_id,
-                                'tool_question_id' => $heatSourceWaterQuestion->id,
-                                'tool_question_custom_value_id' => $customValueId,
-                            ], ['answer' => $newAnswer, 'created_at' => now(), 'updated_at' => now()]);
-                    }
-
-                    $buildingsToRecalculate[] = $building->building_id;
-                    ++$i;
-
-                    if ($i % 1000 === 0) {
-                        $this->infoLog("{$i} / {$total}");
-                    }
+                if ($answers->contains('answer', 'hr-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'hr-boiler')->first()->id;
+                } elseif ($answers->contains('answer', 'heat-pump')) {
+                    $idsToKeep[] = $answers->where('answer', 'heat-pump')->first()->id;
+                } elseif ($answers->contains('answer', 'district-heating')) {
+                    $idsToKeep[] = $answers->where('answer', 'district-heating')->first()->id;
+                } elseif ($answers->contains('answer', 'electric-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'electric-boiler')->first()->id;
+                } elseif ($answers->contains('answer', 'heat-pump-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'heat-pump-boiler')->first()->id;
+                } elseif ($answers->contains('answer', 'kitchen-geyser')) {
+                    $idsToKeep[] = $answers->where('answer', 'kitchen-geyser')->first()->id;
+                } elseif ($answers->contains('answer', 'none')) {
+                    $idsToKeep[] = $answers->where('answer', 'none')->first()->id;
                 }
-            });
 
-            $buildingsQuery = DB::table('tool_question_answers')
-                ->where('tool_question_id', $newHeatSourceQuestion->id)
+                // Always keep sun boiler
+                if ($answers->contains('answer', 'sun-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'sun-boiler')->first()->id;
+                }
+
+                DB::table('tool_question_answers')
+                    ->where('tool_question_id', $heatSourceWaterQuestion->id)
+                    ->where('building_id', $building->building_id)
+                    ->where('input_source_id', $inputSource->id)
+                    ->whereNotIn('id', $idsToKeep)
+                    ->delete();
+
+                ++$i;
+
+                if ($i % 1000 === 0) {
+                    $this->infoLog("{$i} / {$total}");
+                }
+            }
+        }
+
+        $newHeatSourceWaterQuestion = ToolQuestion::findByShort('new-heat-source-warm-tap-water');
+
+        foreach ($relevantInputSources as $inputSource) {
+            $buildings = DB::table('tool_question_answers')
+                ->select('building_id')
+                ->where('tool_question_id', $newHeatSourceWaterQuestion->id)
                 ->where('input_source_id', $inputSource->id)
-                ->select('id', 'building_id', 'input_source_id')
-                ->distinct();
+                ->groupBy('building_id')
+                ->havingRaw('COUNT(*) > 1')
+                ->get();
 
-            $total = $buildingsQuery->count();
-            $this->infoLog("Starting new-heat-source to single new-heat-source-warm-tap-water map for input source {$inputSource->id} for a total of {$total} answers");
+            $total = $buildings->count();
+            $this->infoLog("{$total} buildings have more than 1 answer for new-heat-source-warm-tap-water for source {$inputSource->short}");
 
             $i = 0;
 
-            $buildingsQuery->orderBy('id')->chunkById(100, function ($buildings) use (&$buildingsToRecalculate, &$i, $total, $newHeatSourceQuestion, $newHeatSourceWaterQuestion) {
-                // Map relevant answers from new-heat-source to new-heat-source-warm-tap-water
-                // $building is a ToolQuestionCustomValue
-                foreach ($buildings as $building) {
-                    $answersForBuilding = DB::table('tool_question_answers')
-                        ->where('tool_question_id', $newHeatSourceQuestion->id)
-                        ->where('input_source_id', $building->input_source_id)
-                        ->where('building_id', $building->building_id)
-                        ->pluck('answer')
-                        ->toArray();
+            foreach ($buildings as $building) {
+                $answers = DB::table('tool_question_answers')
+                    ->where('tool_question_id', $newHeatSourceWaterQuestion->id)
+                    ->where('building_id', $building->building_id)
+                    ->where('input_source_id', $inputSource->id)
+                    ->get();
 
-                    $newAnswerForBuilding = [];
+                $idsToKeep = [];
 
-                    if (in_array('hr-boiler', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['hr-boiler'];
-                    } elseif (in_array('heat-pump', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['heat-pump'];
-                    } elseif (in_array('district-heating', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['district-heating'];
-                    } elseif (in_array('infrared', $answersForBuilding)) {
-                        $newAnswerForBuilding = ['electric-boiler'];
-                    }
-
-                    // Always add sun-boiler as extra
-                    if (in_array('sun-boiler', $answersForBuilding)) {
-                        $newAnswerForBuilding[] = 'sun-boiler';
-                    }
-
-                    foreach ($newAnswerForBuilding as $newAnswer) {
-                        $customValueId = ToolQuestionCustomValue::where('tool_question_id', $newHeatSourceWaterQuestion->id)
-                            ->whereShort($newAnswer)
-                            ->first()->id;
-
-                        DB::table('tool_question_answers')
-                            ->updateOrInsert([
-                                'building_id' => $building->building_id,
-                                'input_source_id' => $building->input_source_id,
-                                'tool_question_id' => $newHeatSourceWaterQuestion->id,
-                                'tool_question_custom_value_id' => $customValueId,
-                            ], ['answer' => $newAnswer, 'created_at' => now(), 'updated_at' => now()]);
-                    }
-
-                    $buildingsToRecalculate[] = $building->building_id;
-                    ++$i;
-
-                    if ($i % 1000 === 0) {
-                        $this->infoLog("{$i} / {$total}");
-                    }
+                if ($answers->contains('answer', 'hr-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'hr-boiler')->first()->id;
+                } elseif ($answers->contains('answer', 'heat-pump')) {
+                    $idsToKeep[] = $answers->where('answer', 'heat-pump')->first()->id;
+                } elseif ($answers->contains('answer', 'district-heating')) {
+                    $idsToKeep[] = $answers->where('answer', 'district-heating')->first()->id;
+                } elseif ($answers->contains('answer', 'electric-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'electric-boiler')->first()->id;
+                } elseif ($answers->contains('answer', 'heat-pump-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'heat-pump-boiler')->first()->id;
+                } elseif ($answers->contains('answer', 'kitchen-geyser')) {
+                    $idsToKeep[] = $answers->where('answer', 'kitchen-geyser')->first()->id;
+                } elseif ($answers->contains('answer', 'none')) {
+                    $idsToKeep[] = $answers->where('answer', 'none')->first()->id;
                 }
-            });
+
+                // Always keep sun boiler
+                if ($answers->contains('answer', 'sun-boiler')) {
+                    $idsToKeep[] = $answers->where('answer', 'sun-boiler')->first()->id;
+                }
+
+                DB::table('tool_question_answers')
+                    ->where('tool_question_id', $newHeatSourceWaterQuestion->id)
+                    ->where('building_id', $building->building_id)
+                    ->where('input_source_id', $inputSource->id)
+                    ->whereNotIn('id', $idsToKeep)
+                    ->delete();
+
+                ++$i;
+
+                if ($i % 1000 === 0) {
+                    $this->infoLog("{$i} / {$total}");
+                }
+            }
         }
     }
 

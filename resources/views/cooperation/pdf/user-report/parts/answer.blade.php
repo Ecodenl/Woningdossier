034de288a984f46b0cf1deefc0bb9e113364fb69
Yinci <yannisch@hotmail.com>
2023-01-18 15:54:18 +0100

    Ensure answers show on one line

--- resources/views/cooperation/pdf/user-report/parts/answer.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/answer.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
-    <div class="col-6 {{ $class ?? '' }}">
-        <p class="pr-1">
+    <div class="col-8">
+        <p class="pr-1 {{ $class ?? '' }}">
             {{ $label }}
         </p>
     </div>
-    <div class="col-6">
+    <div class="col-4">
         <p>
             {!! $answer !!}
         </p>

d76faddf56b0365682532f44f20e2655f7a236f7
Yinci <yannisch@hotmail.com>
2023-01-19 12:04:22 +0100

    Spacing

--- resources/views/cooperation/pdf/user-report/parts/comment.blade.php
+++ resources/views/cooperation/pdf/user-report/parts/comment.blade.php
@@ -8,7 +8,7 @@
 <div class="row">
     <div class="col-12">
         <p class="comment">
-            {{ trim($comment) }}
+            {!! nl2br(trim(strip_tags($comment))) !!}
         </p>
     </div>
 </div>

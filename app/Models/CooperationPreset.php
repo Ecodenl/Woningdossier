f4a918d509b1434e049500372e161dfdbf26c1a3
Yinci <yannisch@hotmail.com>
2023-01-26 13:02:49 +0100

    Add migrations and models for cooperation presets

---
+++ app/Models/CooperationPreset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class CooperationPreset extends Model
+{
+    protected $fillable = [
+        'title', 'short',
+    ];
+
+    public function cooperationPresetContents(): HasMany
+    {
+        return $this->hasMany(CooperationPresetContent::class);
+    }
+}

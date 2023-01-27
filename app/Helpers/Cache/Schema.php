700fe28dea96316ea5172d75c22a60a9d48c92f4
Bodhi <bodhilooij1998@gmail.com>
2023-01-27 11:18:29 +0100

    - Added Schema cache class with hasColumn mnethod. Applied it in the GetMyValuesTrait.php.

---
+++ app/Helpers/Cache/Schema.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Helpers\Cache;
+
+use Illuminate\Support\Facades\Cache;
+
+class Schema extends BaseCache
+{
+    const CACHE_KEY_HAS_COLUMN = 'Schema_hasColumn_%s_%s';
+
+    public static function hasColumn($table, $attribute): bool
+    {
+        return Cache::rememberForever(
+            self::getCacheKey(static::CACHE_KEY_HAS_COLUMN, $table, $attribute),
+            function () use ($table, $attribute) {
+                return \Illuminate\Support\Facades\Schema::hasColumn($table, $attribute);
+            }
+        );
+    }
+}

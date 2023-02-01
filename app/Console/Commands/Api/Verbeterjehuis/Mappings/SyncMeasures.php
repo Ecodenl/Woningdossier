118d9088e747d98888f635ff6d0b722827271991
Yinci <yannisch@hotmail.com>
2023-02-01 16:09:34 +0100

    New measure map

--- app/Console/Commands/Api/Verbeterjehuis/Mappings/SyncMeasures.php
+++ app/Console/Commands/Api/Verbeterjehuis/Mappings/SyncMeasures.php
@@ -45,42 +45,42 @@ class SyncMeasures extends Command
     public function handle(MappingService $mappingService)
     {
         $map = [
-            'floor-insulation' => [2934],
-            'bottom-insulation' => [2934],
-            'floor-insulation-research' => [2934],
-            'cavity-wall-insulation' => [2933],
-            'facade-wall-insulation' => [2933],
-            'wall-insulation-research' => [2933],
-            'glass-in-lead' => [2936],
-            'hrpp-glass-only' => [2936],
-            'hrpp-glass-frames' => [2936],
-            'hr3p-frames' => [2936],
-            'crack-sealing' => [],
-            'roof-insulation-pitched-inside' => [2935],
-            'roof-insulation-pitched-replace-tiles' => [2935],
-            'roof-insulation-flat-current' => [2935],
-            'roof-insulation-flat-replace-current' => [2935],
+            'floor-insulation' => [2834, 2934],
+            'bottom-insulation' => [2834, 2934],
+            'floor-insulation-research' => [2834, 2934],
+            'cavity-wall-insulation' => [2834, 2933], // TODO: Mapping says gevel, which do we pick? 2933 === muur, 4296 === geveltuin
+            'facade-wall-insulation' => [2834, 2933], // TODO: Above
+            'wall-insulation-research' => [2834, 2933], // TODO: Above
+            'glass-in-lead' => [2834, 2936],
+            'hrpp-glass-only' => [2834, 2936],
+            'hrpp-glass-frames' => [2834, 2936],
+            'hr3p-frames' => [2834, 2936],
+            'crack-sealing' => [], // TODO: Nothing in the mapping, but seeing we have a small-measures crack sealing, could this not apply for 4483 also? Or otherwise 4655 (overige energie maatregelen)?
+            'roof-insulation-pitched-inside' => [2834, 2935],
+            'roof-insulation-pitched-replace-tiles' => [2834, 2935],
+            'roof-insulation-flat-current' => [2834, 2935],
+            'roof-insulation-flat-replace-current' => [2834, 2935],
             'high-efficiency-boiler-replace' => [],
             'heater-place-replace' => [2837],
             'solar-panels-place-replace' => [2836],
-            'ventilation-balanced-wtw' => [],
-            'ventilation-decentral-wtw' => [],
-            'ventilation-demand-driven' => [],
+            'ventilation-balanced-wtw' => [2937, 2941],
+            'ventilation-decentral-wtw' => [2937, 2941],
+            'ventilation-demand-driven' => [2937],
             'hybrid-heat-pump-outside-air' => [2835],
-            'hybrid-heat-pump-ventilation-air' => [2835],
-            'hybrid-heat-pump-pvt-panels' => [2835],
+            'hybrid-heat-pump-ventilation-air' => [2835, 2941],
+            'hybrid-heat-pump-pvt-panels' => [2835, 2836],
             'full-heat-pump-outside-air' => [2835],
             'full-heat-pump-ground-heat' => [2835],
-            'full-heat-pump-pvt-panels' => [2835],
-            'heat-pump-boiler-place-replace' => [],
-            'save-energy-with-light' => [],
-            'energy-efficient-equipment' => [],
-            'energy-efficient-installations' => [],
-            'save-energy-with-crack-sealing' => [],
-            'improve-radiators' => [],
-            'improve-heating-installations' => [],
-            'save-energy-with-warm-tap-water' => [],
-            'general' => [],
+            'full-heat-pump-pvt-panels' => [2835, 2836],
+            'heat-pump-boiler-place-replace' => [2941],
+            'save-energy-with-light' => [4483],
+            'energy-efficient-equipment' => [4483],
+            'energy-efficient-installations' => [4483],
+            'save-energy-with-crack-sealing' => [4483],
+            'improve-radiators' => [4483],
+            'improve-heating-installations' => [4483],
+            'save-energy-with-warm-tap-water' => [4483],
+            'general' => [4483],
         ];
 
         $targetGroups = collect(
@@ -88,6 +88,7 @@ class SyncMeasures extends Command
         )->keyBy('Value');
 
         foreach ($map as $measureApplicationShort => $targetMeasureValues) {
+            // TODO: This will now only ever sync one of the values...
             foreach ($targetMeasureValues as $targetMeasureValue) {
                 $mappingService
                     ->from(MeasureApplication::findByShort($measureApplicationShort))

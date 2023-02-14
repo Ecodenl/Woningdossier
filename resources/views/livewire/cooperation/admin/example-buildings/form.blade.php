89f4a7d8fdc36ef0c88b2104cae76b9299121753
Yinci <yannisch@hotmail.com>
2023-02-14 12:47:43 +0100

    A cooperation admin should not be allowed to select a cooperation for the EB

--- resources/views/livewire/cooperation/admin/example-buildings/form.blade.php
+++ resources/views/livewire/cooperation/admin/example-buildings/form.blade.php
@@ -43,10 +43,15 @@
 
     <div class="form-group {{ $errors->has('exampleBuildingValues.cooperation_id') ? ' has-error' : '' }}">
         <label for="cooperation">@lang('cooperation/admin/example-buildings.components.cooperation')</label>
-        <select id="cooperation" wire:model="exampleBuildingValues.cooperation_id" class="form-control">
-            <option value="" selected="selected">-</option>
+        <select id="cooperation" wire:model="exampleBuildingValues.cooperation_id" class="form-control"
+                @if(! $isSuperAdmin) disabled @endif>
+            @if($isSuperAdmin)
+                <option value="" selected="selected">-</option>
+            @endif
             @foreach($cooperations as $cooperation)
-                <option value="{{ $cooperation->id }}">{{ $cooperation->name }}</option>
+                <option value="{{ $cooperation->id }}">
+                    {{ $cooperation->name }}
+                </option>
             @endforeach
         </select>
 

da565a64e6324bdef547e4cb65a0667bbbcd267e
Bodhi <bodhilooij1998@gmail.com>
2023-02-16 15:40:21 +0100

    This is fine aswell.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,33 +2,16 @@
 
 namespace Tests;
 
-use Database\Seeders\ToolQuestionTypesTableSeeder;
-use Database\Seeders\StatusesTableSeeder;
-use Database\Seeders\RoleTableSeeder;
-use Database\Seeders\InputSourcesTableSeeder;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use KIlluminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    /** @var bool $usesDatabase Not every test uses the database (e.g. StrTest). This allows us to disable the seeders. */
-    protected bool $usesDatabase = true;
-
     protected function setUp(): void
     {
         parent::setUp();
-
-        if ($this->usesDatabase) {
-            // Seed data that should ALWAYS be present
-            Artisan::call('db:seed', ['--class' => ToolQuestionTypesTableSeeder::class, '--force' => true]);
-            Artisan::call('db:seed', ['--class' => InputSourcesTableSeeder::class, '--force' => true]);
-            Artisan::call('db:seed', ['--class' => RoleTableSeeder::class, '--force' => true]);
-            Artisan::call('db:seed', ['--class' => StatusesTableSeeder::class, '--force' => true]);
-
-            // Ensure we clear cache (findByShort could be troublesome)
-            Artisan::call('cache:clear');
-        }
+        Artisan::call('cache:clear');
     }
 }

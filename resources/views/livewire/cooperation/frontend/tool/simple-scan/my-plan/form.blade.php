b848f47427f6e27b09512086f5cc64585d3da6b8
Yinci <yannisch@hotmail.com>
2023-02-17 11:42:18 +0100

    Styling consistency

--- resources/views/livewire/cooperation/frontend/tool/simple-scan/my-plan/form.blade.php
+++ resources/views/livewire/cooperation/frontend/tool/simple-scan/my-plan/form.blade.php
@@ -78,7 +78,9 @@
                                 @if(! empty($card['info']))
                                     <i class="icon-md icon-info-light clickable" x-on:click="toggle()"></i>
                                     @component('cooperation.frontend.layouts.components.modal')
-                                        {!! $card['info'] !!}
+                                        <div class="as-text">
+                                            {!! $card['info'] !!}
+                                        </div>
                                     @endcomponent
                                 @endif
                             </div>

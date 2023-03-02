91c9f4b8454dc0898d3e4e8ada05b2161f8f5966
Bodhi <bodhilooij1998@gmail.com>
2023-03-02 13:46:54 +0100

    - Removed various olds tables and their related models.] - Edited the measure_categories table so the short is added.

--- database/migrations/2023_03_02_134459_add_short_column_to_measure_categories_table.php
+++ database/migrations/2023_03_02_134459_add_short_column_to_measure_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDeviceOptionsTable extends Migration
+class AddShortColumnToMeasureCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateDeviceOptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('device_options', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('measure_categories', function (Blueprint $table) {
+            $table->string('short')->after('name')->nullable()->default(null);
         });
     }
 
@@ -27,6 +25,8 @@ class CreateDeviceOptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('device_options');
+        Schema::table('measure_categories', function (Blueprint $table) {
+            $table->dropColumn('short');
+        });
     }
 }

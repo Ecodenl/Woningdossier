c49ebe53716e122f296459cce35365b55033fb8c
Yinci <yannisch@hotmail.com>
2023-03-06 07:43:21 +0100

    Call bound service so we have a client

--- app/Http/Controllers/Cooperation/CreateBuildingController.php
+++ app/Http/Controllers/Cooperation/CreateBuildingController.php
@@ -40,7 +40,7 @@ class CreateBuildingController extends Controller
         $data = $request->all();
 
         // now get the picoaddress data.
-        BagService::init()->addressExpanded($data['postal_code'], $data['number'])->prepareForBuilding();
+        app(BagService::class)->addressExpanded($data['postal_code'], $data['number'])->prepareForBuilding();
 
         $data['bag_addressid'] = isset($picoAddressData['bag_adresid']) ? $picoAddressData['bag_adresid'] : '';
 

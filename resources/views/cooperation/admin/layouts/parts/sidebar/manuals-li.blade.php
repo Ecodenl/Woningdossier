115c7d5bb328c9ec58947380bebe0d9a964e3f9a
Bodhi <bodhilooij1998@gmail.com>
2023-04-05 10:33:09 +0200

    Added the manuals in the sidebar.

---
+++ resources/views/cooperation/admin/layouts/parts/sidebar/manuals-li.blade.php
@@ -0,0 +1,11 @@
+@if(in_array(\App\Helpers\HoomdossierSession::getRole(true)->name, [RoleHelper::ROLE_COOPERATION_ADMIN, RoleHelper::ROLE_COORDINATOR]))
+<li class="list-group-item">
+        <a href="https://alfresco.econobis.nl/share/s/hcfoj_gOQByf_moMhrXz2w" target="_blank">Beheerdershandleiding</a>
+</li>
+@endif
+<li class="list-group-item">
+    <a href="https://alfresco.econobis.nl/share/s/ihlfTvAkSoSgzQTVaJN90A" target="_blank">Coachhandleiding</a>
+</li>
+<li class="list-group-item">
+    <a href="https://alfresco.econobis.nl/share/s/kNtHbpHxTeq5Q5EOib1ySA" target="_blank">Bewonershandleiding</a>
+</li>

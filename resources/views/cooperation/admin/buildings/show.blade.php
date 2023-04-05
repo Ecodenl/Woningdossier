23384d24a89ddd72ca5440bc0ad6918763d7cda5
Bodhi <bodhilooij1998@gmail.com>
2023-04-05 10:04:12 +0200

    Instead of showing nothing, show "Gemeente onbekend."

--- resources/views/cooperation/admin/buildings/show.blade.php
+++ resources/views/cooperation/admin/buildings/show.blade.php
@@ -7,7 +7,7 @@
                 'name' => $user->getFullName(),
                 'street-and-number' => $building->street.' '.$building->number.' '.$building->extension,
                 'zipcode-and-city' => $building->postal_code.' '.$building->city,
-                'municipality' => optional($building->municipality)->name,
+                'municipality' => optional($building->municipality)->name ?? 'Gemeente onbekend',
                 'email' => $user->account->email,
                 'phone-number' => $user->phone_number,
             ])

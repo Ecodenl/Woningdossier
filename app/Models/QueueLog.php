052a46491186a423e8783160dc57de497322c465
Yinci <yannisch@hotmail.com>
2023-04-24 12:00:52 +0200

    Don't increment

--- app/Models/QueueLog.php
+++ app/Models/QueueLog.php
@@ -10,12 +10,13 @@ class QueueLog extends Model
 {
     use HasFactory;
 
-    // If we set this to null it causes an exception... Table has NO primary key.
-    //protected $primaryKey = null;
+    protected $primaryKey = null;
 
     protected $fillable = [
         'queue', 'size',
     ];
+
+    public $incrementing = false;
 
     // Scopes
     public function scopeForQueue(Builder $queue, string $queueName): Builder

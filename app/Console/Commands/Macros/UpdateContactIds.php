351a20d9b4af1181340dec7d67895a9fb277bccd
Bodhi <bodhilooij1998@gmail.com>
2023-06-15 15:28:52 +0200

    - Updated the UpdateContactIds.php command for a specific update file.

--- app/Console/Commands/Macros/UpdateContactIds.php
+++ app/Console/Commands/Macros/UpdateContactIds.php
@@ -2,13 +2,20 @@
 
 namespace App\Console\Commands\Macros;
 
+use App\Helpers\Arr;
 use App\Helpers\FileFormats\CsvHelper;
 use App\Models\Building;
+use App\Models\Cooperation;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * NOTE;
+ * This command will change, since the given csvs will not and never will be anywhere near consistent.
+ * DO NOT load up the contact csv file and run it blindly.
+ */
 class UpdateContactIds extends Command
 {
     /**
@@ -34,25 +41,23 @@ class UpdateContactIds extends Command
 
         $changes = CsvHelper::toArray(Storage::disk('local')->path('contact-ids.csv'), ';', false);
         foreach ($changes as $data) {
-            // Not using associative headers because misspellings...
-            $buildingId = $data[2];
-            $newContactId = $data[0];
+            $cooperationName = $data[0];
+            $accountId = $data[1];
+            $newContactId = $data[2];
 
-            $building = Building::find($buildingId);
-            $user = $building->user ?? null;
+            $cooperation = Cooperation::where('slug', 'gouda')->first();
+            $user = User::forMyCooperation($cooperation->id)->where('account_id', $accountId)->first();
 
             if ($user instanceof User) {
                 $extra = $user->extra ?? [];
-                $oldContactId = data_get($extra, 'contact_id');
-                Log::info("Changing the contact ID of user {$user->id} (building {$buildingId}) from {$oldContactId} to {$newContactId}");
+                $oldContactId = Arr::get($extra, 'contact_id', 'No old');
+                Log::info("Changing the contact ID of user {$user->id} from {$oldContactId} to {$newContactId}");
 
                 $extra['contact_id'] = $newContactId;
 
                 $user->update([
                     'extra' => $extra,
                 ]);
-            } else {
-                Log::warning("No user found for building {$buildingId}");
             }
         }
     }

8f156d82fabfe43b5c7ca23e7567504fc681f240
Bodhi <bodhilooij1998@gmail.com>
2023-10-12 11:52:40 +0200

    Added an import and export command for the tool question translations.

---
+++ app/Imports/ToolQuestionsImport.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\ToolQuestion;
+use App\User;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+
+class ToolQuestionsImport implements ToCollection, WithCustomCsvSettings, WithHeadingRow
+{
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) {
+            $short = $row['short'];
+            if ( ! empty($short)) {
+                $toolQuestionTable = (new ToolQuestion())->getTable();
+
+                if (DB::table($toolQuestionTable)->where('short', $short)->exists()) {
+                    Log::debug("updating for {$short}");
+                    DB::table($toolQuestionTable)
+                        ->where('short', $short)
+                        ->update([
+                            'name' => $row['name'],
+                            'help_text' => $row['help_text'],
+                        ]);
+                } else {
+                    Log::debug(__CLASS__."{$short} does not exist in the tool questions!");
+                }
+            }
+        }
+    }
+
+    public function getCsvSettings(): array
+    {
+        return [
+            'delimiter' => ",",
+            'escape_character' => "\""
+        ];
+    }
+}

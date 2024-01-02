a6e3f269c0b0e24a6653d357a10ee4640b7af020
pvankouteren <pvankouteren@gmail.com>
2024-01-02 14:28:55 +0100

    Leave out content length as it seems it's not always calculated the right way. This would create a false expectation on the receiving end, which might result in a time-out as it the receiver is expecting more bytes to be transferred.

--- app/Console/Commands/Api/Econobis/Out/Hoomdossier/PdfReport.php
+++ app/Console/Commands/Api/Econobis/Out/Hoomdossier/PdfReport.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\Api\Econobis\Out\Hoomdossier;
 
+use App\Helpers\Queue;
 use App\Jobs\Econobis\Out\SendPdfReportToEconobis;
 use App\Models\FileStorage;
 use App\Models\FileType;
@@ -80,7 +81,7 @@ class PdfReport extends Command
             ->chunkById(50, function ($fileStorages) {
                 foreach ($fileStorages as $fileStorage) {
                     Log::debug("Sending PDF report to Econobis for building {$fileStorage->building_id}");
-                    SendPdfReportToEconobis::dispatch($fileStorage->building);
+                    SendPdfReportToEconobis::dispatch($fileStorage->building)->onQueue(Queue::APP_EXTERNAL);
                 }
             }, FileStorage::getModel()->getTable().'.'.FileStorage::getModel()->getKeyName(), 'file_storage_id');
 

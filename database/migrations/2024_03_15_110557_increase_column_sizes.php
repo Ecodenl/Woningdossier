2fbeb1bc05e78258b5c732845c080f9fdecb62e7
Yinci <yannisch@hotmail.com>
2024-03-15 11:24:35 +0100

    Increase max size

---
+++ database/migrations/2024_03_15_110557_increase_column_sizes.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class IncreaseColumnSizes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_action_plan_advices', function (Blueprint $table) {
+            $table->decimal('savings_gas', 65)->nullable()->change();
+            $table->decimal('savings_electricity', 65)->nullable()->change();
+            $table->decimal('savings_money', 65)->nullable()->change();
+        });
+
+        Schema::table('user_energy_habits', function (Blueprint $table) {
+            $table->bigInteger('amount_electricity')->nullable()->change();
+            $table->bigInteger('amount_gas')->nullable()->change();
+            $table->bigInteger('amount_water')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // No need to roll back
+    }
+}

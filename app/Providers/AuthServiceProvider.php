9ce6ddb5aff273b3246b6d41fad3330b058f0ab1
Yinci <yannisch@hotmail.com>
2024-07-05 15:03:32 +0200

    Allow verifying emails

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,27 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Building;
-use App\Models\Cooperation;
-use App\Models\FileStorage;
-use App\Models\Media;
-use App\Models\PrivateMessage;
-use App\Models\Questionnaire;
-use App\Models\Role;
-use App\Models\SubStep;
-use App\Models\ToolQuestion;
-use App\Models\User;
-use App\Models\UserActionPlanAdvice;
 use App\Policies\BuildingPolicy;
-use App\Policies\CooperationPolicy;
-use App\Policies\FileStoragePolicy;
-use App\Policies\MediaPolicy;
-use App\Policies\PrivateMessagePolicy;
-use App\Policies\QuestionnairePolicy;
 use App\Policies\RolePolicy;
-use App\Policies\SubStepPolicy;
-use App\Policies\ToolQuestionPolicy;
-use App\Policies\UserActionPlanAdvicePolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -35,17 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        SubStep::class => SubStepPolicy::class,
-        ToolQuestion::class => ToolQuestionPolicy::class,
-        PrivateMessage::class => PrivateMessagePolicy::class,
-        Questionnaire::class => QuestionnairePolicy::class,
-        Cooperation::class => CooperationPolicy::class,
-        User::class => UserPolicy::class,
-        Building::class => BuildingPolicy::class,
-        FileStorage::class => FileStoragePolicy::class,
-        UserActionPlanAdvice::class => UserActionPlanAdvicePolicy::class,
-        Media::class => MediaPolicy::class,
-        Role::class => RolePolicy::class
+        // Use auto discovery
     ];
 
     /**
@@ -56,6 +27,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        Gate::guessPolicyNamesUsing(function ($modelClass) {
+            return 'App\\Policies\\'.class_basename($modelClass).'Policy';
+        });
 
         Gate::define('talk-to-resident', BuildingPolicy::class.'@talkToResident');
         Gate::define('access-building', BuildingPolicy::class.'@accessBuilding');

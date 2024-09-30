6b97bbe324e3804817d88568188cf9bc9c3dfb64
Yinci <yannisch@hotmail.com>
2024-09-30 11:12:58 +0200

    Remove todo

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,7 +14,6 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
         $this->artisan('cache:clear');
-        // TODO: check with colleges, if this is actually fine (https://spatie.be/docs/laravel-permission/v6/upgrading)
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
     }
 
@@ -23,7 +22,6 @@ abstract class TestCase extends BaseTestCase
         // This is important, as the cache created during testing might conflict with the app, which can cause
         // weird behaviour.
         Artisan::call('cache:clear');
-        // TODO: check with colleges, if this is actually fine (https://spatie.be/docs/laravel-permission/v6/upgrading)
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
         parent::tearDown();
     }

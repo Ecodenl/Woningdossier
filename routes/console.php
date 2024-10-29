64667140cc4fa5ea3ccbf1cd0f83112c547beb2f
Shift <shift@laravelshift.com>
2024-10-29 12:52:03 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,42 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\App;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\SendNotifications;
+use App\Console\Commands\CleanupExpiredFileStorages;
+use App\Console\Commands\Monitoring\MonitorQueue;
+use App\Console\Commands\AVG\CleanupPasswordResets;
+use App\Console\Commands\AVG\CleanupAudits;
+use App\Console\Commands\Api\Verbeterjehuis\Mappings\SyncTargetGroups;
+use App\Console\Commands\Api\Verbeterjehuis\Mappings\SyncMeasures;
+use App\Console\Commands\Api\Econobis\Out\Hoomdossier\Woonplan;
+use App\Console\Commands\Api\Econobis\Out\Hoomdossier\PdfReport;
+use App\Console\Commands\Api\Econobis\Out\Hoomdossier\Gebruik;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('send:notifications --type=private-message')->everyFifteenMinutes();
+
+Schedule::command(MonitorQueue::class)->everyFiveMinutes();
+
+Schedule::command(CleanupAudits::class)->daily();
+Schedule::command(CleanupPasswordResets::class)->daily();
+Schedule::command(CleanupExpiredFileStorages::class)->everyThirtyMinutes();
+
+Schedule::command(SyncTargetGroups::class)->daily();
+Schedule::command(SyncMeasures::class)->daily();
+
+Schedule::command(Gebruik::class)->dailyAt('01:00');
+if (App::environment() == 'accept') {
+    Schedule::command(Woonplan::class)->everyMinute()->withoutOverlapping();
+    Schedule::command(PdfReport::class)->everyMinute()->withoutOverlapping();
+} else {
+    Schedule::command(Woonplan::class)->everyFiveMinutes()->withoutOverlapping();
+    Schedule::command(PdfReport::class)->everyFiveMinutes()->withoutOverlapping();
+}

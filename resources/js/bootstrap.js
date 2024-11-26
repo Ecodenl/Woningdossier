3202d278652072738912eeb3c0fcbd0cf1b00c1f
Yinci <yannisch@hotmail.com>
2024-11-26 10:52:25 +0100

    Remove Axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,39 +7,13 @@ window._ = _;
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
+import jQuery from 'jquery';
 
-    require('bootstrap-sass');
+try {
+    Object.assign(window, { $: jQuery, jQuery });
 } catch (e) {}
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-/*
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-*/
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-/*
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-*/
+import 'bootstrap-sass';
 
 /**
  * Define functions that will be used throughout the whole application, that

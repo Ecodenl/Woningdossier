65a1bc496bbab5896c8963ea507d043f973c2985
Yinci <yannisch@hotmail.com>
2025-08-26 13:11:19 +0200

    Fix console warnings

--- resources/views/cooperation/tool-question-type-templates/text/show.blade.php
+++ resources/views/cooperation/tool-question-type-templates/text/show.blade.php
@@ -1,4 +1,4 @@
-<input class="form-input @if(!empty($toolQuestion->unit_of_measure)) with-append @endif"
+<input class="form-input @if(! empty($toolQuestion->unit_of_measure)) with-append @endif"
        id="{{$toolQuestion->short}}" wire:model.change="filledInAnswers.{{$toolQuestion->short}}"
        placeholder="{{$toolQuestion->placeholder}}" type="text"
        @if(($disabled ?? false))
@@ -7,7 +7,7 @@
            x-on:input-updated.window="$el.setAttribute('disabled', true);"
            x-on:input-update-processed.window="$el.removeAttribute('disabled');"
        @endif>
-@if(!empty($toolQuestion->unit_of_measure))
+@if(! empty($toolQuestion->unit_of_measure))
     <div class="input-group-append">
         {{$toolQuestion->unit_of_measure}}
     </div>

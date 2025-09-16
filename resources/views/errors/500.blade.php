9797dbe433957b475df092d02b4cdad00d4fa967
Yinci <yannisch@hotmail.com>
2025-09-16 11:08:43 +0200

    No nonce in error

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -7,7 +7,8 @@
         <!-- Sentry JS SDK 2.1.+ required -->
         <script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
 
-        <script type="module" nonce="{{ $cspNonce }}">
+{{--        <script type="module" nonce="{{ $cspNonce }}">--}}
+        <script type="module">
             @if (Auth::check())
             Raven.showReportDialog({
                 eventId: '{{ Sentry::getLastEventID() }}',
